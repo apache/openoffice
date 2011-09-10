2a5dd75803c2d3c3dd2a1cef4c6c9ea3e560ce9e
Eike Rathke <erack@apache.org>
2011-09-10 22:47:10 +0000

    impress212: removed warning
    # User sj <sj@openoffice.org>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ooo/trunk/main@1167630 13f79535-47bb-0310-9956-ffa450edef68

--- svtools/source/filter/wmf/winmtf.cxx
+++ svtools/source/filter/wmf/winmtf.cxx
@@ -82,9 +82,8 @@ void WinMtfClipPath::ExcludeClipRect( const Rectangle& rRect )
 
 void WinMtfClipPath::SetClipPath( const PolyPolygon& rPolyPolygon, sal_Int32 nClippingMode )
 {
-//	if ( !rPolyPolygon.Count() )
-	if ( true )        
-		aPolyPoly = rPolyPolygon;
+	if ( !rPolyPolygon.Count() )
+        aPolyPoly = rPolyPolygon;
 	else if ( nDepth < WIN_MTF_MAX_CLIP_DEPTH )
 	{
 		nDepth++;
