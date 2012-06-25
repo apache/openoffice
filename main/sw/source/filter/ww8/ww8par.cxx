4f0128d8ce6b987e30fa3455ca3a6f1f4a2880c8
Armin Le Grand <alg@apache.org>
2012-06-25 11:47:20 +0000

    #119516# Be prepared to allow not only 0x8 0x1 detection for canvases, but also 0x1 0x1. Patch by: Lei Debin Detected by: louqle Review by: alg
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ooo/trunk@1353476 13f79535-47bb-0310-9956-ffa450edef68

--- main/sw/source/filter/ww8/ww8par.cxx
+++ main/sw/source/filter/ww8/ww8par.cxx
@@ -2762,30 +2762,52 @@ bool SwWW8ImplReader::ReadChar(long nPosCp, long nCpOfs)
             graphic preview of an associated ole2 object (or a simple
             graphic of course)
             */
-            if (!IsInlineEscherHack())
-            {
-                SwFrmFmt *pResult = 0;
-                if (bObj)
-                    pResult = ImportOle();
-                else if (bSpec)
-                    pResult = ImportGraf();
+			//normally in the canvas field, the code is 0x8 0x1.
+			//in a special case, the code is 0x1 0x1, which yields a simple picture
+			{
+				bool bReadObj = IsInlineEscherHack();
+				if( bReadObj )
+				{
+					long nCurPos = pStrm->Tell();
+					sal_uInt8   nByteCode;
+					sal_uInt16 nWordCode;
 
-                //#102160# If we have a bad 0x1 insert a space instead.
-                if (!pResult)
-                {
-                    cInsert = ' ';
-                    ASSERT(!bObj && !bEmbeddObj && !nObjLocFc,
-                        "WW8: Please report this document, it may have a "
-                        "missing graphic");
-                }
-                else
-                {
-                    // reset the flags.
-                    bObj = bEmbeddObj = false;
-                    nObjLocFc = 0;
-                }
-            }
-            break;
+					if( bIsUnicode )
+						*pStrm >> nWordCode;
+					else
+					{
+						*pStrm >> nByteCode;
+						nWordCode = nByteCode;
+					}
+					if( nWordCode == 0x1 )
+						bReadObj = false;
+					pStrm->Seek( nCurPos );	
+				}
+				if( !bReadObj )
+				{
+					SwFrmFmt *pResult = 0;
+					if (bObj)
+						pResult = ImportOle();
+					else if (bSpec)
+						pResult = ImportGraf();
+
+					//#102160# If we have a bad 0x1 insert a space instead.
+					if (!pResult)
+					{
+						cInsert = ' ';
+						ASSERT(!bObj && !bEmbeddObj && !nObjLocFc,
+							"WW8: Please report this document, it may have a "
+							"missing graphic");
+					}
+					else
+					{
+						// reset the flags.
+						bObj = bEmbeddObj = false;
+						nObjLocFc = 0;
+					}
+				}
+			}
+			break;
         case 0x8:
             if( !bObj )
                 Read_GrafLayer( nPosCp );
