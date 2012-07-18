afcfdd493d8dd420821462ae311cf7fe5d9ef64d
Liu Zhe <liuzhe@apache.org>
2012-07-18 02:28:42 +0000

    #120295# - VCLAuto patch: refactor function verification test. Test cases about text document and presentation.           Patch by: Li Lin Yi           Review by: Liu Zhe
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ooo/trunk/main@1362744 13f79535-47bb-0310-9956-ffa450edef68

--- test/testoo/src/testcase/presentation/headerandfooter/OpenDocumentWithHeaderFooter.java
+++ test/testoo/src/testcase/presentation/headerandfooter/OpenDocumentWithHeaderFooter.java
@@ -21,53 +21,41 @@
 
 
 
-package testcase.sd.headerandfooter;
+/**
+ * 
+ */
+package testcase.presentation.headerandfooter;
 
 import static testlib.AppUtil.fullPath;
-import static testlib.AppUtil.initApp;
 import static testlib.AppUtil.openStartcenter;
 import static testlib.AppUtil.submitSaveDlg;
 import static testlib.AppUtil.submitOpenDlg;
 import static testlib.AppUtil.testFile;
-import static testlib.UIMap.startcenter;
 import static testlib.UIMap.*;
-
-import java.io.File;
-
 import static org.junit.Assert.*;
 import static org.openoffice.test.vcl.Tester.*;
-import static org.openoffice.test.vcl.Tester.typeKeys;
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.openoffice.test.common.FileUtil;
-
 import testlib.Log;
-import testlib.UIMap;
 
-public class OpenAOO34WithHeaderFooter {
-	/**
-	 * TestCapture helps us to do
-	 * 1. Take a screenshot when failure occurs.
-	 * 2. Collect extra data when OpenOffice crashes.
-	 */	
+public class OpenDocumentWithHeaderFooter {
+
 	@Rule
 	public Log LOG = new Log();
-	
-	/**
-	 * initApp helps us to do 
-	 * 1. Patch the OpenOffice to enable automation if necessary.
-	 * 2. Start OpenOffice with automation enabled if necessary.
-	 * 3. Reset OpenOffice to startcenter.
-	 * 
-	 * @throws java.lang.Exception
-	 */
+
 	@Before
 	public void setUp() throws Exception {
-		initApp();
+		app.start();
 	}
-	
+
+	@After
+	public void tearDown() throws Exception {
+		app.close();
+	}
+
 	/**
 	 * Test open AOO3.4 presentation with header and footer.
 	 * edit and save to ODP
@@ -77,9 +65,9 @@ public class OpenAOO34WithHeaderFooter {
 	public void testOpenAOO34WithHeaderFooter() throws Exception{
 		//open sample file
 		String file = testFile("sd/AOO3.4HeaderFooter.odp");
-		startcenter.menuItem("File->Open").select();
+		app.dispatch(".uno:Open");
 		submitOpenDlg(file);
-		
+
 		//check after reopen		
 		impress.menuItem("View->Header and Footer...").select();
 		sleep(1);
@@ -88,11 +76,10 @@ public class OpenAOO34WithHeaderFooter {
 		assertEquals(true,SD_FooterTextOnSlide.isChecked());
 		assertEquals("footer test",SD_FooterTextOnSlideInput.getText());
 		assertEquals(true,SD_SlideNumAsFooterOnSlide.isChecked());
-				
+
 		SD_SlideNumAsFooterOnSlide.uncheck();
 		SD_ApplyToAllButtonOnSlideFooter.click();
-		
-	
+
 		//save to odp and reopen
 		impress.menuItem("File->Save As...").select();
 		String saveTo2 = fullPath("temp/" + "AOO3.4HeaderFooter.odp");
@@ -100,18 +87,85 @@ public class OpenAOO34WithHeaderFooter {
 		submitSaveDlg(saveTo2);	
 		impress.menuItem("File->Close").select();
 		sleep(1);
-		
+
 		openStartcenter();
-		startcenter.menuItem("File->Open").select();
+		app.dispatch(".uno:Open");
 		String openFrom2=fullPath("temp/" + "AOO3.4HeaderFooter.odp");
 		submitOpenDlg(openFrom2);
-		
+
 		impress.menuItem("View->Header and Footer...").select();
 		sleep(1);
 		assertEquals(false,SD_SlideNumAsFooterOnSlide.isChecked());
 		//end-save to odp and reopen
 	}
-	
 
-	
+	/**
+	 * Test open ppt file with header and footer.
+	 * edit and save to PPT/ODP
+	 * @throws Exception
+	 */
+	@Test
+	public void testOpenPPTWithHeaderFooter() throws Exception{
+		//open sample file
+		String file = testFile("sd/gfdd.ppt");
+		app.dispatch(".uno:Open");
+		submitOpenDlg(file);
+
+		//check after reopen		
+		impress.menuItem("View->Header and Footer...").select();
+		sleep(1);
+		assertEquals(true,SD_DateAndTimeFooterOnSlide.isChecked());
+		assertEquals("testdte",SD_FixedDateAndTimeOnSlideInput.getText());
+		assertEquals(true,SD_FooterTextOnSlide.isChecked());
+		assertEquals("yesy",SD_FooterTextOnSlideInput.getText());
+		assertEquals(true,SD_SlideNumAsFooterOnSlide.isChecked());
+
+		SD_SlideNumAsFooterOnSlide.uncheck();
+		SD_ApplyToAllButtonOnSlideFooter.click();
+
+		//save to ppt and reopen
+		impress.menuItem("File->Save As...").select();
+		String saveTo = fullPath("temp/" + "gfdd.ppt");
+		FileUtil.deleteFile(saveTo);
+		submitSaveDlg(saveTo);	
+		if (AlienFormatDlg.exists(3))
+			AlienFormatDlg.ok();
+		sleep(1);
+		impress.menuItem("File->Close").select();
+		sleep(1);
+
+		openStartcenter();
+		app.dispatch(".uno:Open");
+		String openFrom=fullPath("temp/" + "gfdd.ppt");
+		submitOpenDlg(openFrom);
+
+		impress.menuItem("View->Header and Footer...").select();
+		sleep(1);
+		assertEquals(false,SD_SlideNumAsFooterOnSlide.isChecked());
+		//end-save to ppt and reopen
+
+		//close Header and Footer dialog.
+		SD_ApplyButtonOnSlideFooter.focus();
+		typeKeys("<tab>");
+		typeKeys("<enter>");
+		//end
+
+		//save to odp and reopen
+		impress.menuItem("File->Save As...").select();
+		String saveTo2 = fullPath("temp/" + "gfdd.odp");
+		FileUtil.deleteFile(saveTo2);
+		submitSaveDlg(saveTo2);	
+		impress.menuItem("File->Close").select();
+		sleep(1);
+
+		openStartcenter();
+		app.dispatch(".uno:Open");
+		String openFrom2=fullPath("temp/" + "gfdd.odp");
+		submitOpenDlg(openFrom2);
+
+		impress.menuItem("View->Header and Footer...").select();
+		sleep(1);
+		assertEquals(false,SD_SlideNumAsFooterOnSlide.isChecked());
+		//end-save to odp and reopen
+	}
 }
