b6113d73428c2f886635a0d5ecac7ab137196d0b
Liu Zhe <liuzhe@apache.org>
2012-07-18 02:28:42 +0000

    #120295# - VCLAuto patch: refactor function verification test. Test cases about text document and presentation.           Patch by: Li Lin Yi           Review by: Liu Zhe
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ooo/trunk@1362744 13f79535-47bb-0310-9956-ffa450edef68

--- main/test/testoo/src/testcase/textdocument/table/Table.java
+++ main/test/testoo/src/testcase/textdocument/table/Table.java
@@ -24,80 +24,67 @@
 /**
  * 
  */
-package testcase.sw.table;
+package testcase.textdocument.table;
 
 import static testlib.AppUtil.*;
 import static testlib.UIMap.*;
-
-import java.awt.Rectangle;
-import java.io.File;
-
 import static org.junit.Assert.*;
-import static org.openoffice.test.vcl.Tester.*;
-
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
-import org.openoffice.test.common.FileUtil;
-import org.openoffice.test.common.GraphicsUtil;
+import org.openoffice.test.common.SystemUtil;
 
-import testlib.CalcUtil;
 import testlib.Log;
 
 /**
  *
  */
-public class ConvertTableToText {
-	
-	/**
-	 * TestCapture helps us to do
-	 * 1. Take a screenshot when failure occurs.
-	 * 2. Collect extra data when OpenOffice crashes.
-	 */	
+public class Table {
+
 	@Rule
 	public Log LOG = new Log();
-	
-	/**
-	 * initApp helps us to do 
-	 * 1. Patch the OpenOffice to enable automation if necessary.
-	 * 2. Start OpenOffice with automation enabled if necessary.
-	 * 3. Reset OpenOffice to startcenter.
-	 * 
-	 * @throws java.lang.Exception
-	 */
+
 	@Before
 	public void setUp() throws Exception {
-		initApp();
+		app.start();
 	}
-	
+
+	@After
+	public void tearDown() throws Exception {
+		app.close();
+	}
+
 	/**
 	 * Test convert table to text in text document
 	 * @throws Exception
 	 */
 	@Test
 	public void testConvertTableToText() throws Exception{
-		
+
 		//Create a new text document
 		startcenter.menuItem("File->New->Text Document").select();
 		sleep(3);
-		
+
 		// Insert a table and input some data
 		writer.menuItem("Insert->Table...").select();
 		writer_InsertTable.ok();
-		writer.click(1, 1);
+		writer.focus();
 		typeKeys("1<right>2<right>3<right>4");
 		sleep(1);
-		
+
 		// Convert table to text
 		writer.menuItem("Table->Convert->Table to Text...").select();
 		assertTrue("Convert Table to Text dialog pop up", writer_ConvertTableToTextDlg.exists());
 		typeKeys("<enter>");
 		sleep(1);
-		
+
 		// Verify if text is converted successfully
 		writer.menuItem("Edit->Select All").select();
-		typeKeys("<$copy>");
-		assertEquals("Converted text", "1	2\n3	4\n", app.getClipboard());
+		app.dispatch(".uno:Copy");
+		if (SystemUtil.isWindows())
+			assertEquals("Converted text", "1\t2\r\n3\t4\r\n", app.getClipboard());	// windows�лس�������\r\n
+		else
+			assertEquals("Converted text", "1\t2\n3\t4\n", app.getClipboard());
 	}
 }
