059b96ec9e22648e0e2361f5956f453542aa937d
Andre Fischer <af@apache.org>
2012-08-13 08:13:54 +0000

    Add default to switch statements to avoid compiler warnings.
        Patch by: Pavel Janik
        Review by: Andre Fischer
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ooo/trunk@1372306 13f79535-47bb-0310-9956-ffa450edef68

--- main/filter/source/msfilter/svdfppt.cxx
+++ main/filter/source/msfilter/svdfppt.cxx
@@ -7940,6 +7940,9 @@ void	SdrPowerPointImport::ApplyTextAnchorAttributes( PPTTextObj& rTextObj, SfxIt
 		case mso_anchorBottomCentered:
 			eTHA = SDRTEXTHORZADJUST_LEFT;
 			break;
+
+        default:
+            break;
 		}
 		// if there is a 100% use of following attributes, the textbox can been aligned also in vertical direction
 		switch ( eTextAnchor )
@@ -7989,6 +7992,9 @@ void	SdrPowerPointImport::ApplyTextAnchorAttributes( PPTTextObj& rTextObj, SfxIt
 		case mso_anchorBottomCentered:
 			eTVA = SDRTEXTVERTADJUST_BOTTOM;
 			break;
+
+        default:
+            break;
 		}
 		// if there is a 100% usage of following attributes, the textbox can be aligned also in horizontal direction
 		switch ( eTextAnchor )
