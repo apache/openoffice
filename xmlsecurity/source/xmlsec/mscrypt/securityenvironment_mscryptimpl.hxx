f515bd9b9f2a15bac61517ee9e5913dd91e4b740
Jian Fang Zhang <zhangjf@apache.org>
2012-09-04 02:46:03 +0000

    #i120675#, the certificate store handles need to be closed after closing the document
    Found by: zhangjf 
    Patch by: zhangjf
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ooo/trunk/main@1380438 13f79535-47bb-0310-9956-ffa450edef68

--- xmlsecurity/source/xmlsec/mscrypt/securityenvironment_mscryptimpl.hxx
+++ xmlsecurity/source/xmlsec/mscrypt/securityenvironment_mscryptimpl.hxx
@@ -75,7 +75,13 @@ class SecurityEnvironment_MSCryptImpl : public ::cppu::WeakImplHelper4<
 
 		//Certiticate store
 		HCERTSTORE							m_hCertStore ;
-
+		
+		// i120675, save the store handles
+		HCERTSTORE 							m_hMySystemStore;
+		HCERTSTORE 							m_hRootSystemStore;
+		HCERTSTORE 							m_hTrustSystemStore;
+		HCERTSTORE 							m_hCaSystemStore;
+		
 		//Enable default system cryptography setting
 		sal_Bool							m_bEnableDefault ;
 
