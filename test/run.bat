598968888b2bc22963b112588e3bd56e82bea87f
Liu Zhe <liuzhe@apache.org>
2012-09-17 13:15:13 +0000

    fix run.bat can't be started on windows
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ooo/trunk@1386600 13f79535-47bb-0310-9956-ffa450edef68

--- test/run.bat
+++ test/run.bat
@@ -1,3 +1,3 @@
 @echo OFF
 set BASE=%~pd0%
-java -cp "%BASE%\lib\junit.jar;%BASE%\testcommon\bin;%BASE%\testgui\bin;%BASE%\testuno\bin;%BASE%\testgui\data;%BASE%\testuno\data" org.openoffice.test.Run -r org.openoffice.test.common.Installer -l org.openoffice.test.common.XMLReporter -l org.openoffice.test.common.ReportUploader %*
+java -cp "%BASE%\lib\junit.jar;%BASE%\testcommon\bin;%BASE%\testgui\bin;%BASE%\testuno\bin;%BASE%\testgui\data;%BASE%\testuno\data" org.openoffice.test.Run -r org.openoffice.test.common.Installer -l org.openoffice.test.common.XMLReporter -l org.openoffice.test.common.ReportUploader "-Dtestspace=%BASE%\testspace" -Dsingleton=true %*
