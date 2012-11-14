82c22f63fde620022bb6c58049a06047c83fa63d
Andrew Rist <arist@apache.org>
2012-11-14 23:53:04 +0000

    gnumake4_087_8113b354673f.patch # HG changeset patch # User Ocke Janssen [oj] <Ocke.Janssen@oracle.com> # Date 1302768235 -7200 # Node ID 8113b354673f91015f63660168e7e1196ed224ea # Parent ca1d5b7d208318e84278b7f08069764c999c227b gnumake4: converted dbaccess
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ooo/branches/gbuild@1409572 13f79535-47bb-0310-9956-ffa450edef68

--- main/dbaccess/source/filter/xml/xmlservices.cxx
+++ main/dbaccess/source/filter/xml/xmlservices.cxx
@@ -22,7 +22,7 @@
 
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_dbaccess.hxx"
+#include "precompiled_dbaxml.hxx"
 
 #ifndef _CPPUHELPER_FACTORY_HXX_
 #include <cppuhelper/factory.hxx>
@@ -70,7 +70,7 @@ extern "C" void SAL_CALL createRegistryInfo_dbaxml()
 
 //---------------------------------------------------------------------------------------
 
-extern "C" void SAL_CALL component_getImplementationEnvironment(
+extern "C" SAL_DLLPUBLIC_EXPORT void SAL_CALL component_getImplementationEnvironment(
 				const sal_Char	**ppEnvTypeName,
 				uno_Environment	**
 			)
@@ -80,7 +80,7 @@ extern "C" void SAL_CALL component_getImplementationEnvironment(
 }
 
 //---------------------------------------------------------------------------------------
-extern "C" void* SAL_CALL component_getFactory(
+extern "C" SAL_DLLPUBLIC_EXPORT void* SAL_CALL component_getFactory(
 					const sal_Char* pImplementationName,
 					void* pServiceManager,
 					void* /*pRegistryKey*/)
