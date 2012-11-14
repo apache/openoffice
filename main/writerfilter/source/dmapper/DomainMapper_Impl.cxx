ea5fbc370b6627062b060d9985ce05083962976d
Andrew Rist <arist@apache.org>
2012-11-14 21:55:58 +0000

    writerfilter10_24_9d195c8de43d.patch # HG changeset patch # User Henning Brinkmann <hbrinkm@openoffice.org> # Date 1298390235 -3600 # Node ID 9d195c8de43d6ba3f08f7fea2559405faedff470 # Parent  6952713a7a64383f599ce496b2756f0775b32bd6 writerfilter10: fix wrong #ifdef
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ooo/branches/gbuild@1409460 13f79535-47bb-0310-9956-ffa450edef68

--- main/writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ main/writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1497,7 +1497,7 @@ void DomainMapper_Impl::PushShapeContext( const uno::Reference< drawing::XShape 
     catch ( const uno::Exception& e )
     {
         (void) e;
-#if DEBUG
+#if DEBUG_DOMAINMAPPER
         dmapper_logger->element("exception");
 #endif
     }
