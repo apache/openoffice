2143f3338461997250329e0bb1ec5428b18e0924
Armin Le Grand <alg@apache.org>
2013-02-04 12:17:37 +0000

    removing reintegrated branch
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/branches/alg@1442089 13f79535-47bb-0310-9956-ffa450edef68

--- clibboard/main/external/msvcp100/README_msvcX100.dll.txt
+++
@@ -1,2 +0,0 @@
-Put msvcr100.dll  in this directory.
-Needed to support Java 7 built with VS2010.
--- clibboard/main/soltools/winunistd/unistd.h
+++
@@ -1,4 +0,0 @@
-/* Dummy unistd.h for the wntmsci3 environment. Required because flex 
- * generates a lexical scanner which includes <unistd.h>
- */
-
