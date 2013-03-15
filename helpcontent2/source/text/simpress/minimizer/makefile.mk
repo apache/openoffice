7269e788e8f4e8667c90b4867f448000d497016e
Ariel Constenla-Haile <arielch@apache.org>
2013-03-15 23:19:19 +0000

    i121872 - Integrate the Presentation Minimizer
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/trunk/main@1457158 13f79535-47bb-0310-9956-ffa450edef68

--- helpcontent2/source/text/simpress/minimizer/makefile.mk
+++ helpcontent2/source/text/simpress/minimizer/makefile.mk
@@ -1,5 +1,5 @@
 #**************************************************************
-#  
+#
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
 #  distributed with this work for additional information
@@ -7,18 +7,50 @@
 #  to you under the Apache License, Version 2.0 (the
 #  "License"); you may not use this file except in compliance
 #  with the License.  You may obtain a copy of the License at
-#  
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing,
 #  software distributed under the License is distributed on an
 #  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 #  KIND, either express or implied.  See the License for the
 #  specific language governing permissions and limitations
 #  under the License.
-#  
+#
 #**************************************************************
 
 
 
-EXTENSIONNAME:=PresentationMinimizer
+# edit to match directory level 
+PRJ=..$/..$/..$/..
+# same for all makefiles in "helpcontent2"
+PRJNAME = helpcontent2
+# edit to match the current package
+PACKAGE = text/simpress/minimizer
+# uniqe name (module wide);
+# using a modified form of package should do here
+TARGET  = text_simpress_minimizer
+# edit to match the current module
+MODULE  = simpress
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE : settings.mk
+.INCLUDE : $(PRJ)$/settings.pmk
+
+# this list matches the *.xhp files to process
+XHPFILES = \
+   main.xhp \
+   step1.xhp \
+   step2.xhp \
+   step3.xhp \
+   step4.xhp \
+   step5.xhp \
+   infobox.xhp \
+   shared.xhp \
+
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE :  target.mk
+.INCLUDE : tg_help.mk
