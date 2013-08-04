d7071b73a927bb3ad35ec37f7259cc6a2ca24b24
Jan Iversen <jani@apache.org>
2013-08-04 10:43:20 +0000

    cleaned l10ntools, all old tools removed
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/branches/l10n40@1510129 13f79535-47bb-0310-9956-ffa450edef68

--- main/l10ntools/source/makefile.mk
+++ main/l10ntools/source/makefile.mk
@@ -26,7 +26,7 @@ PRJ=..
 INCPRE=$(MISC)
 
 PRJNAME=l10ntools
-TARGET=X40transex
+TARGET=genLang
 TARGETTYPE=CUI
 LIBTARGET=no
 # --- Settings -----------------------------------------------------
@@ -43,178 +43,42 @@ CFLAGS+=-DSYSTEM_EXPAT
 	
 # --- Files --------------------------------------------------------
 
-OBJFILES=   			\
-	$(OBJ)$/export.obj	\
-	$(OBJ)$/export2.obj	\
-	$(OBJ)$/merge.obj   \
-	$(OBJ)$/srciter.obj		\
-	$(OBJ)$/utf8conv.obj	\
-	$(OBJ)$/xmlparse.obj    \
-    $(OBJ)$/helpmerge.obj   \
-    $(OBJ)$/helpex.obj      \
-    $(OBJ)$/file.obj        \
-	$(OBJ)$/directory.obj   
 
+OBJFILES= $(OBJ)$/gLang.obj        \
+          $(OBJ)$/gL10nMem.obj     \
+          $(OBJ)$/gL10nMemDB.obj   \
+          $(OBJ)$/gHandler.obj     \
+          $(OBJ)$/gConProp.obj     \
+          $(OBJ)$/gCon.obj         \
+          $(OBJ)$/gConDB.obj       \
+          $(OBJ)$/gConPoWrap.obj   \
+          $(OBJ)$/gConSrcWrap.obj  \
+          $(OBJ)$/gConXrmWrap.obj  \
+          $(OBJ)$/gConXhpWrap.obj  \
+          $(OBJ)$/gConXcsWrap.obj  \
+          $(OBJ)$/gConXcuWrap.obj  \
+          $(OBJ)$/gConUlfWrap.obj  \
+          $(OBJ)$/gConTreeWrap.obj
 
-LIB1TARGET= $(LB)$/$(TARGET).lib
-LIB1ARCHIV= $(LB)$/libtransex.a
-#LIB1FILES=  $(LB)$/transex3.lib
-LIB1OBJFILES=        $(OBJ)$/export.obj      \
-        $(OBJ)$/export2.obj     \
-        $(OBJ)$/merge.obj   \
-        $(OBJ)$/srciter.obj             \
-        $(OBJ)$/file.obj \
-		$(OBJ)$/directory.obj     \
-        $(OBJ)$/utf8conv.obj    
-   
-
-APP1VERSIONMAP=exports.map
-
-# extractor and merger for *.src and *.hrc
-APP1TARGET=  X40transex3
-#APP1OBJS=   $(OBJ)$/src_yy.obj
-APP1OBJS=   $(OBJ)$/src_yy_wrapper.obj
-
-APP1STDLIBS+= \
-            $(TOOLSLIB) \
-            $(VOSLIB) \
-            $(SALLIB)
-
-.IF "$(OS)"=="MACOSX"
-# static libs at end for OS X
-.ENDIF
-
-APP1LIBS+=	$(LB)$/$(TARGET).lib
-APP1DEPN=   $(OBJ)$/src_yy_wrapper.obj $(LB)$/$(TARGET).lib
-
-APP2TARGET= X40helpex
-APP2OBJS= $(OBJ)$/helpmerge.obj  $(OBJ)$/xmlparse.obj $(OBJ)$/export2.obj $(OBJ)$/utf8conv.obj $(OBJ)$/merge.obj $(OBJ)$/helpex.obj 
-APP2RPATH= NONE
-
-.IF "$(OS)"!="MACOSX"
-.ENDIF
-
-APP2STDLIBS+=$(SALLIB) $(EXPATASCII3RDLIB) $(TOOLSLIB) $(VOSLIB)
-
-.IF "$(OS)"=="MACOSX"
-# static libs at end for OS X
-.ENDIF
-
-# extractor and merger for *.lng and *.lng
-APP3TARGET= X40ulfex
-APP3OBJS=   $(OBJ)$/lngmerge.obj $(OBJ)$/merge.obj $(OBJ)$/export2.obj $(OBJ)$/lngex.obj $(OBJ)$/utf8conv.obj
-APP3RPATH=  NONE
-
-.IF "$(OS)"!="MACOSX"
-#APP3STDLIBS+= $(BTSTRPLIB)
-.ENDIF
-APP3STDLIBS+= \
-            $(TOOLSLIB) \
-            $(VOSLIB) \
-            $(SALLIB)
-.IF "$(OS)"=="MACOSX"
-# static libs at end for OS X
-.ENDIF
-
-# encoding converter for *.gsi
-APP4TARGET= X40gsiconv
-APP4OBJS=   $(OBJ)$/utf8conv.obj $(OBJ)$/gsiconv.obj
-APP4STDLIBS+= \
-            $(TOOLSLIB) \
-            $(VOSLIB) \
-            $(SALLIB)
-
-# tag checker for *.gsi
-APP5TARGET= X40gsicheck
-APP5OBJS=   $(OBJ)$/gsicheck.obj $(OBJ)$/tagtest.obj
-APP5STDLIBS+= \
-            $(TOOLSLIB) \
-            $(VOSLIB) \
-            $(SALLIB)
-
-# extractor and merger for *.cfg
-APP6TARGET= X40cfgex
-APP6OBJS=   $(OBJ)$/cfgmerge.obj $(OBJ)$/cfg_yy_wrapper.obj  $(OBJ)$/merge.obj $(OBJ)$/export2.obj $(OBJ)$/utf8conv.obj
-
-.IF "$(OS)"!="MACOSX"
-#APP6STDLIBS+= $(BTSTRPLIB)
-.ENDIF
-
-APP6STDLIBS+= \
-            $(TOOLSLIB) \
-            $(VOSLIB) \
-            $(SALLIB)
-
-.IF "$(OS)"=="MACOSX"
-# static libs at end for OS X
-.ENDIF
-
-# extractor and merger for *.xrm
-APP7TARGET= X40xrmex
-APP7OBJS=   $(OBJ)$/xrmmerge.obj $(OBJ)$/xrm_yy_wrapper.obj $(OBJ)$/merge.obj $(OBJ)$/export2.obj $(OBJ)$/utf8conv.obj
-APP7RPATH=  NONE
-
-.IF "$(OS)"!="MACOSX"
-.ENDIF
-
-APP7STDLIBS+= \
-            $(TOOLSLIB) \
-            $(VOSLIB) \
-            $(SALLIB)
-
-.IF "$(OS)"=="MACOSX"
-# static libs at end for OS X
-.ENDIF
 
 # localizer for new l10n framework
-APP8TARGET= genLang 
-APP8OBJS= $(OBJ)$/gLang.obj       $(OBJ)$/gL10nMem.obj    \
-          $(OBJ)$/gL10nMemDB.obj  $(OBJ)$/gHandler.obj    \
-          $(OBJ)$/gConProp.obj    $(OBJ)$/gCon.obj        \
-          $(OBJ)$/gConDB.obj                              \
-          $(OBJ)$/gConPoWrap.obj  $(OBJ)$/gConSrcWrap.obj \
-          $(OBJ)$/gConXrmWrap.obj $(OBJ)$/gConXhpWrap.obj \
-          $(OBJ)$/gConXcsWrap.obj $(OBJ)$/gConXcuWrap.obj \
-          $(OBJ)$/gConUlfWrap.obj $(OBJ)$/gConTreeWrap.obj
-APP8RPATH=  NONE
-APP8STDLIBS= 
-APP8LIBS= 
-APP8LINKTYPE=STATIC
-APP8LIBSALCPPRT=
+APP1TARGET=   genLang 
+APP1OBJS=     $(OBJFILES)
+APP1RPATH=    NONE
+APP1LINKTYPE= STATIC
 
+APP1LIBS= 
+APP1STDLIBS= 
+APP1LIBSALCPPRT=
 
-
-
-# localizer for l10n framework
-APP9TARGET= X40localize_sl
-EXCEPTIONSFILES=                            \
-                    $(OBJ)$/localize.obj
-APP9OBJS=   $(OBJ)$/localize.obj $(OBJ)$/utf8conv.obj $(OBJ)$/srciter.obj $(OBJ)$/export2.obj $(OBJ)$/file.obj $(OBJ)$/directory.obj $(OBJ)$/treeconfig.obj $(OBJ)$/inireader.obj
-
-APP9STDLIBS+= \
-            $(TOOLSLIB) \
-            $(VOSLIB) \
-            $(ICUINLIB) \
-			$(ICUUCLIB) \
-			$(STLPORTLIB) \
-			$(SALLIB)
-
-DEPOBJFILES=$(APP1OBJS) $(APP2OBJS) $(APP3OBJS) $(APP4OBJS) $(APP5OBJS) $(APP6OBJS) $(APP7OBJS) $(APP8OBJS) $(APP9OBJS)
 
 # --- Targets ------------------------------------------------------
-
 .INCLUDE :  target.mk
 
 $(MISC)$/%_yy.c : %lex.l
 	flex -l -w -8 -o$@ $<
 
-
-
 # --- Files --------------------------------------------------------
-
-
-# Helper to suppress warnings in lex generated c code, see #i57362#
-
 $(OBJ)$/gConPoWrap.obj:   $(MISC)$/gConPo_yy.c
 $(OBJ)$/gConSrcWrap.obj:  $(MISC)$/gConSrc_yy.c
 $(OBJ)$/gConXcuWrap.obj:  $(MISC)$/gConXcu_yy.c
@@ -223,7 +87,3 @@ $(OBJ)$/gConXrmWrap.obj:  $(MISC)$/gConXrm_yy.c
 $(OBJ)$/gConXhpWrap.obj:  $(MISC)$/gConXhp_yy.c
 $(OBJ)$/gConUlfWrap.obj:  $(MISC)$/gConUlf_yy.c
 $(OBJ)$/gConTreeWrap.obj: $(MISC)$/gConTree_yy.c
-
-$(OBJ)$/src_yy_wrapper.obj: $(MISC)$/src_yy.c
-$(OBJ)$/cfg_yy_wrapper.obj: $(MISC)$/cfg_yy.c
-$(OBJ)$/xrm_yy_wrapper.obj: $(MISC)$/xrm_yy.c
