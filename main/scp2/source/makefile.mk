bb7713dd701f7a06c1837daaedc8cca197ed84a6
Jan Iversen <jani@apache.org>
2013-08-17 15:07:37 +0000

    end of makefiles for second test round.
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/branches/l10n40@1514999 13f79535-47bb-0310-9956-ffa450edef68

---
+++ main/scp2/source/makefile.mk
@@ -0,0 +1,70 @@
+#**************************************************************
+#  
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#  
+#    http://www.apache.org/licenses/LICENSE-2.0
+#  
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+#  
+#**************************************************************
+
+PRJ=..
+PRJNAME=scp2
+TARGET=helper
+
+LANGUAGE_FILELIST="\
+kde/module_kde.ulf,\
+quickstart/module_quickstart.ulf,\
+graphicfilter/module_graphicfilter.ulf,\
+onlineupdate/module_onlineupdate.ulf,\
+impress/folderitem_impress.ulf,\
+impress/registryitem_impress.ulf,\
+impress/module_impress.ulf,\
+impress/module_ogltrans.ulf,\
+sdkoo/module_sdkoo.ulf,\
+writer/folderitem_writer.ulf,\
+writer/registryitem_writer.ulf,\
+writer/module_writer.ulf,\
+math/module_math.ulf,\
+math/folderitem_math.ulf,\
+math/registryitem_math.ulf,\
+javafilter/module_javafilter.ulf,\
+javafilter/registryitem_javafilter.ulf,\
+winexplorerext/module_winexplorerext.ulf,\
+xsltfilter/module_xsltfilter.ulf,\
+draw/registryitem_draw.ulf,\
+draw/folderitem_draw.ulf,\
+draw/module_draw.ulf,\
+calc/folderitem_calc.ulf,\
+calc/registryitem_calc.ulf,\
+calc/module_calc.ulf,\
+ooo/registryitem_ooo.ulf,\
+ooo/module_systemint.ulf,\
+ooo/module_ooo.ulf,\
+ooo/module_java.ulf,\
+ooo/module_headless.ulf,\
+ooo/directory_ooo.ulf,\
+ooo/folderitem_ooo.ulf,\
+ooo/module_langpack.ulf,\
+gnome/module_gnome.ulf,\
+base/folderitem_base.ulf,\
+base/registryitem_base.ulf,\
+base/module_base.ulf,\
+activex/module_activex.ulf,\
+python/module_python_mailmerge.ulf,\
+python/module_python.ulf"
+
+.INCLUDE :	settings.mk
+
+.INCLUDE :	target.mk
---
+++ main/sd/source/makefile.mk
@@ -0,0 +1,90 @@
+#**************************************************************
+#  
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#  
+#    http://www.apache.org/licenses/LICENSE-2.0
+#  
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+#  
+#**************************************************************
+
+PRJ=..
+PRJNAME=sd
+TARGET=helper
+
+LANGUAGE_FILELIST="\
+../util/hidother.src,\
+filter/html/pubdlg.src,\
+ui/dlg/sdpreslt.src,\
+ui/dlg/inspagob.src,\
+ui/dlg/dlgsnap.src,\
+ui/dlg/dlgfield.src,\
+ui/dlg/vectdlg.src,\
+ui/dlg/tabtempl.src,\
+ui/dlg/custsdlg.src,\
+ui/dlg/dlgass.src,\
+ui/dlg/PaneDockingWindow.src,\
+ui/dlg/headerfooterdlg.src,\
+ui/dlg/present.src,\
+ui/dlg/dlgolbul.src,\
+ui/dlg/prltempl.src,\
+ui/dlg/copydlg.src,\
+ui/dlg/paragr.src,\
+ui/dlg/dlg_char.src,\
+ui/dlg/dlgpage.src,\
+ui/dlg/tpaction.src,\
+ui/dlg/brkdlg.src,\
+ui/dlg/masterlayoutdlg.src,\
+ui/dlg/tpoption.src,\
+ui/dlg/morphdlg.src,\
+ui/dlg/navigatr.src,\
+ui/dlg/layeroptionsdlg.src,\
+ui/dlg/ins_paste.src,\
+ui/dlg/LayerDialog.src,\
+ui/dlg/animobjs.src,\
+ui/dlg/prntopts.src,\
+ui/annotations/annotations.src,\
+ui/accessibility/accessibility.src,\
+ui/slideshow/slideshow.src,\
+ui/app/menuids4_tmpl.src,\
+ui/app/app.src,\
+ui/app/popup2_tmpl.src,\
+ui/app/strings.src,\
+ui/app/res_bmp.src,\
+ui/app/menuids_tmpl.src,\
+ui/app/toolbox2_tmpl.src,\
+ui/app/menuids3_tmpl.src,\
+ui/app/menuportal_tmpl.src,\
+ui/app/toolbox.src,\
+ui/app/accelids_tmpl.src,\
+ui/app/menu_tmpl.src,\
+ui/app/popup.src,\
+ui/app/sdstring.src,\
+ui/app/menuids2_tmpl.src,\
+ui/app/tbxids_tmpl.src,\
+ui/animations/CustomAnimationPane.src,\
+ui/animations/CustomAnimationCreateDialog.src,\
+ui/animations/CustomAnimationDialog.src,\
+ui/animations/CustomAnimation.src,\
+ui/animations/SlideTransitionPane.src,\
+ui/slidesorter/shell/SlideSorterChildWindow.src,\
+ui/slidesorter/view/SlsResource.src,\
+ui/view/DocumentRenderer.src,\
+ui/table/TableDesignPane.src,\
+ui/notes/NotesChildWindow.src,\
+core/glob.src"
+
+.INCLUDE :	settings.mk
+
+.INCLUDE :	target.mk
