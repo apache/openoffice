1b904d09dd99e6e0926e26db752c7e5dc50d6303
steve_y <steve_y@13f79535-47bb-0310-9956-ffa450edef68>
2013-12-13 05:20:32 +0000

    fixes for issues checked out by coverity
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/trunk/main@1550645 13f79535-47bb-0310-9956-ffa450edef68

--- svx/source/table/accessibletableshape.cxx
+++ svx/source/table/accessibletableshape.cxx
@@ -328,6 +328,7 @@ void SAL_CALL AccessibleTableShapeImpl::disposing( const EventObject& /*Source*/
 AccessibleTableShape::AccessibleTableShape( const AccessibleShapeInfo& rShapeInfo, const AccessibleShapeTreeInfo& rShapeTreeInfo)
 : AccessibleTableShape_Base(rShapeInfo, rShapeTreeInfo)
 , mxImpl( new AccessibleTableShapeImpl( maShapeTreeInfo ) )
+, mnPreviousSelectionCount(0)
 {
 }
 
@@ -342,8 +343,7 @@ AccessibleTableShape::~AccessibleTableShape (void)
 void AccessibleTableShape::Init()
 {
 	try
-	{
-		mnPreviousSelectionCount = 0;
+	{		
 		Reference< XPropertySet > xSet( mxShape, UNO_QUERY_THROW );
 		Reference< XTable > xTable( xSet->getPropertyValue(C2U("Model")), UNO_QUERY_THROW );
 
