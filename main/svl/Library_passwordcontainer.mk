902261c5330301ee5af7acaae7f620cb2d3d7895
arielch <arielch@13f79535-47bb-0310-9956-ffa450edef68>
2016-10-25 11:47:19 +0000

    i127139 - Work around broken toolchain
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/branches/AOO414@1766518 13f79535-47bb-0310-9956-ffa450edef68

--- main/svl/Library_passwordcontainer.mk
+++ main/svl/Library_passwordcontainer.mk
@@ -37,6 +37,17 @@ $(eval $(call gb_Library_set_include,passwordcontainer,\
 	-I$(OUTDIR)/inc \
 ))
 
+ifeq ($(GUI),UNX)
+ifeq ($(COM),GCC)
+ifeq ($(CPUNAME),INTEL)
+$(eval $(call gb_Library_set_ldflags,passwordcontainer,\
+    $(gb_LinkTarget_LDFLAGS) \
+    -Wl$(COMMA)--start-group -lc -Wl$(COMMA)--end-group \
+))
+endif
+endif
+endif
+
 $(eval $(call gb_Library_add_linked_libs,passwordcontainer,\
 	cppu \
 	cppuhelper \
