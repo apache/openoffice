apache/openoffice

commit b37b109fe99df4c72ec4ef122ab790591557941e

Port main/x11_extensions to gbuild.
Patch by: me


git-svn-id: https://svn.apache.org/repos/asf/openoffice/trunk@1772599 13f79535-47bb-0310-9956-ffa450edef68

main/x11_extensions/Package_inc.mk
@@ -21,55 +21,11 @@
 
 
 
-$(eval $(call gb_Module_Module,ooo))
+$(eval $(call gb_Package_Package,x11_extensions_inc,$(SRCDIR)/x11_extensions))
 
-$(eval $(call gb_Module_add_moduledirs,ooo,\
-	MathMLDTD \
-	basebmp \
-	basegfx \
-	canvas \
-	comphelper \
-	cppcanvas \
-	dbaccess \
-	drawinglayer \
-	editeng \
-	fileaccess \
-	formula \
-	framework \
-	idl \
-	io \
-	linguistic \
-	o3tl \
-	offapi \
-	oovbaapi \
-	oox \
-	padmin \
-	package \
-	reportdesign \
-	sax \
-	sd \
-	sfx2 \
-	sot \
-	starmath \
-	svgio \
-	svl \
-	svtools \
-	svx \
-	sw \
-	toolkit \
-	tools \
-	ucbhelper \
-	udkapi \
-	unotools \
-	unoxml \
-	vbahelper \
-	vcl \
-	wizards \
-	writerfilter \
-	x11_extensions \
-	xmloff \
-	xmlreader \
-	xmlscript \
-))
-
-# vim: set noet ts=4 sw=4:
+$(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/Xrandr,inc/Xrandr.h))
+$(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/Xrender,inc/Xrender.h))
+$(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/randr.h,inc/randr.h))
+$(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/randrproto.h,inc/randrproto.h))
+$(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/render.h,inc/render.h))
+$(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/renderproto.h,inc/renderproto.h))

main/x11_extensions/Makefile
@@ -19,57 +19,14 @@
 #  
 #**************************************************************
 
+ifeq ($(strip $(SOLARENV)),)
+$(error No environment set!)
+endif
 
+gb_PARTIALBUILD := T
+GBUILDDIR := $(SOLARENV)/gbuild
+include $(GBUILDDIR)/gbuild.mk
 
-$(eval $(call gb_Module_Module,ooo))
+$(eval $(call gb_Module_make_global_targets,$(shell ls $(dir $(realpath $(firstword $(MAKEFILE_LIST))))/Module*.mk)))
 
-$(eval $(call gb_Module_add_moduledirs,ooo,\
-	MathMLDTD \
-	basebmp \
-	basegfx \
-	canvas \
-	comphelper \
-	cppcanvas \
-	dbaccess \
-	drawinglayer \
-	editeng \
-	fileaccess \
-	formula \
-	framework \
-	idl \
-	io \
-	linguistic \
-	o3tl \
-	offapi \
-	oovbaapi \
-	oox \
-	padmin \
-	package \
-	reportdesign \
-	sax \
-	sd \
-	sfx2 \
-	sot \
-	starmath \
-	svgio \
-	svl \
-	svtools \
-	svx \
-	sw \
-	toolkit \
-	tools \
-	ucbhelper \
-	udkapi \
-	unotools \
-	unoxml \
-	vbahelper \
-	vcl \
-	wizards \
-	writerfilter \
-	x11_extensions \
-	xmloff \
-	xmlreader \
-	xmlscript \
-))
-
-# vim: set noet ts=4 sw=4:
+# vim: set noet sw=4 ts=4:

main/x11_extensions/Module_x11_extensions.mk
@@ -21,55 +21,12 @@
 
 
 
-$(eval $(call gb_Module_Module,ooo))
+$(eval $(call gb_Module_Module,x11_extensions))
 
-$(eval $(call gb_Module_add_moduledirs,ooo,\
-	MathMLDTD \
-	basebmp \
-	basegfx \
-	canvas \
-	comphelper \
-	cppcanvas \
-	dbaccess \
-	drawinglayer \
-	editeng \
-	fileaccess \
-	formula \
-	framework \
-	idl \
-	io \
-	linguistic \
-	o3tl \
-	offapi \
-	oovbaapi \
-	oox \
-	padmin \
-	package \
-	reportdesign \
-	sax \
-	sd \
-	sfx2 \
-	sot \
-	starmath \
-	svgio \
-	svl \
-	svtools \
-	svx \
-	sw \
-	toolkit \
-	tools \
-	ucbhelper \
-	udkapi \
-	unotools \
-	unoxml \
-	vbahelper \
-	vcl \
-	wizards \
-	writerfilter \
-	x11_extensions \
-	xmloff \
-	xmlreader \
-	xmlscript \
+ifeq ($(GUIBASE),unx)
+$(eval $(call gb_Module_add_targets,x11_extensions,\
+	Package_inc \
 ))
+endif
 
-# vim: set noet ts=4 sw=4:
+# vim: set noet sw=4 ts=4:

main/x11_extensions/prj/build.lst
@@ -1,3 +1,3 @@
 x1  x11_extensions : solenv NULL
-x1  x11_extensions                         usr1    -   u     x11_mkout NULL
+x1  x11_extensions\prj                         nmake    -   all     x11_prj NULL
 

main/x11_extensions/prj/d.lst
@@ -1,5 +1,0 @@
-mkdir: %_DEST%\inc%_EXT%\X11
-mkdir: %_DEST%\inc%_EXT%\X11\extensions
-
-..\inc\*.h %_DEST%\inc%_EXT%\X11\extensions\*.h
-

main/x11_extensions/prj/makefile.mk
@@ -21,55 +21,24 @@
 
 
 
-$(eval $(call gb_Module_Module,ooo))
+PRJ=..
+TARGET=prj
 
-$(eval $(call gb_Module_add_moduledirs,ooo,\
-	MathMLDTD \
-	basebmp \
-	basegfx \
-	canvas \
-	comphelper \
-	cppcanvas \
-	dbaccess \
-	drawinglayer \
-	editeng \
-	fileaccess \
-	formula \
-	framework \
-	idl \
-	io \
-	linguistic \
-	o3tl \
-	offapi \
-	oovbaapi \
-	oox \
-	padmin \
-	package \
-	reportdesign \
-	sax \
-	sd \
-	sfx2 \
-	sot \
-	starmath \
-	svgio \
-	svl \
-	svtools \
-	svx \
-	sw \
-	toolkit \
-	tools \
-	ucbhelper \
-	udkapi \
-	unotools \
-	unoxml \
-	vbahelper \
-	vcl \
-	wizards \
-	writerfilter \
-	x11_extensions \
-	xmloff \
-	xmlreader \
-	xmlscript \
-))
+.INCLUDE : settings.mk
 
-# vim: set noet ts=4 sw=4:
+.IF "$(VERBOSE)"!=""
+VERBOSEFLAG :=
+.ELSE
+VERBOSEFLAG := -s
+.ENDIF
+
+.IF "$(DEBUG)"!=""
+DEBUG_ARGUMENT=DEBUG=$(DEBUG)
+.ELIF "$(debug)"!=""
+DEBUG_ARGUMENT=debug=$(debug)
+.ELSE
+DEBUG_ARGUMENT=
+.ENDIF
+
+all:
+	cd $(PRJ) && $(GNUMAKE) $(VERBOSEFLAG) -r -j$(MAXPROCESS) $(gb_MAKETARGET) $(DEBUG_ARGUMENT) && $(GNUMAKE) $(VERBOSEFLAG) -r deliverlog

commit 6a487aaed6fc42cc8aac57bc44af22310950405d

Fix filename problems in main/x11_extensions introduced in r1772599.
Patch by: me


git-svn-id: https://svn.apache.org/repos/asf/openoffice/trunk@1772804 13f79535-47bb-0310-9956-ffa450edef68

main/x11_extensions/Package_inc.mk
@@ -23,8 +23,8 @@
 
 $(eval $(call gb_Package_Package,x11_extensions_inc,$(SRCDIR)/x11_extensions))
 
-$(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/Xrandr,inc/Xrandr.h))
-$(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/Xrender,inc/Xrender.h))
+$(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/Xrandr.h,inc/Xrandr.h))
+$(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/Xrender.h,inc/Xrender.h))
 $(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/randr.h,inc/randr.h))
 $(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/randrproto.h,inc/randrproto.h))
 $(eval $(call gb_Package_add_file,x11_extensions_inc,inc/X11/extensions/render.h,inc/render.h))