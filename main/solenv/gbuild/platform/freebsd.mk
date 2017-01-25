apache/openoffice

commit e15a45347c4af89b363e7d092ae18e347f2f052f

Update endianness detection on FreeBSD.
This produces a working AOO on FreeBSD PowerPC64!

This is related to i126615 which was also fixed by Curtis a while ago.
Kudos for both contributions, makes me want to get a PowerPC to run
FreeBSD + AOO on it!

Author:	Curtis Hamilton -- cmhamilto at gmail


git-svn-id: https://svn.apache.org/repos/asf/openoffice/trunk@1773166 13f79535-47bb-0310-9956-ffa450edef68

main/solenv/gbuild/platform/freebsd.mk
@@ -36,6 +36,10 @@ ifeq ($(CPU),X)
 CPUNAME := X86_64
 endif
 
+ifeq ($(CPU),P)
+CPUNAME := POWERPC64
+endif
+
 # use CC/CXX if they are nondefaults
 ifneq ($(origin CC),default)
 gb_CC := $(CC)
@@ -66,6 +70,12 @@ ifeq ($(CPUNAME),X86_64)
 gb_CPUDEFS := -D$(CPUNAME)
 else
 gb_CPUDEFS := -DX86
+endif
+
+ifeq ($(CPUNAME),POWERPC64)
+gb_CPUDEFS := -D$(CPUNAME)
+else
+gb_CPUDEFS := -DPOWERPC64
 endif
 
 gb_CFLAGS := \

main/bridges/source/cpp_uno/gcc3_freebsd_powerpc64/makefile.mk
@@ -34,7 +34,7 @@ ENABLE_EXCEPTIONS=TRUE
 
 # --- Files --------------------------------------------------------
 
-.IF "$(COM)$(OS)$(CPU)$(COMNAME)$(CPUNAME)" == "GCCLINUXPgcc3POWERPC64"
+.IF "$(COM)$(OS)$(CPU)$(COMNAME)$(CPUNAME)" == "GCCFREEBSDPgcc3POWERPC64"
 
 .IF "$(cppu_no_leak)" == ""
 CFLAGS += -DLEAK_STATIC_DATA

main/bridges/source/cpp_uno/gcc3_freebsd_powerpc64/uno2cpp.cxx
@@ -24,7 +24,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_bridges.hxx"
 
-#include <malloc.h>
+#include <stdlib.h>
 
 #include <com/sun/star/uno/genfunc.hxx>
 #include <uno/data.h>

main/desktop/source/deployment/misc/dp_platform.cxx
@@ -57,6 +57,7 @@
 #define PLATFORM_SOLARIS_SPARC64    "solaris_sparc64"
 #define PLATFORM_SOLARIS_X86        "solaris_x86"
 #define PLATFORM_FREEBSD_POWERPC    "freebsd_powerpc"
+#define PLATFORM_FREEBSD_POWERPC64  "freebsd_powerpc64"
 #define PLATFORM_FREEBSD_X86        "freebsd_x86"
 #define PLATFORM_FREEBSD_X86_64     "freebsd_x86_64"
 #define PLATFORM_MACOSX_X86         "macosx_x86"
@@ -135,6 +136,10 @@ namespace
             ret = checkOSandCPU(OUSTR("kFreeBSD"), OUSTR("X86_64"));  
         else if (token.equals(OUSTR(PLATFORM_LINUX_SPARC)))
             ret = checkOSandCPU(OUSTR("Linux"), OUSTR("SPARC"));  
+        else if (token.equals(OUSTR(PLATFORM_FREEBSD_POWERPC)))
+            ret = checkOSandCPU(OUSTR("FreeBSD"), OUSTR("PowerPC"));  
+        else if (token.equals(OUSTR(PLATFORM_FREEBSD_POWERPC64)))
+            ret = checkOSandCPU(OUSTR("FreeBSD"), OUSTR("PowerPC64"));  
         else if (token.equals(OUSTR(PLATFORM_LINUX_POWERPC)))
             ret = checkOSandCPU(OUSTR("Linux"), OUSTR("PowerPC"));  
         else if (token.equals(OUSTR(PLATFORM_LINUX_POWERPC64)))

main/odk/settings/platform.mk
@@ -34,6 +34,9 @@ else
 	ifeq "$(UNOPKG_PLATFORM)" "Linux_PowerPC"
 		EXTENSION_PLATFORM=linux_powerpc
 	else
+	ifeq "$(UNOPKG_PLATFORM)" "FreeBSD_PowerPC64"
+		EXTENSION_PLATFORM=freebsd_powerpc64
+	else
 	ifeq "$(UNOPKG_PLATFORM)" "Linux_x86"
 		EXTENSION_PLATFORM=linux_x86
 	else

main/odk/settings/settings.mk
@@ -507,12 +507,19 @@ UNOPKG_PLATFORM=FreeBSD_x86_64
 else
 UNOPKG_PLATFORM=FreeBSD_x86
 endif
+ifeq "$(PROCTYPE)" "powerpc"
+UNOPKG_PLATFORM=FreeBSD_ppc
+endif
 endif
 
 ifeq "$(PROCTYPE)" "x86_64"
 JAVA_PROC_TYPE=amd64
 else
 JAVA_PROC_TYPE=i386
+endif
+
+ifeq "$(PROCTYPE)" "powerpc64"
+JAVA_PROC_TYPE=ppc64
 endif
 
 ifeq (kfreebsd,$(findstring kfreebsd,$(PLATFORM)))

main/sal/inc/osl/endian.h
@@ -73,15 +73,13 @@ extern "C" {
 #ifdef FREEBSD
 #   include <sys/param.h>
 #   include <machine/endian.h>
-#if __FreeBSD_version < 500000
 #   if BYTE_ORDER == LITTLE_ENDIAN
-#   	define _LITTLE_ENDIAN
+#   	undef _BIG_ENDIAN
 #   elif BYTE_ORDER == BIG_ENDIAN
-#   	define _BIG_ENDIAN
+#   	undef _LITTLE_ENDIAN
 #   elif BYTE_ORDER == PDP_ENDIAN
 #   	define _PDP_ENDIAN
 #   endif
-#endif
 #endif
 
 #ifdef SCO

main/sal/osl/unx/system.h
@@ -126,6 +126,8 @@
 
 #endif
 
+#include <osl/endian.h>
+
 #ifdef NETBSD
 #	define  ETIME ETIMEDOUT
 #	define _POSIX_THREAD_SYSCALL_SOFT 1
@@ -176,15 +178,13 @@
 #   include <netinet/tcp.h>
 #	define 	IORESOURCE_TRANSFER_BSD 
 #   include <machine/endian.h>
-#if __FreeBSD_version < 500000
 #   if BYTE_ORDER == LITTLE_ENDIAN
-#   	define _LITTLE_ENDIAN
+#   	undef _BIG_ENDIAN
 #   elif BYTE_ORDER == BIG_ENDIAN
-#   	define _BIG_ENDIAN
+#   	undef _LITTLE_ENDIAN
 #   elif BYTE_ORDER == PDP_ENDIAN
 #   	define _PDP_ENDIAN
 #   endif
-#endif
 #	define  NO_PTHREAD_RTL
 #endif
 

commit 8f07f855b27ed7bcc010af365ebc4fea86de7182

Fix a FreeBSD regression in the gbuild gb_CPUDEFS variable caused by r1773166, where it was always set to POWERPC64 due to a swapped if-else.
This needs further work, on all platforms!

Patch by: me



git-svn-id: https://svn.apache.org/repos/asf/openoffice/trunk@1780134 13f79535-47bb-0310-9956-ffa450edef68

main/solenv/gbuild/platform/freebsd.mk
@@ -68,14 +68,10 @@ gb_COMPILERDEFS := \
 
 ifeq ($(CPUNAME),X86_64)
 gb_CPUDEFS := -D$(CPUNAME)
-else
-gb_CPUDEFS := -DX86
-endif
-
-ifeq ($(CPUNAME),POWERPC64)
+else ifeq ($(CPUNAME),POWERPC64)
 gb_CPUDEFS := -D$(CPUNAME)
 else
-gb_CPUDEFS := -DPOWERPC64
+gb_CPUDEFS := -DX86
 endif
 
 gb_CFLAGS := \