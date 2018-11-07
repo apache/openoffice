916fc1775232f14fd6100b1abf40e3e265ebde2d
Matthias Seidel <mseidel@apache.org>
2018-11-07 16:16:22 +0000

    Fixed typo, deleted whitespace
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/trunk@1846052 13f79535-47bb-0310-9956-ffa450edef68

--- main/extensions/test/ole/MfcControl/StdAfx.h
+++ main/extensions/test/ole/MfcControl/StdAfx.h
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 #if !defined(AFX_STDAFX_H__AC221FBA_A0D8_11D4_833B_005004526AB4__INCLUDED_)
@@ -27,20 +27,20 @@
 #endif // _MSC_VER > 1000
 
 // stdafx.h : include file for standard system include files,
-//      or project specific include files that are used frequently,
-//      but are changed infrequently
+// or project specific include files that are used frequently,
+// but are changed infrequently
 
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
-#include <afxctl.h>         // MFC support for ActiveX Controls
-#include <afxext.h>         // MFC extensions
-#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Comon Controls
+#include <afxctl.h>			// MFC support for ActiveX Controls
+#include <afxext.h>			// MFC extensions
+#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
 #ifndef _AFX_NO_AFXCMN_SUPPORT
 #include <afxcmn.h>			// MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
 // Delete the two includes below if you do not wish to use the MFC
-//  database classes
+// database classes
 #include <afxdb.h>			// MFC database classes
 #include <afxdao.h>			// MFC DAO database classes
 
