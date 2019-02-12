69456848cbf33066456424bee4920cf3cbcda233
mseidel <mseidel@unknown>
2019-02-12 12:52:01 +0000

    Removed whitespace, fixed typos
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/trunk@1853432 13f79535-47bb-0310-9956-ffa450edef68

--- main/bean/java/officebean/src/main/java/com/sun/star/comp/beans/OfficeConnection.java
+++ main/bean/java/officebean/src/main/java/com/sun/star/comp/beans/OfficeConnection.java
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -29,10 +29,10 @@ import com.sun.star.lang.XComponent;
 import com.sun.star.uno.XComponentContext;
 
 /**
- * This abstract class reprecents a connection to the office 
+ * This abstract class represents a connection to the office
  * application.
 
-    @deprecated
+	@deprecated
  */
 public interface OfficeConnection
 	extends XComponent
@@ -46,17 +46,17 @@ public interface OfficeConnection
 		throws java.net.MalformedURLException;
 
 	/**
-	 * Sets an AWT container catory.
+	 * Sets an AWT container factory.
 	 *
-	 * @param containerFactory This is a application provided AWT container 
+	 * @param containerFactory This is a application provided AWT container
 	 *	factory.
 	 */
 	void setContainerFactory(ContainerFactory containerFactory);
 
 	/**
-	 * Retrives the UNO component context.
-	 * Establishes a connection if necessary and initialises the  
-	 * UNO service manager if it has not already been initialised.
+	 * Retrieves the UNO component context.
+	 * Establishes a connection if necessary and initializes the
+	 * UNO service manager if it has not already been initialized.
 	 *
 	 * @return The office UNO component context.
 	 */
@@ -64,10 +64,10 @@ public interface OfficeConnection
 
 	/**
 	 * Creates an office window.
-	 * The window is either a sub-class of java.awt.Canvas (local) or 
-	 * java.awt.Container (RVP). 
-	 * 
-	 * This method does not add add the office window to its container.
+	 * The window is either a sub-class of java.awt.Canvas (local) or
+	 * java.awt.Container (RVP).
+	 *
+	 * This method does not add the office window to its container.
 	 *
 	 * @param container This is an AWT container.
 	 * @return The office window instance.
