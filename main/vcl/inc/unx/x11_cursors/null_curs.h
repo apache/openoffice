1b03b6a45cdb4f17c31a964595a595fc8ae877ec
Matthias Seidel <mseidel@apache.org>
2019-05-02 11:13:38 +0000

    Merged r1858284, r1858381 and r1858382 from trunk
     - Reformated Hex-Dumps for better "readability"
     - Fixed missing line
     - Fixed error introduced in my last commit
    
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/branches/AOO42X@1858528 13f79535-47bb-0310-9956-ffa450edef68

--- main/vcl/inc/unx/x11_cursors/null_curs.h
+++ main/vcl/inc/unx/x11_cursors/null_curs.h
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -24,4 +24,5 @@
 #define nullcurs_height 4
 #define nullcurs_x_hot 2 
 #define nullcurs_y_hot 2
-static const unsigned char nullcurs_bits[] = { 0x00, 0x00 };
+
+static const unsigned char nullcurs_bits[] = {0x00,0x00};
--- main/vcl/inc/unx/x11_cursors/tblselw_mask.h
+++ main/vcl/inc/unx/x11_cursors/tblselw_mask.h
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,21 +7,38 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
+
 
 #define tblselw_mask_width 16
 #define tblselw_mask_height 16
+#define tblselw_mask_x_hot 1
+#define tblselw_mask_y_hot 8
+
 static const unsigned char tblselw_mask_bits[] = {
-   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x60, 0x00, 0x70, 0x00, 0x78, 0x00,
-   0xfc, 0x7f, 0xfe, 0x7f, 0xff, 0x7f, 0xfe, 0x7f, 0xfc, 0x7f, 0x78, 0x00,
-   0x70, 0x00, 0x60, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	0x00,0x00,
+	0x00,0x00,
+	0x00,0x00,
+	0x60,0x00,
+	0x70,0x00,
+	0x78,0x00,
+	0xfc,0x7f,
+	0xfe,0x7f,
+	0xff,0x7f,
+	0xfe,0x7f,
+	0xfc,0x7f,
+	0x78,0x00,
+	0x70,0x00,
+	0x60,0x00,
+	0x00,0x00,
+	0x00,0x00};
