b87f12bb81d7d34bb81a54b5c5528c145f92cca6
Matthias Seidel <mseidel@apache.org>
2019-05-12 14:17:46 +0000

    OpenOffice Calc offers functions for converting from old national currencies to EURO.
    Additional countries joined the euro area: Estonia (2011), Latvia (2014) and Lithuania (2015), see:
    https://www.ecb.europa.eu/euro/changeover/estonia/html/index.en.html
    https://www.ecb.europa.eu/euro/changeover/latvia/html/index.en.html
    https://www.ecb.europa.eu/euro/changeover/lithuania/html/index.en.html
    
    This patch adds those currencies and conversion rates for CONVERT (Calc.xcu) and EUROCONVERT (interpr2.cxx).
    
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/trunk@1859148 13f79535-47bb-0310-9956-ffa450edef68

--- main/sc/source/core/tool/interpr2.cxx
+++ main/sc/source/core/tool/interpr2.cxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -2668,7 +2668,10 @@ sal_Bool lclConvertMoney( const String& aSearchUnit, double& rfRate, int& rnDec 
         { "SIT", 239.640,  2 },
         { "MTL", 0.429300, 2 },
         { "CYP", 0.585274, 2 },
-        { "SKK", 30.1260,  2 }
+        { "SKK", 30.1260,  2 },
+        { "EEK", 15.6466,  2 },
+        { "LVL", 0.702804, 2 },
+        { "LTL", 3.45280,  2 }
     };
 
     const size_t nConversionCount = sizeof( aConvertTable ) / sizeof( aConvertTable[0] );
