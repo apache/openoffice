70c44df84b35db1ca478f6cf4e330381bdb3b202
Matthias Seidel <mseidel@apache.org>
2019-05-19 11:15:04 +0000

    Fixed typos
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/trunk@1859494 13f79535-47bb-0310-9956-ffa450edef68

--- main/framework/inc/threadhelp/transactionbase.hxx
+++ main/framework/inc/threadhelp/transactionbase.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -53,11 +53,11 @@ namespace framework{
 //_________________________________________________________________________________________________________________
 
 /*-************************************************************************************************************//**
-    @short          make it possible to instanciate a transacion manager as first member!
-    @descr          If you use a transaction manager as a member of your class and whish to use it earlier then other ones
-					you should have a look on this implementation. You must use it as the first base class
+    @short          make it possible to instantiate a transaction manager as first member!
+    @descr          If you use a transaction manager as a member of your class and wish to use it earlier than other ones
+					you should have a look at this implementation. You must use it as the first base class
 					of your implementation - because base classes are initialized by his order and before your
-                    member! Thats why ist a good place to declare this member.
+                    member! That's why it is a good place to declare this member.
 
 	@implements		-
 	@base			-
