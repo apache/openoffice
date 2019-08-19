b63d80d5c1e43f7d6447ac0bbc280a0c8d1cdc30
Damjan Jovanovic <damjan@apache.org>
2019-08-19 20:00:02 +0200

    If the vendor is unknown, treat it as OpenJDK, as OpenJDK now allows every builder to set its own vendor string and we can't use that to identify them any more.
    Patch by: me
    
    (cherry picked from commit 61a4f434029376f30410cf27dbdd93c1f6011f21)

--- main/jvmfwk/plugins/sunmajor/javaenvsetup/javaldx.cxx
+++ main/jvmfwk/plugins/sunmajor/javaenvsetup/javaldx.cxx
@@ -121,6 +121,7 @@ SAL_IMPLEMENT_MAIN_WITH_ARGS(argc, argv)
     rtl::OUString sVendor7(RTL_CONSTASCII_USTRINGPARAM("Free Software Foundation, Inc."));
     rtl::OUString sVendor8(RTL_CONSTASCII_USTRINGPARAM("The FreeBSD Foundation"));
     rtl::OUString sVendor9(RTL_CONSTASCII_USTRINGPARAM("AdoptOpenJDK"));
+    rtl::OUString sVendor10(RTL_CONSTASCII_USTRINGPARAM("OpenJDK"));
     if ( ! (sVendor0.equals(pInfo->sVendor) == sal_True
             || sVendor1.equals(pInfo->sVendor) == sal_True
             || sVendor2.equals(pInfo->sVendor) == sal_True
@@ -130,7 +131,8 @@ SAL_IMPLEMENT_MAIN_WITH_ARGS(argc, argv)
             || sVendor6.equals(pInfo->sVendor) == sal_True
             || sVendor7.equals(pInfo->sVendor) == sal_True
             || sVendor8.equals(pInfo->sVendor) == sal_True
-            || sVendor9.equals(pInfo->sVendor) == sal_True))
+            || sVendor9.equals(pInfo->sVendor) == sal_True
+            || sVendor10.equals(pInfo->sVendor) == sal_True))
         return 0;
     
     rtl::OString sPaths = getLD_LIBRARY_PATH(pInfo->arVendorData);
