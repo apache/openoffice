055aaed46ae13807e8ff8b5002b11ec66f578b5d
mseidel <mseidel@apache.org>
2019-08-22 00:53:18 +0200

    Removed whitespace, fixed typos

--- main/xmloff/inc/xmloff/xmlimppr.hxx
+++ main/xmloff/inc/xmloff/xmlimppr.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,17 +7,18 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
+
 
 
 #ifndef _XMLOFF_XMLIMPPR_HXX
@@ -56,30 +57,30 @@ class SvXMLImport;
  */
 struct _ContextID_Index_Pair
 {
-    sal_Int16 nContextID;
-    sal_Int32 nIndex;
+	sal_Int16 nContextID;
+	sal_Int32 nIndex;
 };
 
 class XMLOFF_DLLPUBLIC SvXMLImportPropertyMapper : public UniRefBase
 {
 	UniReference< SvXMLImportPropertyMapper> mxNextMapper;
 
-    SvXMLImport& rImport;   // access to error handling
+	SvXMLImport& rImport; // access to error handling
 
-    SAL_DLLPRIVATE SvXMLImportPropertyMapper(SvXMLImportPropertyMapper &);
-        // not defined
-    SAL_DLLPRIVATE void operator =(SvXMLImportPropertyMapper &); // not defined
+	SAL_DLLPRIVATE SvXMLImportPropertyMapper(SvXMLImportPropertyMapper &);
+		// not defined
+	SAL_DLLPRIVATE void operator =(SvXMLImportPropertyMapper &); // not defined
 
 protected:
 
 	UniReference< XMLPropertySetMapper > maPropMapper;
-    SvXMLImport& GetImport() const { return rImport;}
+	SvXMLImport& GetImport() const { return rImport;}
 
 public:
 
 	SvXMLImportPropertyMapper(
 			const UniReference< XMLPropertySetMapper >& rMapper,
-            SvXMLImport& rImport);
+			SvXMLImport& rImport);
 	virtual ~SvXMLImportPropertyMapper();
 
 	// Add a ImportPropertyMapper at the end of the import mapper chain.
@@ -91,18 +92,18 @@ public:
 	/** fills the given itemset with the attributes in the given list */
 	void importXML(
 			::std::vector< XMLPropertyState >& rProperties,
-		    ::com::sun::star::uno::Reference<
+			::com::sun::star::uno::Reference<
 					::com::sun::star::xml::sax::XAttributeList > xAttrList,
 			const SvXMLUnitConverter& rUnitConverter,
 			const SvXMLNamespaceMap& rNamespaceMap,
-		    sal_uInt32 nPropType	) const;
+			sal_uInt32 nPropType	) const;
 
-	/** like above, except that the mart is only serached within the range
-	  *  [nStartIdx, nEndIdx[
-	  */
+	/** like above, except that the map is only searched within the range
+	 *  [nStartIdx, nEndIdx[
+	 */
 	void importXML(
 			::std::vector< XMLPropertyState >& rProperties,
-		    ::com::sun::star::uno::Reference<
+			::com::sun::star::uno::Reference<
 					::com::sun::star::xml::sax::XAttributeList > xAttrList,
 			const SvXMLUnitConverter& rUnitConverter,
 			const SvXMLNamespaceMap& rNamespaceMap,
@@ -117,7 +118,7 @@ public:
 			const SvXMLUnitConverter& rUnitConverter,
 			const SvXMLNamespaceMap& rNamespaceMap ) const;
 
-	/** This method is called when all attributes have benn processed. It may be used to remove items that are incomplete */
+	/** This method is called when all attributes have been processed. It may be used to remove items that are incomplete */
 	virtual void finished(
 			::std::vector< XMLPropertyState >& rProperties,
 			sal_Int32 nStartIndex, sal_Int32 nEndIndex ) const;
@@ -126,75 +127,75 @@ public:
 			const ::std::vector< XMLPropertyState >& aProperties,
 			const ::com::sun::star::uno::Reference<
 					::com::sun::star::beans::XPropertySet > rPropSet,
-        	_ContextID_Index_Pair* pSpecialContextIds ) const;
+			_ContextID_Index_Pair* pSpecialContextIds ) const;
 
-    sal_Bool FillPropertySet(
+	sal_Bool FillPropertySet(
 			const ::std::vector< XMLPropertyState >& aProperties,
 			const ::com::sun::star::uno::Reference<
 					::com::sun::star::beans::XPropertySet > rPropSet,
-        	_ContextID_Index_Pair* pSpecialContextIds = NULL ) const;
+			_ContextID_Index_Pair* pSpecialContextIds = NULL ) const;
 
 	void FillPropertySequence(
 			const ::std::vector< XMLPropertyState >& aProperties,
-            ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& rValues )
-            const;
+			::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& rValues )
+			const;
 
 	inline const UniReference< XMLPropertySetMapper >&
 		getPropertySetMapper() const;
 
 
 
-    /** implementation helper for FillPropertySet: fill an XPropertySet.
-     * Exceptions will be asserted. */
+	/** implementation helper for FillPropertySet: fill an XPropertySet.
+	 *  Exceptions will be asserted. */
 	static sal_Bool _FillPropertySet(
-        const ::std::vector<XMLPropertyState> & rProperties,
-        const ::com::sun::star::uno::Reference<
-            ::com::sun::star::beans::XPropertySet> & rPropSet,
-        const ::com::sun::star::uno::Reference<
-            ::com::sun::star::beans::XPropertySetInfo> & rPropSetInfo,
-        const UniReference<XMLPropertySetMapper> & rPropMapper,
-        SvXMLImport& rImport,
+		const ::std::vector<XMLPropertyState> & rProperties,
+		const ::com::sun::star::uno::Reference<
+			::com::sun::star::beans::XPropertySet> & rPropSet,
+		const ::com::sun::star::uno::Reference<
+			::com::sun::star::beans::XPropertySetInfo> & rPropSetInfo,
+		const UniReference<XMLPropertySetMapper> & rPropMapper,
+		SvXMLImport& rImport,
 
-        // parameter for use by txtstyli.cxx; allows efficient
-        // catching the combined characters property
-        _ContextID_Index_Pair* pSpecialContextIds = NULL );
+		// parameter for use by txtstyli.cxx; allows efficient
+		// catching the combined characters property
+		_ContextID_Index_Pair* pSpecialContextIds = NULL );
 
-    /** implementation helper for FillPropertySet: fill an XMultiPropertySet.
-     * If unsuccessul, set return value. */
+	/** implementation helper for FillPropertySet: fill an XMultiPropertySet.
+	 *  If unsuccessful, set return value. */
 	static sal_Bool _FillMultiPropertySet(
-        const ::std::vector<XMLPropertyState> & rProperties,
-        const ::com::sun::star::uno::Reference<
-            ::com::sun::star::beans::XMultiPropertySet> & rMultiPropSet,
-        const ::com::sun::star::uno::Reference<
-            ::com::sun::star::beans::XPropertySetInfo> & rPropSetInfo,
-        const UniReference<XMLPropertySetMapper> & rPropMapper,
+		const ::std::vector<XMLPropertyState> & rProperties,
+		const ::com::sun::star::uno::Reference<
+			::com::sun::star::beans::XMultiPropertySet> & rMultiPropSet,
+		const ::com::sun::star::uno::Reference<
+			::com::sun::star::beans::XPropertySetInfo> & rPropSetInfo,
+		const UniReference<XMLPropertySetMapper> & rPropMapper,
 
-        // parameters for use by txtstyli.cxx; allows efficient
-        // catching the combined characters property
-        _ContextID_Index_Pair* pSpecialContextIds = NULL );
+		// parameters for use by txtstyli.cxx; allows efficient
+		// catching the combined characters property
+		_ContextID_Index_Pair* pSpecialContextIds = NULL );
 
-    /** implementation helper for FillPropertySet: fill an XTolerantMultiPropertySet.
-     * If unsuccessul, set return value. */
+	/** implementation helper for FillPropertySet: fill an XTolerantMultiPropertySet.
+	 *  If unsuccessful, set return value. */
 	static sal_Bool _FillTolerantMultiPropertySet(
-        const ::std::vector<XMLPropertyState> & rProperties,
-        const ::com::sun::star::uno::Reference<
-            ::com::sun::star::beans::XTolerantMultiPropertySet> & rTolPropSet,
-        const UniReference<XMLPropertySetMapper> & rPropMapper,
-        SvXMLImport& rImport,
+		const ::std::vector<XMLPropertyState> & rProperties,
+		const ::com::sun::star::uno::Reference<
+			::com::sun::star::beans::XTolerantMultiPropertySet> & rTolPropSet,
+		const UniReference<XMLPropertySetMapper> & rPropMapper,
+		SvXMLImport& rImport,
 
-        // parameters for use by txtstyli.cxx; allows efficient
-        // catching the combined characters property
-        _ContextID_Index_Pair* pSpecialContextIds = NULL );
+		// parameters for use by txtstyli.cxx; allows efficient
+		// catching the combined characters property
+		_ContextID_Index_Pair* pSpecialContextIds = NULL );
 
 
-    static void _PrepareForMultiPropertySet(
-        const ::std::vector<XMLPropertyState> & rProperties,
-        const ::com::sun::star::uno::Reference<
-            ::com::sun::star::beans::XPropertySetInfo> & rPropSetInfo,
-        const UniReference<XMLPropertySetMapper> & rPropMapper,
-        _ContextID_Index_Pair* pSpecialContextIds,
-        ::com::sun::star::uno::Sequence< ::rtl::OUString >& rNames,
-        ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >& rValues);
+	static void _PrepareForMultiPropertySet(
+		const ::std::vector<XMLPropertyState> & rProperties,
+		const ::com::sun::star::uno::Reference<
+			::com::sun::star::beans::XPropertySetInfo> & rPropSetInfo,
+		const UniReference<XMLPropertySetMapper> & rPropMapper,
+		_ContextID_Index_Pair* pSpecialContextIds,
+		::com::sun::star::uno::Sequence< ::rtl::OUString >& rNames,
+		::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >& rValues);
 };
 
 
@@ -204,4 +205,4 @@ inline const UniReference< XMLPropertySetMapper >&
 	return maPropMapper;
 }
 
-#endif	//  _XMLOFF_XMLIMPPR_HXX
+#endif // _XMLOFF_XMLIMPPR_HXX
