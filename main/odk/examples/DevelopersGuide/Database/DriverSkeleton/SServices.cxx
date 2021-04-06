97581ef47cb9ffaae6b8757544c8939c93b1f0cc
mseidel <mseidel@apache.org>
2021-04-06 23:13:01 +0200

    Fixed links to Wiki
    (cherry picked from commit aa01715fee31b673717d064c9bd5fb3872e6dbd3)

--- main/odk/examples/DevelopersGuide/Database/DriverSkeleton/SServices.cxx
+++ main/odk/examples/DevelopersGuide/Database/DriverSkeleton/SServices.cxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -37,7 +37,7 @@ using ::com::sun::star::lang::XMultiServiceFactory;
 typedef Reference< XSingleServiceFactory > (SAL_CALL *createFactoryFunc)
 		(
 			const Reference< XMultiServiceFactory > & rServiceManager,
-			const OUString & rComponentName, 
+			const OUString & rComponentName,
 			::cppu::ComponentInstantiation pCreateFunction,
 			const Sequence< OUString > & rServiceNames,
 			rtl_ModuleCount* _pTemp
@@ -51,8 +51,8 @@ typedef Reference< XSingleServiceFactory > (SAL_CALL *createFactoryFunc)
 
 //---------------------------------------------------------------------------------------
 void REGISTER_PROVIDER(
-		const OUString& aServiceImplName, 
-		const Sequence< OUString>& Services, 
+		const OUString& aServiceImplName,
+		const Sequence< OUString>& Services,
 		const Reference< ::com::sun::star::registry::XRegistryKey > & xKey)
 {
 	OUString aMainKeyName;
@@ -60,7 +60,7 @@ void REGISTER_PROVIDER(
 	aMainKeyName += aServiceImplName;
 	aMainKeyName += OUString::createFromAscii("/UNO/SERVICES");
 
-	Reference< ::com::sun::star::registry::XRegistryKey >  xNewKey( xKey->createKey(aMainKeyName) );
+	Reference< ::com::sun::star::registry::XRegistryKey > xNewKey( xKey->createKey(aMainKeyName) );
 	OSL_ENSURE(xNewKey.is(), "SKELETON::component_writeInfo : could not create a registry key !");
 
 	for (sal_uInt32 i=0; i<Services.getLength(); ++i)
@@ -86,23 +86,23 @@ struct ProviderRequest
 
 	inline
 	sal_Bool CREATE_PROVIDER(
-				const OUString& Implname, 
-				const Sequence< OUString > & Services, 
+				const OUString& Implname,
+				const Sequence< OUString > & Services,
 				::cppu::ComponentInstantiation Factory,
 				createFactoryFunc creator
 			)
 	{
 		if (!xRet.is() && (Implname == sImplementationName))
-		try																							
-		{																								
-			xRet = creator( xServiceManager, sImplementationName,Factory, Services,0);	
+		try
+		{
+			xRet = creator( xServiceManager, sImplementationName,Factory, Services,0);
 		}
 		catch(...)
 		{
 		}
 		return xRet.is();
 	}
-	
+
 	void* getProvider() const { return xRet.get(); }
 };
 
@@ -118,21 +118,21 @@ extern "C" SAL_DLLPUBLIC_EXPORT void SAL_CALL component_getImplementationEnviron
 
 // This method not longer necessary since OOo 3.4 where the component registration was
 // was changed to passive component registration. For more details see
-// http://wiki.services.openoffice.org/wiki/Passive_Component_Registration
+// https://wiki.openoffice.org/wiki/Passive_Component_Registration
 //---------------------------------------------------------------------------------------
 // extern "C" SAL_DLLPUBLIC_EXPORT sal_Bool SAL_CALL component_writeInfo(
 // 				void* pServiceManager,
 // 				void* pRegistryKey
 // 			)
 // {
-// 	if (pRegistryKey) 
-// 	try 
+// 	if (pRegistryKey)
+// 	try
 // 	{
 // 		Reference< ::com::sun::star::registry::XRegistryKey > xKey(reinterpret_cast< ::com::sun::star::registry::XRegistryKey*>(pRegistryKey));
 
 // 		REGISTER_PROVIDER(
-// 			SkeletonDriver::getImplementationName_Static(), 
-// 			SkeletonDriver::getSupportedServiceNames_Static(), xKey);		
+// 			SkeletonDriver::getImplementationName_Static(),
+// 			SkeletonDriver::getSupportedServiceNames_Static(), xKey);
 
 // 		return sal_True;
 // 	}
@@ -156,9 +156,9 @@ extern "C" SAL_DLLPUBLIC_EXPORT void* SAL_CALL component_getFactory(
 		ProviderRequest aReq(pServiceManager,pImplementationName);
 
 		aReq.CREATE_PROVIDER(
-			SkeletonDriver::getImplementationName_Static(), 
+			SkeletonDriver::getImplementationName_Static(),
 			SkeletonDriver::getSupportedServiceNames_Static(),
-			SkeletonDriver_CreateInstance, ::cppu::createSingleFactory) 
+			SkeletonDriver_CreateInstance, ::cppu::createSingleFactory)
 		;
 
 		if(aReq.xRet.is())
@@ -169,5 +169,4 @@ extern "C" SAL_DLLPUBLIC_EXPORT void* SAL_CALL component_getFactory(
 
 	return pRet;
 };
-
 
