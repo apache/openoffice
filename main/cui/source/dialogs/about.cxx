73167bc4952ce47a23c1b855c7ab7f273b075910
Arrigo Marchiori <ardovm@yahoo.it>
2021-04-15 21:39:19 +0200

    Give the new variable a better name
    (cherry picked from commit a524e3b9fa2406b2d2d0639635ac5f2826f38374)

--- main/cui/source/dialogs/about.cxx
+++ main/cui/source/dialogs/about.cxx
@@ -499,14 +499,14 @@ const rtl::OUString AboutDialog::GetBuildId() const
     // For better readability, strip the duplicate ProductMajor ("340").
     if ( sProductSource.getLength() )
     {
-        sal_Int32 versionLength = sProductSource.getLength() - 3;
+        sal_Int32 nMajorLength = sProductSource.getLength() - 3;
         bool bMatchingUPD =
                 ( sProductSource.getLength() >= 3 )
-            &&  ( sBuildId.getLength() >= versionLength )
-            &&  ( sProductSource.copy( 3 ) == sBuildId.copy( 0, versionLength ) );
+            &&  ( sBuildId.getLength() >= nMajorLength )
+            &&  ( sProductSource.copy( 3 ) == sBuildId.copy( 0, nMajorLength ) );
         OSL_ENSURE( bMatchingUPD, "BUILDID and ProductSource do not match in their UPD" );
         if ( bMatchingUPD )
-            sProductSource = sProductSource.copy( 0, sProductSource.getLength() - versionLength );
+            sProductSource = sProductSource.copy( 0, sProductSource.getLength() - nMajorLength );
 
         // prepend the product source
         sBuildIdBuff.insert( 0, sProductSource );
