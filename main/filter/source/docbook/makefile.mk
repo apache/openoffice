fe8621838a5185ff889f62aa34a43ca0873c0a90
mseidel <mseidel@apache.org>
2021-05-29 16:32:38 +0200

    Use .ott instead of .stw for DocBook Template
    (cherry picked from commit 703a52b984f78cabb1072aa364a6130f4be2620c)

--- main/filter/source/docbook/makefile.mk
+++ main/filter/source/docbook/makefile.mk
@@ -1,5 +1,5 @@
 #**************************************************************
-#  
+#
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
 #  distributed with this work for additional information
@@ -7,16 +7,16 @@
 #  to you under the Apache License, Version 2.0 (the
 #  "License"); you may not use this file except in compliance
 #  with the License.  You may obtain a copy of the License at
-#  
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing,
 #  software distributed under the License is distributed on an
 #  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 #  KIND, either express or implied.  See the License for the
 #  specific language governing permissions and limitations
 #  under the License.
-#  
+#
 #**************************************************************
 
 
@@ -28,20 +28,20 @@ TARGET=fl_javafilter_binaries
 .INCLUDE: settings.mk
 
 all: \
-	$(BIN)$/docbooktosoffheadings.xsl   \
-	$(BIN)$/sofftodocbookheadings.xsl   \
-	$(BIN)$/DocBookTemplate.stw
-	
+	$(BIN)$/docbooktosoffheadings.xsl \
+	$(BIN)$/sofftodocbookheadings.xsl \
+	$(BIN)$/DocBookTemplate.ott
 
 
 $(OUT)$/bin$/docbooktosoffheadings.xsl: docbooktosoffheadings.xsl
-         $(COPY) docbooktosoffheadings.xsl $(OUT)$/bin$/docbooktosoffheadings.xsl
+	$(COPY) docbooktosoffheadings.xsl $(OUT)$/bin$/docbooktosoffheadings.xsl
 
 $(OUT)$/bin$/sofftodocbookheadings.xsl: sofftodocbookheadings.xsl
-         $(COPY) sofftodocbookheadings.xsl $(OUT)$/bin$/sofftodocbookheadings.xsl
+	$(COPY) sofftodocbookheadings.xsl $(OUT)$/bin$/sofftodocbookheadings.xsl
 
-$(OUT)$/bin$/DocBookTemplate.stw: DocBookTemplate.stw
-         $(COPY) DocBookTemplate.stw $(OUT)$/bin$/DocBookTemplate.stw
+$(OUT)$/bin$/DocBookTemplate.ott: DocBookTemplate.ott
+	$(COPY) DocBookTemplate.ott $(OUT)$/bin$/DocBookTemplate.ott
 
 .INCLUDE: target.mk
 
+# vim: set noet ts=4 sw=4:
