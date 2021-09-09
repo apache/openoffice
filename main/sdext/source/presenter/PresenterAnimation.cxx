6d8c2cf3a16923a7b13de6a07a1a47521cd88b08
mseidel <mseidel@apache.org>
2021-09-09 17:28:25 +0200

    Fixed mixed style (space/tabs) in indentation
    (cherry picked from commit aa9633e79cf97d25152daf08078c72555c2e123f)

--- main/sdext/source/presenter/PresenterAnimation.cxx
+++ main/sdext/source/presenter/PresenterAnimation.cxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -32,25 +32,25 @@ namespace sdext { namespace presenter {
 
 sal_uInt64 GetCurrentTime (void)
 {
-    TimeValue aTimeValue;
-    if (osl_getSystemTime(&aTimeValue))
-        return sal_uInt64(aTimeValue.Seconds * 1000.0 + aTimeValue.Nanosec / 1000000.0);
-    else
-        return 0;
+	TimeValue aTimeValue;
+	if (osl_getSystemTime(&aTimeValue))
+		return sal_uInt64(aTimeValue.Seconds * 1000.0 + aTimeValue.Nanosec / 1000000.0);
+	else
+		return 0;
 }
 
 
 
 
 PresenterAnimation::PresenterAnimation (
-    const sal_uInt64 nStartDelay,
-    const sal_uInt64 nTotalDuration,
-    const sal_uInt64 nStepDuration)
-    : mnStartTime(GetCurrentTime()+nStartDelay),
-      mnTotalDuration(nTotalDuration),
-      mnStepDuration(nStepDuration),
-      mpStartCallbacks(),
-      mpEndCallbacks()
+	const sal_uInt64 nStartDelay,
+	const sal_uInt64 nTotalDuration,
+	const sal_uInt64 nStepDuration)
+	: mnStartTime(GetCurrentTime()+nStartDelay),
+	  mnTotalDuration(nTotalDuration),
+	  mnStepDuration(nStepDuration),
+	  mpStartCallbacks(),
+	  mpEndCallbacks()
 {
 }
 
@@ -66,7 +66,7 @@ PresenterAnimation::~PresenterAnimation (void)
 
 sal_uInt64 PresenterAnimation::GetStartTime (void)
 {
-    return mnStartTime;
+	return mnStartTime;
 }
 
 
@@ -74,7 +74,7 @@ sal_uInt64 PresenterAnimation::GetStartTime (void)
 
 sal_uInt64 PresenterAnimation::GetEndTime (void)
 {
-    return mnStartTime + mnTotalDuration;
+	return mnStartTime + mnTotalDuration;
 }
 
 
@@ -82,7 +82,7 @@ sal_uInt64 PresenterAnimation::GetEndTime (void)
 
 sal_uInt64 PresenterAnimation::GetStepDuration (void)
 {
-    return mnStepDuration;
+	return mnStepDuration;
 }
 
 
@@ -90,9 +90,9 @@ sal_uInt64 PresenterAnimation::GetStepDuration (void)
 
 void PresenterAnimation::AddStartCallback (const Callback& rCallback)
 {
-    if (mpStartCallbacks.get() == NULL)
-        mpStartCallbacks.reset(new ::std::vector<Callback>());
-    mpStartCallbacks->push_back(rCallback);
+	if (mpStartCallbacks.get() == NULL)
+		mpStartCallbacks.reset(new ::std::vector<Callback>());
+	mpStartCallbacks->push_back(rCallback);
 }
 
 
@@ -100,21 +100,21 @@ void PresenterAnimation::AddStartCallback (const Callback& rCallback)
 
 void PresenterAnimation::AddEndCallback (const Callback& rCallback)
 {
-    if (mpEndCallbacks.get() == NULL)
-        mpEndCallbacks.reset(new ::std::vector<Callback>());
-    mpEndCallbacks->push_back(rCallback);
+	if (mpEndCallbacks.get() == NULL)
+		mpEndCallbacks.reset(new ::std::vector<Callback>());
+	mpEndCallbacks->push_back(rCallback);
 }
 
 
 
 void PresenterAnimation::RunStartCallbacks (void)
 {
-    if (mpStartCallbacks.get() != NULL)
-    {
-        ::std::vector<Callback>::const_iterator iCallback;
-        for (iCallback=mpStartCallbacks->begin(); iCallback!=mpStartCallbacks->end(); ++iCallback)
-            (*iCallback)();
-    }
+	if (mpStartCallbacks.get() != NULL)
+	{
+		::std::vector<Callback>::const_iterator iCallback;
+		for (iCallback=mpStartCallbacks->begin(); iCallback!=mpStartCallbacks->end(); ++iCallback)
+			(*iCallback)();
+	}
 }
 
 
@@ -122,12 +122,12 @@ void PresenterAnimation::RunStartCallbacks (void)
 
 void PresenterAnimation::RunEndCallbacks (void)
 {
-    if (mpEndCallbacks.get() != NULL)
-    {
-        ::std::vector<Callback>::const_iterator iCallback;
-        for (iCallback=mpEndCallbacks->begin(); iCallback!=mpEndCallbacks->end(); ++iCallback)
-            (*iCallback)();
-    }
+	if (mpEndCallbacks.get() != NULL)
+	{
+		::std::vector<Callback>::const_iterator iCallback;
+		for (iCallback=mpEndCallbacks->begin(); iCallback!=mpEndCallbacks->end(); ++iCallback)
+			(*iCallback)();
+	}
 }
 
 
--- main/sdext/source/presenter/components.cxx
+++ main/sdext/source/presenter/components.cxx
@@ -19,6 +19,8 @@
  *
  *************************************************************/
 
+
+
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sdext.hxx"
 
@@ -36,39 +38,39 @@ rtl_StandardModuleCount g_moduleCount = MODULE_COUNT_INIT;
 
 static struct cppu::ImplementationEntry gServiceEntries[] =
 {
-    {
-        PresenterProtocolHandler::Create,
-        PresenterProtocolHandler::getImplementationName_static,
-        PresenterProtocolHandler::getSupportedServiceNames_static,
-        cppu::createSingleComponentFactory, &g_moduleCount.modCnt, 0 
-    },
-    {
-        PresenterScreenJob::Create,
-        PresenterScreenJob::getImplementationName_static,
-        PresenterScreenJob::getSupportedServiceNames_static,
-        cppu::createSingleComponentFactory, 0, 0 
-    },
-    { 0, 0, 0, 0, 0, 0 }
+	{
+		PresenterProtocolHandler::Create,
+		PresenterProtocolHandler::getImplementationName_static,
+		PresenterProtocolHandler::getSupportedServiceNames_static,
+		cppu::createSingleComponentFactory, &g_moduleCount.modCnt, 0
+	},
+	{
+		PresenterScreenJob::Create,
+		PresenterScreenJob::getImplementationName_static,
+		PresenterScreenJob::getSupportedServiceNames_static,
+		cppu::createSingleComponentFactory, 0, 0
+	},
+	{ 0, 0, 0, 0, 0, 0 }
 };
 
 extern "C"
 {
-    SAL_DLLPUBLIC_EXPORT sal_Bool SAL_CALL component_canUnload( TimeValue *pTime )
-    {
-        return g_moduleCount.canUnload( &g_moduleCount , pTime );
-    }
+	SAL_DLLPUBLIC_EXPORT sal_Bool SAL_CALL component_canUnload( TimeValue *pTime )
+	{
+		return g_moduleCount.canUnload( &g_moduleCount , pTime );
+	}
 
-    SAL_DLLPUBLIC_EXPORT void SAL_CALL component_getImplementationEnvironment(
-        const sal_Char ** ppEnvTypeName, uno_Environment ** )
-    {
-        *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
-    }
+	SAL_DLLPUBLIC_EXPORT void SAL_CALL component_getImplementationEnvironment(
+		const sal_Char ** ppEnvTypeName, uno_Environment ** )
+	{
+		*ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
+	}
 
-    SAL_DLLPUBLIC_EXPORT void * SAL_CALL component_getFactory(
-        const sal_Char * pImplName, void * pServiceManager, void * pRegistryKey )
-    {
-        return cppu::component_getFactoryHelper( pImplName, pServiceManager, pRegistryKey , gServiceEntries);
-    }
+	SAL_DLLPUBLIC_EXPORT void * SAL_CALL component_getFactory(
+		const sal_Char * pImplName, void * pServiceManager, void * pRegistryKey )
+	{
+		return cppu::component_getFactoryHelper( pImplName, pServiceManager, pRegistryKey , gServiceEntries);
+	}
 }
 
 } } // end of namespace sdext::presenter
