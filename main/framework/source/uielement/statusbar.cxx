c2cd596f6c8defeedb5f4d7f2bcc900c6435f2fa
mseidel <mseidel@apache.org>
2021-09-19 22:26:12 +0200

    Fixed mixed style (space/tabs) in indentation
    (cherry picked from commit 4971a9916dc668e27c3a563358ae54cdda4e80ea)

--- main/framework/source/uielement/statusbar.cxx
+++ main/framework/source/uielement/statusbar.cxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -38,72 +38,72 @@
 namespace framework
 {
 
-FrameworkStatusBar::FrameworkStatusBar( 
-    Window*           pParent,
-    WinBits           nWinBits ) :
-    StatusBar( pParent, nWinBits ),
-    m_pMgr( NULL ),
-    m_bShow( sal_False ),
-    m_bLock( sal_False )
+FrameworkStatusBar::FrameworkStatusBar(
+	Window* pParent,
+	WinBits nWinBits ) :
+	StatusBar( pParent, nWinBits ),
+	m_pMgr( NULL ),
+	m_bShow( sal_False ),
+	m_bLock( sal_False )
 {
-    // set optimal size
-    SetOutputSizePixel( CalcWindowSizePixel() );
+	// set optimal size
+	SetOutputSizePixel( CalcWindowSizePixel() );
 }
 
 FrameworkStatusBar::~FrameworkStatusBar()
 {
 }
-        
+
 void FrameworkStatusBar::SetStatusBarManager( StatusBarManager* pStatusBarManager )
 {
-    vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
-    m_pMgr = pStatusBarManager;
+	vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+	m_pMgr = pStatusBarManager;
 }
 
 void FrameworkStatusBar::UserDraw(const UserDrawEvent& rUDEvt)
 {
-    if ( m_pMgr )
-        m_pMgr->UserDraw( rUDEvt );
+	if ( m_pMgr )
+		m_pMgr->UserDraw( rUDEvt );
 }
 
 void FrameworkStatusBar::Command( const CommandEvent& rEvt )
 {
-    if ( m_pMgr )
-        m_pMgr->Command( rEvt );
+	if ( m_pMgr )
+		m_pMgr->Command( rEvt );
 }
 
 void FrameworkStatusBar::StateChanged( StateChangedType nType )
 {
-    if ( m_pMgr )
-        m_pMgr->StateChanged( nType );
+	if ( m_pMgr )
+		m_pMgr->StateChanged( nType );
 }
 
 void FrameworkStatusBar::DataChanged( const DataChangedEvent& rDCEvt )
 {
-    StatusBar::DataChanged( rDCEvt );
-    if ( m_pMgr )
-        m_pMgr->DataChanged( rDCEvt );
+	StatusBar::DataChanged( rDCEvt );
+	if ( m_pMgr )
+		m_pMgr->DataChanged( rDCEvt );
 }
 
 void FrameworkStatusBar::MouseMove( const MouseEvent& rMEvt )
 {
-    StatusBar::MouseMove( rMEvt );
-    if ( m_pMgr )
-        m_pMgr->MouseMove( rMEvt );
+	StatusBar::MouseMove( rMEvt );
+	if ( m_pMgr )
+		m_pMgr->MouseMove( rMEvt );
 }
 
 void FrameworkStatusBar::MouseButtonDown( const MouseEvent& rMEvt )
 {
-    StatusBar::MouseButtonDown( rMEvt );
-    if ( m_pMgr )
-        m_pMgr->MouseButtonDown( rMEvt );
+	StatusBar::MouseButtonDown( rMEvt );
+	if ( m_pMgr )
+		m_pMgr->MouseButtonDown( rMEvt );
 }
 
 void FrameworkStatusBar::MouseButtonUp( const MouseEvent& rMEvt )
 {
-    StatusBar::MouseButtonUp( rMEvt );
-    if ( m_pMgr )
-        m_pMgr->MouseButtonUp( rMEvt );
+	StatusBar::MouseButtonUp( rMEvt );
+	if ( m_pMgr )
+		m_pMgr->MouseButtonUp( rMEvt );
 }
 
 }
--- main/framework/source/uielement/toolbar.cxx
+++ main/framework/source/uielement/toolbar.cxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -35,39 +35,39 @@ namespace framework
 {
 
 ToolBar::ToolBar( Window* pParent, WinBits nWinBits ) :
-    ToolBox( pParent, nWinBits )
-    ,   m_pToolBarManager( 0 )
+	ToolBox( pParent, nWinBits )
+	, m_pToolBarManager( 0 )
 {
 }
 
 ToolBar::~ToolBar()
 {
 }
-        
+
 void ToolBar::SetToolBarManager( ToolBarManager* pTbMgr )
 {
-    m_pToolBarManager = pTbMgr;
+	m_pToolBarManager = pTbMgr;
 }
 
 void ToolBar::Command( const CommandEvent& rCEvt )
 {
 	if ( m_aCommandHandler.IsSet() )
 		m_aCommandHandler.Call( (void *)( &rCEvt ));
-    ToolBox::Command( rCEvt );
+	ToolBox::Command( rCEvt );
 }
 
 void ToolBar::StateChanged( StateChangedType nType )
 {
-    ToolBox::StateChanged( nType );
-    if ( m_aStateChangedHandler.IsSet() )
-        m_aStateChangedHandler.Call( &nType );
+	ToolBox::StateChanged( nType );
+	if ( m_aStateChangedHandler.IsSet() )
+		m_aStateChangedHandler.Call( &nType );
 }
 
 void ToolBar::DataChanged( const DataChangedEvent& rDCEvt )
 {
-    ToolBox::DataChanged( rDCEvt );
-    if ( m_aDataChangedHandler.IsSet() )
-        m_aDataChangedHandler.Call( (void*)&rDCEvt );
+	ToolBox::DataChanged( rDCEvt );
+	if ( m_aDataChangedHandler.IsSet() )
+		m_aDataChangedHandler.Call( (void*)&rDCEvt );
 }
 
 }
