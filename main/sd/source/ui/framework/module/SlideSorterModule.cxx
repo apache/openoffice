d88d3165fe83d355d6f5a6be3591cce6d0ed94ea
mseidel <mseidel@apache.org>
2021-10-19 16:29:41 +0200

    Fixed typos, removed whitespace

--- main/sd/source/ui/framework/module/SlideSorterModule.cxx
+++ main/sd/source/ui/framework/module/SlideSorterModule.cxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -46,30 +46,30 @@ namespace sd { namespace framework {
 //===== SlideSorterModule ==================================================
 
 SlideSorterModule::SlideSorterModule (
-    const Reference<frame::XController>& rxController,
-    const OUString& rsLeftPaneURL)
-    : ResourceManager(rxController,
-        FrameworkHelper::CreateResourceId(FrameworkHelper::msSlideSorterURL, rsLeftPaneURL)),
-      mxViewTabBarId(FrameworkHelper::CreateResourceId(
-          FrameworkHelper::msViewTabBarURL,
-          FrameworkHelper::msCenterPaneURL)),
-      mxControllerManager(rxController,UNO_QUERY)
+	const Reference<frame::XController>& rxController,
+	const OUString& rsLeftPaneURL)
+	: ResourceManager(rxController,
+		FrameworkHelper::CreateResourceId(FrameworkHelper::msSlideSorterURL, rsLeftPaneURL)),
+	mxViewTabBarId(FrameworkHelper::CreateResourceId(
+		FrameworkHelper::msViewTabBarURL,
+		FrameworkHelper::msCenterPaneURL)),
+	mxControllerManager(rxController,UNO_QUERY)
 {
-    if (mxConfigurationController.is())
-    {
-        UpdateViewTabBar(NULL);
+	if (mxConfigurationController.is())
+	{
+		UpdateViewTabBar(NULL);
 
-        AddActiveMainView(FrameworkHelper::msImpressViewURL);
-        AddActiveMainView(FrameworkHelper::msOutlineViewURL);
-        AddActiveMainView(FrameworkHelper::msNotesViewURL);
-        
-        AddActiveMainView(FrameworkHelper::msDrawViewURL);
+		AddActiveMainView(FrameworkHelper::msImpressViewURL);
+		AddActiveMainView(FrameworkHelper::msOutlineViewURL);
+		AddActiveMainView(FrameworkHelper::msNotesViewURL);
 
-        mxConfigurationController->addConfigurationChangeListener(
-            this,
-            FrameworkHelper::msResourceActivationEvent,
-            Any());
-    }
+		AddActiveMainView(FrameworkHelper::msDrawViewURL);
+
+		mxConfigurationController->addConfigurationChangeListener(
+			this,
+			FrameworkHelper::msResourceActivationEvent,
+			Any());
+	}
 }
 
 
@@ -78,37 +78,37 @@ SlideSorterModule::SlideSorterModule (
 SlideSorterModule::~SlideSorterModule (void)
 {
 }
-    
+
 
 
 
 void SAL_CALL SlideSorterModule::notifyConfigurationChange (
-    const ConfigurationChangeEvent& rEvent)
-    throw (RuntimeException)
+	const ConfigurationChangeEvent& rEvent)
+	throw (RuntimeException)
 {
-    if (rEvent.Type.equals(FrameworkHelper::msResourceActivationEvent))
-    {
-        if (rEvent.ResourceId->compareTo(mxViewTabBarId) == 0)
-        {
-            // Update the view tab bar because the view tab bar has just
-            // become active.
-            UpdateViewTabBar(Reference<XTabBar>(rEvent.ResourceObject,UNO_QUERY));
-        }
-        else if (rEvent.ResourceId->getResourceTypePrefix().equals(
-            FrameworkHelper::msViewURLPrefix)
-            && rEvent.ResourceId->isBoundTo(
-                FrameworkHelper::CreateResourceId(FrameworkHelper::msCenterPaneURL),
-                AnchorBindingMode_DIRECT))
-        {
-            // Update the view tab bar because the view in the center pane
-            // has changed.
-            UpdateViewTabBar(NULL);
-        }
-    }
-    else
-    {
-        ResourceManager::notifyConfigurationChange(rEvent);
-    }
+	if (rEvent.Type.equals(FrameworkHelper::msResourceActivationEvent))
+	{
+		if (rEvent.ResourceId->compareTo(mxViewTabBarId) == 0)
+		{
+			// Update the view tab bar because the view tab bar has just
+			// become active.
+			UpdateViewTabBar(Reference<XTabBar>(rEvent.ResourceObject,UNO_QUERY));
+		}
+		else if (rEvent.ResourceId->getResourceTypePrefix().equals(
+			FrameworkHelper::msViewURLPrefix)
+			&& rEvent.ResourceId->isBoundTo(
+				FrameworkHelper::CreateResourceId(FrameworkHelper::msCenterPaneURL),
+				AnchorBindingMode_DIRECT))
+		{
+			// Update the view tab bar because the view in the center pane
+			// has changed.
+			UpdateViewTabBar(NULL);
+		}
+	}
+	else
+	{
+		ResourceManager::notifyConfigurationChange(rEvent);
+	}
 }
 
 
@@ -116,34 +116,34 @@ void SAL_CALL SlideSorterModule::notifyConfigurationChange (
 
 void SlideSorterModule::UpdateViewTabBar (const Reference<XTabBar>& rxTabBar)
 {
-    if ( ! mxControllerManager.is())
-        return;
+	if ( ! mxControllerManager.is())
+		return;
 
-    Reference<XTabBar> xBar (rxTabBar);
-    if ( ! xBar.is())
-    {
-        Reference<XConfigurationController> xCC (
-            mxControllerManager->getConfigurationController());
-        if (xCC.is())
-            xBar = Reference<XTabBar>(xCC->getResource(mxViewTabBarId), UNO_QUERY);
-    }
+	Reference<XTabBar> xBar (rxTabBar);
+	if ( ! xBar.is())
+	{
+		Reference<XConfigurationController> xCC (
+			mxControllerManager->getConfigurationController());
+		if (xCC.is())
+			xBar = Reference<XTabBar>(xCC->getResource(mxViewTabBarId), UNO_QUERY);
+	}
 
-    if (xBar.is())
-    {
-        TabBarButton aButtonA;
-        aButtonA.ResourceId = FrameworkHelper::CreateResourceId(
-            FrameworkHelper::msSlideSorterURL,
-            FrameworkHelper::msCenterPaneURL);
-        aButtonA.ButtonLabel = String(SdResId(STR_SLIDE_MODE));
+	if (xBar.is())
+	{
+		TabBarButton aButtonA;
+		aButtonA.ResourceId = FrameworkHelper::CreateResourceId(
+			FrameworkHelper::msSlideSorterURL,
+			FrameworkHelper::msCenterPaneURL);
+		aButtonA.ButtonLabel = String(SdResId(STR_SLIDE_MODE));
 
-        TabBarButton aButtonB;
-        aButtonB.ResourceId = FrameworkHelper::CreateResourceId(
-            FrameworkHelper::msHandoutViewURL,
-            FrameworkHelper::msCenterPaneURL);
+		TabBarButton aButtonB;
+		aButtonB.ResourceId = FrameworkHelper::CreateResourceId(
+			FrameworkHelper::msHandoutViewURL,
+			FrameworkHelper::msCenterPaneURL);
 
-        if ( ! xBar->hasTabBarButton(aButtonA))
-            xBar->addTabBarButtonAfter(aButtonA, aButtonB);
-    }
+		if ( ! xBar->hasTabBarButton(aButtonA))
+			xBar->addTabBarButtonAfter(aButtonA, aButtonB);
+	}
 }
 
 
