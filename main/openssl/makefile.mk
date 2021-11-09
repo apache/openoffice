d193cc068f136fb9081dd35bb407a0f5fe3910ea
Jim Jagielski <jimjag@gmail.com>
2021-11-09 13:01:10 -0500

    Using openssl 1.1.1
    (cherry picked from commit b86e16e6b6a3d6837f70d8d07088d00ba1238de2)

--- main/openssl/makefile.mk
+++ main/openssl/makefile.mk
@@ -1,5 +1,5 @@
 #**************************************************************
-#  
+#
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
 #  distributed with this work for additional information
@@ -7,16 +7,16 @@
 #  to you under the Apache License, Version 2.0 (the
 #  "License"); you may not use this file except in compliance
 #  with the License.  You may obtain a copy of the License at
-#  
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing,
 #  software distributed under the License is distributed on an
 #  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 #  KIND, either express or implied.  See the License for the
 #  specific language governing permissions and limitations
 #  under the License.
-#  
+#
 #**************************************************************
 
 
@@ -42,14 +42,14 @@ TARGET=openssl
 	@echo "openssl disabled...."
 .ENDIF
 
-OPENSSL_NAME=openssl-1.0.2u
+OPENSSL_NAME=openssl-1.1.1l
 
 TARFILE_NAME=$(OPENSSL_NAME)
-TARFILE_MD5=cdc2638f789ecc2db2c91488265686c1
+TARFILE_MD5=ac0d4387f3ba0ad741b0580dd45f6ff3
 
 CONFIGURE_DIR=.
 CONFIGURE_ACTION=config
-CONFIGURE_FLAGS=-I$(SYSBASE)$/usr$/include -L$(SYSBASE)$/usr$/lib shared 
+CONFIGURE_FLAGS=-I$(SYSBASE)$/usr$/include -L$(SYSBASE)$/usr$/lib shared
 
 BUILD_DIR=.
 BUILD_ACTION=make CC='$(CC)' build_libs
@@ -110,13 +110,18 @@ UNAME=$(shell uname)
 	.ENDIF
 .ENDIF
 
+.IF "$(OS)" == "MACOSX"
+	PATCH_FILES=opensslmacos.patch
+	CONFIGURE_ACTION=Configure darwin64-x86_64-cc no-dso no-shared $(NO_ASM)
+.ENDIF
+
 .IF "$(OS)" == "WNT"
 
 .IF "$(COM)"=="GCC"
 PATCH_FILES=opensslmingw.patch
 .IF "$(USE_MINGW)" == "cygwin"
 CONFIGURE_ACTION=$(PERL) configure
-CONFIGURE_FLAGS=mingw shared 
+CONFIGURE_FLAGS=mingw shared
 INSTALL_ACTION=mv libcrypto.a libcrypto_static.a && mv libcrypto.dll.a libcrypto.a && mv libssl.a libssl_static.a && mv libssl.dll.a libssl.a
 OUT2LIB = libcrypto_static.*
 OUT2LIB += libssl_static.*
