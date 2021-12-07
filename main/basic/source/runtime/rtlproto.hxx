941cf72f292fa2a5f6d9485e694bf6de4adef8dd
Shubham Singh <87699417+detonators@users.noreply.github.com>
2021-12-07 18:41:49 +0530

    Added one line extra space between the code.
    Added one line extra space between the code lines and comments to make it look more clean.

--- main/basic/source/runtime/rtlproto.hxx
+++ main/basic/source/runtime/rtlproto.hxx
@@ -16,7 +16,8 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
+ *
  *************************************************************/
 
 
@@ -24,8 +25,10 @@
 #include <basic/sbstar.hxx>
 #include "sbtrace.hxx"
 
+
 #define	RTLFUNC( name ) void SbRtl_##name( StarBASIC* pBasic, SbxArray& rPar, sal_Bool bWrite )
 #define	RTLNAME( name ) &SbRtl_##name
+
 
 typedef void( *RtlCall ) ( StarBASIC* p, SbxArray& rArgs, sal_Bool bWrite );
 
@@ -40,6 +43,7 @@ extern RTLFUNC(Nothing);
 extern RTLFUNC(Null);
 extern RTLFUNC(True);
 
+
 extern RTLFUNC(ATTR_NORMAL);
 extern RTLFUNC(ATTR_READONLY);
 extern RTLFUNC(ATTR_HIDDEN);
@@ -47,6 +51,7 @@ extern RTLFUNC(ATTR_SYSTEM);
 extern RTLFUNC(ATTR_VOLUME);
 extern RTLFUNC(ATTR_DIRECTORY);
 extern RTLFUNC(ATTR_ARCHIVE);
+
 
 extern RTLFUNC(V_EMPTY);
 extern RTLFUNC(V_NULL);
@@ -57,6 +62,7 @@ extern RTLFUNC(V_DOUBLE);
 extern RTLFUNC(V_CURRENCY);
 extern RTLFUNC(V_DATE);
 extern RTLFUNC(V_STRING);
+
 
 extern RTLFUNC(MB_OK);
 extern RTLFUNC(MB_OKCANCEL);
@@ -74,6 +80,7 @@ extern RTLFUNC(MB_DEFBUTTON3);
 extern RTLFUNC(MB_APPLMODAL);
 extern RTLFUNC(MB_SYSTEMMODAL);
 
+
 extern RTLFUNC(IDOK);
 extern RTLFUNC(IDCANCEL);
 extern RTLFUNC(IDABORT);
@@ -81,15 +88,19 @@ extern RTLFUNC(IDRETRY);
 extern RTLFUNC(IDYES);
 extern RTLFUNC(IDNO);
 
+
 extern RTLFUNC(CF_TEXT);
 extern RTLFUNC(CF_BITMAP);
 extern RTLFUNC(CF_METAFILEPICT);
 
+
 extern RTLFUNC(PI);
+
 
 extern RTLFUNC(SET_OFF);
 extern RTLFUNC(SET_ON);
 extern RTLFUNC(TOGGLE);
+
 
 extern RTLFUNC(TYP_AUTHORFLD);
 extern RTLFUNC(TYP_CHAPTERFLD);
@@ -131,20 +142,25 @@ extern RTLFUNC(TYP_GETREFPAGEFLD);
 extern RTLFUNC(TYP_INTERNETFLD);
 extern RTLFUNC(TYP_JUMPEDITFLD);
 
+
 extern RTLFUNC(FRAMEANCHORPAGE);
 extern RTLFUNC(FRAMEANCHORPARA);
 extern RTLFUNC(FRAMEANCHORCHAR);
 
+
 extern RTLFUNC(CLEAR_ALLTABS);
 extern RTLFUNC(CLEAR_TAB);
 extern RTLFUNC(SET_TAB);
+
 
 extern RTLFUNC(LINEPROP);
 extern RTLFUNC(LINE_1);
 extern RTLFUNC(LINE_15);
 extern RTLFUNC(LINE_2);
 
+
 // Methoden
+
 
 extern RTLFUNC(CreateObject);
 extern RTLFUNC(Error);
@@ -157,12 +173,19 @@ extern RTLFUNC(ChrW);
 extern RTLFUNC(Cos);
 extern RTLFUNC(CurDir);
 extern RTLFUNC(ChDir);  // JSM
+
 extern RTLFUNC(ChDrive); // JSM
+
 extern RTLFUNC(FileCopy); // JSM
+
 extern RTLFUNC(Kill); // JSM
+
 extern RTLFUNC(MkDir); // JSM
+
 extern RTLFUNC(RmDir); // JSM
+
 extern RTLFUNC(SendKeys); // JSM
+
 extern RTLFUNC(DimArray);
 extern RTLFUNC(Dir);
 extern RTLFUNC(DoEvents);
@@ -228,6 +251,7 @@ extern RTLFUNC(IsNumeric);
 extern RTLFUNC(IsObject);
 extern RTLFUNC(IsUnoStruct);
 
+
 extern RTLFUNC(FileDateTime);
 extern RTLFUNC(Format);
 extern RTLFUNC(GetAttr);
@@ -239,6 +263,7 @@ extern RTLFUNC(VarType);
 extern RTLFUNC(TypeName);
 extern RTLFUNC(TypeLen);
 
+
 extern RTLFUNC(EOF);
 extern RTLFUNC(FileAttr);
 extern RTLFUNC(Loc);
@@ -247,12 +272,14 @@ extern RTLFUNC(Seek);
 extern RTLFUNC(SetAttr); // JSM
 extern RTLFUNC(Reset); // JSM
 
+
 extern RTLFUNC(DDEInitiate);
 extern RTLFUNC(DDETerminate);
 extern RTLFUNC(DDETerminateAll);
 extern RTLFUNC(DDERequest);
 extern RTLFUNC(DDEExecute);
 extern RTLFUNC(DDEPoke);
+
 
 extern RTLFUNC(FreeFile);
 extern RTLFUNC(IsMissing);
@@ -262,13 +289,16 @@ extern RTLFUNC(RGB);
 extern RTLFUNC(QBColor);
 extern RTLFUNC(StrConv);
 
+
 extern RTLFUNC(Beep);
+
 
 extern RTLFUNC(Load);
 extern RTLFUNC(Unload);
 extern RTLFUNC(AboutStarBasic);
 extern RTLFUNC(LoadPicture);
 extern RTLFUNC(SavePicture);
+
 
 extern RTLFUNC(CallByName);
 extern RTLFUNC(CBool); // JSM
@@ -283,9 +313,12 @@ extern RTLFUNC(CStr); // JSM
 extern RTLFUNC(CVar); // JSM
 extern RTLFUNC(CVErr); // JSM
 
+
 extern RTLFUNC(Iif); // JSM
 
+
 extern RTLFUNC(DumpAllObjects);
+
 
 extern RTLFUNC(GetSystemType);
 extern RTLFUNC(GetGUIType);
@@ -293,14 +326,19 @@ extern RTLFUNC(Red);
 extern RTLFUNC(Green);
 extern RTLFUNC(Blue);
 
+
 extern RTLFUNC(Switch);
 extern RTLFUNC(Wait);
+
 //i#64882# add new WaitUntil
+
 extern RTLFUNC(WaitUntil);
+
 
 extern RTLFUNC(GetGUIVersion);
 extern RTLFUNC(Choose);
 extern RTLFUNC(Trim);
+
 
 extern RTLFUNC(DateAdd);
 extern RTLFUNC(DateDiff);
@@ -314,6 +352,7 @@ extern RTLFUNC(Array);
 extern RTLFUNC(FindObject);
 extern RTLFUNC(FindPropertyObject);
 extern RTLFUNC(EnableReschedule);
+
 
 extern RTLFUNC(Put);
 extern RTLFUNC(Get);
@@ -344,11 +383,16 @@ extern RTLFUNC(CompatibilityMode);
 extern RTLFUNC(CDec);
 extern RTLFUNC(CaptureAssertions);
 
-extern RTLFUNC(Partition); // Fong
+
+extern RTLFUNC(Partition);
+// Fong
+
 
 #ifdef DBG_TRACE_BASIC
 extern RTLFUNC(TraceCommand);
 #endif
 
+
 extern double Now_Impl();
 extern void Wait_Impl( bool bDurationBased, SbxArray& rPar );
+
