627628354578bc12b977fd84871d68bc0860b13f
mseidel <mseidel@apache.org>
2022-01-04 13:27:38 +0100

    Fixed typos, removed whitespace

--- main/svx/source/sidebar/area/AreaPropertyPanel.hxx
+++ main/svx/source/sidebar/area/AreaPropertyPanel.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,17 +7,19 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
+
+
 
 #ifndef SVX_PROPERTYPANEL_AREAPAGE_HXX
 #define SVX_PROPERTYPANEL_AREAPAGE_HXX
@@ -55,151 +57,151 @@ class PopupContainer;
 class AreaTransparencyGradientControl;
 
 class AreaPropertyPanel
-:   public Control,
-    public ::sfx2::sidebar::ControllerItem::ItemUpdateReceiverInterface
+:	public Control,
+	public ::sfx2::sidebar::ControllerItem::ItemUpdateReceiverInterface
 {
 public:
-    static AreaPropertyPanel* Create(
-        Window* pParent,
-        const cssu::Reference<css::frame::XFrame>& rxFrame,
-        SfxBindings* pBindings);
+	static AreaPropertyPanel* Create(
+		Window* pParent,
+		const cssu::Reference<css::frame::XFrame>& rxFrame,
+		SfxBindings* pBindings);
 
-    virtual void DataChanged(
-        const DataChangedEvent& rEvent);
+	virtual void DataChanged(
+		const DataChangedEvent& rEvent);
 
-    virtual void NotifyItemUpdate(
-        const sal_uInt16 nSId,
-        const SfxItemState eState,
-        const SfxPoolItem* pState,
-        const bool bIsEnabled);
+	virtual void NotifyItemUpdate(
+		const sal_uInt16 nSId,
+		const SfxItemState eState,
+		const SfxPoolItem* pState,
+		const bool bIsEnabled);
 
-    SfxBindings* GetBindings();
+	SfxBindings* GetBindings();
 
-    const static sal_Int32 DEFAULT_CENTERX;
-    const static sal_Int32 DEFAULT_CENTERY;
-    const static sal_Int32 DEFAULT_ANGLE;
-    const static sal_Int32 DEFAULT_STARTVALUE;
-    const static sal_Int32 DEFAULT_ENDVALUE;
-    const static sal_Int32 DEFAULT_BORDER;
+	const static sal_Int32 DEFAULT_CENTERX;
+	const static sal_Int32 DEFAULT_CENTERY;
+	const static sal_Int32 DEFAULT_ANGLE;
+	const static sal_Int32 DEFAULT_STARTVALUE;
+	const static sal_Int32 DEFAULT_ENDVALUE;
+	const static sal_Int32 DEFAULT_BORDER;
 
-    XGradient GetGradient (const XGradientStyle eStyle) const;
-    void SetGradient (const XGradient& rGradient);
-    sal_Int32 GetSelectedTransparencyTypeIndex (void) const;
+	XGradient GetGradient (const XGradientStyle eStyle) const;
+	void SetGradient (const XGradient& rGradient);
+	sal_Int32 GetSelectedTransparencyTypeIndex (void) const;
 
-    virtual void Resize (void);
+	virtual void Resize (void);
 
 private:
-    sal_uInt16                                          meLastXFS;
-    Color                                               maLastColor;
+	sal_uInt16											meLastXFS;
+	Color												maLastColor;
 
-    sal_uInt16                                          mnLastPosGradient;
-    sal_uInt16                                          mnLastPosHatch;
-    sal_uInt16                                          mnLastPosBitmap;
-    sal_uInt16                                          mnLastTransSolid;
+	sal_uInt16											mnLastPosGradient;
+	sal_uInt16											mnLastPosHatch;
+	sal_uInt16											mnLastPosBitmap;
+	sal_uInt16											mnLastTransSolid;
 
-    XGradient                                           maGradientLinear;
-    XGradient                                           maGradientAxial;
-    XGradient                                           maGradientRadial;
-    XGradient                                           maGradientElliptical;
-    XGradient                                           maGradientSquare;
-    XGradient                                           maGradientRect;
+	XGradient											maGradientLinear;
+	XGradient											maGradientAxial;
+	XGradient											maGradientRadial;
+	XGradient											maGradientElliptical;
+	XGradient											maGradientSquare;
+	XGradient											maGradientRect;
 
-    //ui controls
-    ::boost::scoped_ptr< FixedText >                    mpColorTextFT;
-    ::boost::scoped_ptr< SvxFillTypeBox >               mpLbFillType;
-    ::boost::scoped_ptr< SvxFillAttrBox >               mpLbFillAttr;
-    ::boost::scoped_ptr< Window >                       mpToolBoxColorBackground;
-    ::boost::scoped_ptr< ToolBox >                      mpToolBoxColor; // for new color picker
-    ::boost::scoped_ptr< FixedText >                    mpTrspTextFT;
-    ::boost::scoped_ptr< ListBox >                      mpLBTransType;
-    ::boost::scoped_ptr< MetricField >                  mpMTRTransparent;
-    ::boost::scoped_ptr< Window >                       mpBTNGradientBackground;
-    ::boost::scoped_ptr< ToolBox >                      mpBTNGradient;
+	// ui controls
+	::boost::scoped_ptr< FixedText >					mpColorTextFT;
+	::boost::scoped_ptr< SvxFillTypeBox >				mpLbFillType;
+	::boost::scoped_ptr< SvxFillAttrBox >				mpLbFillAttr;
+	::boost::scoped_ptr< Window >						mpToolBoxColorBackground;
+	::boost::scoped_ptr< ToolBox >						mpToolBoxColor; // for new color picker
+	::boost::scoped_ptr< FixedText >					mpTrspTextFT;
+	::boost::scoped_ptr< ListBox >						mpLBTransType;
+	::boost::scoped_ptr< MetricField >					mpMTRTransparent;
+	::boost::scoped_ptr< Window >						mpBTNGradientBackground;
+	::boost::scoped_ptr< ToolBox >						mpBTNGradient;
 
-    ::boost::scoped_ptr< ::svx::ToolboxButtonColorUpdater > mpColorUpdater;
+	::boost::scoped_ptr< ::svx::ToolboxButtonColorUpdater >	mpColorUpdater;
 
-    ::boost::scoped_ptr< XFillStyleItem >               mpStyleItem;
-    ::boost::scoped_ptr< XFillColorItem >               mpColorItem;
-    ::boost::scoped_ptr< XFillGradientItem >            mpFillGradientItem;
-    ::boost::scoped_ptr< XFillHatchItem >               mpHatchItem;
-    ::boost::scoped_ptr< XFillBitmapItem >              mpBitmapItem;
+	::boost::scoped_ptr< XFillStyleItem >				mpStyleItem;
+	::boost::scoped_ptr< XFillColorItem >				mpColorItem;
+	::boost::scoped_ptr< XFillGradientItem >			mpFillGradientItem;
+	::boost::scoped_ptr< XFillHatchItem >				mpHatchItem;
+	::boost::scoped_ptr< XFillBitmapItem >				mpBitmapItem;
 
-    ::sfx2::sidebar::ControllerItem                     maStyleControl;
-    ::sfx2::sidebar::ControllerItem                     maColorControl;
-    ::sfx2::sidebar::ControllerItem                     maGradientControl;
-    ::sfx2::sidebar::ControllerItem                     maHatchControl;
-    ::sfx2::sidebar::ControllerItem                     maBitmapControl;
-    ::sfx2::sidebar::ControllerItem                     maColorTableControl;
-    ::sfx2::sidebar::ControllerItem                     maGradientListControl;
-    ::sfx2::sidebar::ControllerItem                     maHatchListControl;
-    ::sfx2::sidebar::ControllerItem                     maBitmapListControl;
-    ::sfx2::sidebar::ControllerItem                     maFillTransparenceController;
-    ::sfx2::sidebar::ControllerItem                     maFillFloatTransparenceController;
+	::sfx2::sidebar::ControllerItem						maStyleControl;
+	::sfx2::sidebar::ControllerItem						maColorControl;
+	::sfx2::sidebar::ControllerItem						maGradientControl;
+	::sfx2::sidebar::ControllerItem						maHatchControl;
+	::sfx2::sidebar::ControllerItem						maBitmapControl;
+	::sfx2::sidebar::ControllerItem						maColorTableControl;
+	::sfx2::sidebar::ControllerItem						maGradientListControl;
+	::sfx2::sidebar::ControllerItem						maHatchListControl;
+	::sfx2::sidebar::ControllerItem						maBitmapListControl;
+	::sfx2::sidebar::ControllerItem						maFillTransparenceController;
+	::sfx2::sidebar::ControllerItem						maFillFloatTransparenceController;
 
-    Image                                               maImgAxial;
-    Image                                               maImgElli;
-    Image                                               maImgQuad;
-    Image                                               maImgRadial;
-    Image                                               maImgSquare;
-    Image                                               maImgLinear;
-    Image                                               maImgColor;
+	Image												maImgAxial;
+	Image												maImgElli;
+	Image												maImgQuad;
+	Image												maImgRadial;
+	Image												maImgSquare;
+	Image												maImgLinear;
+	Image												maImgColor;
 
-    //for high contract
-    Image                                               maImgAxialH;
-    Image                                               maImgElliH;
-    Image                                               maImgQuadH;
-    Image                                               maImgRadialH;
-    Image                                               maImgSquareH;
-    Image                                               maImgLinearH;
-    Image                                               maImgColorH;
+	// for high contract
+	Image												maImgAxialH;
+	Image												maImgElliH;
+	Image												maImgQuadH;
+	Image												maImgRadialH;
+	Image												maImgSquareH;
+	Image												maImgLinearH;
+	Image												maImgColorH;
 
-    String                                              msHelpFillType;
-    String                                              msHelpFillAttr;
+	String												msHelpFillType;
+	String												msHelpFillAttr;
 
-    AreaTransparencyGradientPopup maTrGrPopup;
-    ColorPopup maColorPopup; 
+	AreaTransparencyGradientPopup maTrGrPopup;
+	ColorPopup maColorPopup;
 
-    ::boost::scoped_ptr< XFillFloatTransparenceItem >   mpFloatTransparenceItem;
-    ::boost::scoped_ptr< SfxUInt16Item >                mpTransparanceItem;
+	::boost::scoped_ptr< XFillFloatTransparenceItem >	mpFloatTransparenceItem;
+	::boost::scoped_ptr< SfxUInt16Item >				mpTransparanceItem;
 
-    cssu::Reference<css::frame::XFrame>                 mxFrame;
-    SfxBindings*                                        mpBindings;
+	cssu::Reference<css::frame::XFrame>					mxFrame;
+	SfxBindings*										mpBindings;
 
-    /// bitfield
-    bool                                                mbColorAvail : 1;
+	// bitfield
+	bool												mbColorAvail : 1;
 
-    ::sfx2::sidebar::GridLayouter maLayouter;
+	::sfx2::sidebar::GridLayouter maLayouter;
 
-    DECL_LINK(SelectFillTypeHdl, ListBox* );
-    DECL_LINK(SelectFillAttrHdl, ListBox* );
-    DECL_LINK(ChangeTrgrTypeHdl_Impl, void*);
-    DECL_LINK(ModifyTransparentHdl_Impl, void*);
-    DECL_LINK( ImplPopupModeEndHdl, FloatingWindow* );
+	DECL_LINK(SelectFillTypeHdl, ListBox* );
+	DECL_LINK(SelectFillAttrHdl, ListBox* );
+	DECL_LINK(ChangeTrgrTypeHdl_Impl, void*);
+	DECL_LINK(ModifyTransparentHdl_Impl, void*);
+	DECL_LINK( ImplPopupModeEndHdl, FloatingWindow* );
 
-    // for transparency gradient
-    PopupControl* CreateTransparencyGradientControl (PopupContainer* pParent);
-    DECL_LINK( ClickTrGrHdl_Impl, ToolBox* );
+	// for transparency gradient
+	PopupControl* CreateTransparencyGradientControl (PopupContainer* pParent);
+	DECL_LINK( ClickTrGrHdl_Impl, ToolBox* );
 
-    // for color picker 
-    PopupControl* CreateColorPopupControl (PopupContainer* pParent);
-    DECL_LINK(ToolBoxColorDropHdl, ToolBox *); //for new color picker 
+	// for color picker
+	PopupControl* CreateColorPopupControl (PopupContainer* pParent);
+	DECL_LINK(ToolBoxColorDropHdl, ToolBox *); // for new color picker
 
-    // constructor/destuctor
-    AreaPropertyPanel(
-        Window* pParent,
-        const cssu::Reference<css::frame::XFrame>& rxFrame,
-        SfxBindings* pBindings);
-    virtual ~AreaPropertyPanel(void);
+	// constructor/destructor
+	AreaPropertyPanel(
+		Window* pParent,
+		const cssu::Reference<css::frame::XFrame>& rxFrame,
+		SfxBindings* pBindings);
+	virtual ~AreaPropertyPanel(void);
 
-    void SetupIcons(void);
-    void Initialize();
-    void Update();
-    void ImpUpdateTransparencies();
+	void SetupIcons(void);
+	void Initialize();
+	void Update();
+	void ImpUpdateTransparencies();
 
-    Color GetLastColor (void) const;
-    void SetColor (
-        const String& rsColorName,
-        const Color aColor);
+	Color GetLastColor (void) const;
+	void SetColor (
+		const String& rsColorName,
+		const Color aColor);
 };
 
 
