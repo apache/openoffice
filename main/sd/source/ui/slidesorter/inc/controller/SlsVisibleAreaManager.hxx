cc57ba345c81f429cf0b8b3d7e8fb7802efa85ef
mseidel <mseidel@apache.org>
2022-01-15 14:20:05 +0100

    Fixed typos, removed whitespace, removed multiple license headers
    (cherry picked from commit 916729d09a13a81b9710827c57afa2a17578286c)

--- main/sd/source/ui/slidesorter/inc/controller/SlsVisibleAreaManager.hxx
+++ main/sd/source/ui/slidesorter/inc/controller/SlsVisibleAreaManager.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -77,7 +77,7 @@ private:
         Cleared on every call to ForgetVisibleRequests() and MakeVisible().
     */
     ::std::vector<Rectangle> maVisibleRequests;
-    
+
     /** Animation id for a scroll animation that sets the top
         and left of the visible area to maRequestedVisibleTopLeft.
     */
@@ -85,7 +85,7 @@ private:
     Point maRequestedVisibleTopLeft;
     Animator::AnimationMode meRequestedAnimationMode;
     bool mbIsCurrentSlideTrackingActive;
-    int  mnDisableCount;
+    int mnDisableCount;
 
     void MakeVisible (void);
     ::boost::optional<Point> GetRequestedTopLeft (void) const;
