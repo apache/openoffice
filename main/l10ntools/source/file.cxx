a27c115a070e5f15bedb543d2a457c4589d7776c
mseidel <mseidel@apache.org>
2022-09-09 22:40:52 +0200

    Fixed typos, removed whitespace

--- main/l10ntools/source/file.cxx
+++ main/l10ntools/source/file.cxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,18 +7,17 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
-
 
 
 
@@ -28,24 +27,24 @@
 
 namespace transex
 {
-    
+
 File::File( const rtl::OUString sFile )
 {
-    sFileName = sFile;
+	sFileName = sFile;
 }
 
 File::File( const rtl::OUString sFullPath , const rtl::OUString sFile )
 {
-    sFileName = sFile;
-    sFullName = sFullPath;
+	sFileName = sFile;
+	sFullName = sFullPath;
 }
 
 bool File::lessFile ( const File& rKey1, const File& rKey2 )
 {
-    rtl::OUString sName1( ( static_cast< File >( rKey1 ) ).getFileName() ); 
-    rtl::OUString sName2( ( static_cast< File >( rKey2 ) ).getFileName() ); 
-            
-    return sName1.compareTo( sName2 ) < 0 ;
+	rtl::OUString sName1( ( static_cast< File >( rKey1 ) ).getFileName() );
+	rtl::OUString sName2( ( static_cast< File >( rKey2 ) ).getFileName() );
+
+	return sName1.compareTo( sName2 ) < 0 ;
 }
 
 }
--- main/l10ntools/source/utf8conv.cxx
+++ main/l10ntools/source/utf8conv.cxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -35,10 +35,10 @@
 			 			 RTL_TEXTTOUNICODE_FLAGS_MBUNDEFINED_DEFAULT |\
 			 			 RTL_TEXTTOUNICODE_FLAGS_INVALID_DEFAULT)
 
-#define FROM_CVTFLAGS  	(RTL_UNICODETOTEXT_FLAGS_UNDEFINED_DEFAULT |\
-			 		   	 RTL_UNICODETOTEXT_FLAGS_INVALID_DEFAULT |\
-					   	 RTL_UNICODETOTEXT_FLAGS_UNDEFINED_REPLACE |\
-					   	 RTL_UNICODETOTEXT_FLAGS_PRIVATE_MAPTO0)
+#define FROM_CVTFLAGS	(RTL_UNICODETOTEXT_FLAGS_UNDEFINED_DEFAULT |\
+						 RTL_UNICODETOTEXT_FLAGS_INVALID_DEFAULT |\
+						 RTL_UNICODETOTEXT_FLAGS_UNDEFINED_REPLACE |\
+						 RTL_UNICODETOTEXT_FLAGS_PRIVATE_MAPTO0)
 
 /*****************************************************************************/
 void UTF8Converter::Convert( ByteString &rBuffer,
