0c84c94f7ddce5a42af2a789f2a17466ef33eb67
mseidel <mseidel@apache.org>
2022-09-27 23:17:05 +0200

    Add fixed line in dialog
    (cherry picked from commit 24cebcdcd082893769c03848515688e48399d033)

--- main/sw/source/ui/inc/changedb.hxx
+++ main/sw/source/ui/inc/changedb.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,17 +7,18 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
+
 
 
 #ifndef _CHANGEDB_HXX
@@ -45,36 +46,37 @@ class SwWrtShell;
 struct SwDBData;
 
 /*--------------------------------------------------------------------
-	 Beschreibung: Datenbank an Feldern austauschen
+	Description: Exchange database
  --------------------------------------------------------------------*/
 
 class SwChangeDBDlg: public SvxStandardDialog
 {
-    FixedLine       aDBListFL;
+	FixedLine		aDBListFL;
 	FixedText		aUsedDBFT;
 	FixedText		aAvailDBFT;
 	SvTreeListBox	aUsedDBTLB;
 	SwDBTreeList	aAvailDBTLB;
-    PushButton      aAddDBPB;
+	PushButton		aAddDBPB;
 	FixedInfo		aDescFT;
 	FixedText		aDocDBTextFT;
 	FixedText		aDocDBNameFT;
-	OKButton  		aOKBT;
-	CancelButton 	aCancelBT;
-	HelpButton 		aHelpBT;
+	OKButton		aOKBT;
+	CancelButton	aCancelBT;
+	HelpButton		aHelpBT;
+	FixedLine		aApplDBFL;
 //	PushButton		aChangeBT;
 
-    ImageList       aImageList;
-    ImageList       aImageListHC;
+	ImageList		aImageList;
+	ImageList		aImageListHC;
 
-    SwWrtShell      *pSh;
+	SwWrtShell		*pSh;
 	SwFldMgr		*pMgr;
 
 	DECL_LINK(TreeSelectHdl, SvTreeListBox* pBox = 0);
 	DECL_LINK(ButtonHdl, Button* pBtn);
-    DECL_LINK(AddDBHdl, PushButton*);
+	DECL_LINK(AddDBHdl, PushButton*);
 
-	virtual void 	Apply();
+	virtual void	Apply();
 	void			UpdateFlds();
 	void			FillDBPopup();
 	SvLBoxEntry*	Insert(const String& rDBName);
