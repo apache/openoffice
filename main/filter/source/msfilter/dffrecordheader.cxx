8c733e3e3dc39b4d0997cd00d76f346f7fdf5138
mseidel <mseidel@apache.org>
2023-02-26 11:22:10 +0100

    Using vim modeline, cleanup

--- main/filter/source/msfilter/dffrecordheader.cxx
+++ main/filter/source/msfilter/dffrecordheader.cxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,23 +7,22 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
 
 #include "precompiled_filter.hxx"
-
-#include <filter/msfilter/dffrecordheader.hxx> 
+#include <filter/msfilter/dffrecordheader.hxx>
 
 SvStream& operator>>( SvStream& rIn, DffRecordHeader& rRec )
 {
@@ -36,8 +35,8 @@ SvStream& operator>>( SvStream& rIn, DffRecordHeader& rRec )
 	rIn >> rRec.nRecType;
 	rIn >> rRec.nRecLen;
 	if ( rRec.nRecLen > ( SAL_MAX_UINT32 - rRec.nFilePos ) )	// preserving overflow, optimal would be to check
-		rIn.SetError( SVSTREAM_FILEFORMAT_ERROR );				// the record size against the parent header    
+		rIn.SetError( SVSTREAM_FILEFORMAT_ERROR );				// the record size against the parent header
 	return rIn;
 }
 
-/* vi:set tabstop=4 shiftwidth=4 expandtab: */
+/* vim: set noet sw=4 ts=4: */
