8779042fb5f6e895c8ee8d7b5183e32dfa2b3958
Arrigo Marchiori <ardovm@yahoo.it>
2023-04-13 20:58:46 +0200

    Fix typo

--- main/site_scons/platform/windows.py
+++ main/site_scons/platform/windows.py
@@ -253,7 +253,7 @@ class Windows(aooplatform.Platform):
             '-NXCOMPAT',
             '-DYNAMICBASE'
         ]
-        if env['CPUNAME'] == 'INTEL':
+        if soenv['CPUNAME'] == 'INTEL':
             flags += ['-SAFESEH']
         return flags
 
