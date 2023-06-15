adac7096298acbdd944dd298139431859122d37a
mseidel <mseidel@apache.org>
2023-06-15 10:17:03 +0200

    Maintenance cleanup
    (cherry picked from commit 4dc4eaf19a015b5d2bc15f685fbad0ab7bd7d908)

--- main/svx/inc/svx/xtextit0.hxx
+++ main/svx/inc/svx/xtextit0.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -29,9 +29,7 @@
 #include "svx/svxdllapi.h"
 
 /*************************************************************************
-|*
 |* FormText-StyleItem
-|*
 \************************************************************************/
 
 class SVX_DLLPUBLIC XFormTextStyleItem : public SfxEnumItem
@@ -40,19 +38,17 @@ public:
 							TYPEINFO();
 							XFormTextStyleItem(XFormTextStyle = XFT_NONE);
 							XFormTextStyleItem(SvStream& rIn);
-	virtual SfxPoolItem*    Clone(SfxItemPool* pPool = 0) const;
-	virtual SfxPoolItem*    Create(SvStream& rIn, sal_uInt16 nVer) const;
-	virtual sal_uInt16          GetValueCount() const;
+	virtual SfxPoolItem*	Clone(SfxItemPool* pPool = 0) const;
+	virtual SfxPoolItem*	Create(SvStream& rIn, sal_uInt16 nVer) const;
+	virtual sal_uInt16			GetValueCount() const;
 	XFormTextStyle			GetValue() const { return (XFormTextStyle) SfxEnumItem::GetValue(); }
 	// #FontWork#
-	virtual	sal_Bool        QueryValue( com::sun::star::uno::Any& rVal, sal_uInt8 nMemberId = 0 ) const;
-	virtual	sal_Bool		PutValue( const com::sun::star::uno::Any& rVal, sal_uInt8 nMemberId = 0 );
+	virtual sal_Bool		QueryValue( com::sun::star::uno::Any& rVal, sal_uInt8 nMemberId = 0 ) const;
+	virtual sal_Bool		PutValue( const com::sun::star::uno::Any& rVal, sal_uInt8 nMemberId = 0 );
 };
 
 /*************************************************************************
-|*
 |* FormText-HideFormItem
-|*
 \************************************************************************/
 
 class SVX_DLLPUBLIC XFormTextHideFormItem : public SfxBoolItem
@@ -61,8 +57,10 @@ public:
 							TYPEINFO();
 							XFormTextHideFormItem(sal_Bool bHide = sal_False);
 							XFormTextHideFormItem(SvStream& rIn);
-	virtual SfxPoolItem*    Clone(SfxItemPool* pPool = 0) const;
-	virtual SfxPoolItem*    Create(SvStream& rIn, sal_uInt16 nVer) const;
+	virtual SfxPoolItem*	Clone(SfxItemPool* pPool = 0) const;
+	virtual SfxPoolItem*	Create(SvStream& rIn, sal_uInt16 nVer) const;
 };
 
 #endif
+
+/* vim: set noet sw=4 ts=4: */
--- main/svx/inc/svx/zoomsliderctrl.hxx
+++ main/svx/inc/svx/zoomsliderctrl.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -35,24 +35,25 @@ class SVX_DLLPUBLIC SvxZoomSliderControl : public SfxStatusBarControl
 {
 private:
 
-    struct SvxZoomSliderControl_Impl;
-    SvxZoomSliderControl_Impl* mpImpl;
+	struct SvxZoomSliderControl_Impl;
+	SvxZoomSliderControl_Impl* mpImpl;
 
-    sal_uInt16 Offset2Zoom( long nOffset ) const;
-    long Zoom2Offset( sal_uInt16 nZoom ) const;
+	sal_uInt16 Offset2Zoom( long nOffset ) const;
+	long Zoom2Offset( sal_uInt16 nZoom ) const;
 
 public:
 
 	SFX_DECL_STATUSBAR_CONTROL();
 
-    SvxZoomSliderControl( sal_uInt16 _nSlotId, sal_uInt16 _nId, StatusBar& _rStb );
-    ~SvxZoomSliderControl();
+	SvxZoomSliderControl( sal_uInt16 _nSlotId, sal_uInt16 _nId, StatusBar& _rStb );
+	~SvxZoomSliderControl();
 
-    virtual void  StateChanged( sal_uInt16 nSID, SfxItemState eState, const SfxPoolItem* pState );
-    virtual void  Paint( const UserDrawEvent& rEvt );
-    virtual sal_Bool  MouseButtonDown( const MouseEvent & );
-    virtual sal_Bool  MouseMove( const MouseEvent & rEvt );
+	virtual void StateChanged( sal_uInt16 nSID, SfxItemState eState, const SfxPoolItem* pState );
+	virtual void Paint( const UserDrawEvent& rEvt );
+	virtual sal_Bool MouseButtonDown( const MouseEvent & );
+	virtual sal_Bool MouseMove( const MouseEvent & rEvt );
 };
 
 #endif
 
+/* vim: set noet sw=4 ts=4: */
