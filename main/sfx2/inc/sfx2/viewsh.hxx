81caa1bc17cb787671aabdab2c57cbc890dc0c8e
mseidel <mseidel@apache.org>
2023-07-29 00:12:55 +0200

    Fixed typo (docuement -> document)

--- main/sfx2/inc/sfx2/viewsh.hxx
+++ main/sfx2/inc/sfx2/viewsh.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,17 +7,18 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
+
 
 
 #ifndef _SFXVIEWSH_HXX
@@ -261,11 +262,11 @@ public:
     /** forwards the current document, as returned by ->GetCurrentDocument, to SfxObjectShell::SetWorkingDocument
     */
     void                        SetCurrentDocument() const;
-    
-    /** get an XRenderable instance that can render this docuement
+
+    /** get an XRenderable instance that can render this document
     */
     virtual com::sun::star::uno::Reference< com::sun::star::view::XRenderable > GetRenderable();
-        
+
 
 	virtual void				MarginChanged();
 	const Size&					GetMargin() const;
