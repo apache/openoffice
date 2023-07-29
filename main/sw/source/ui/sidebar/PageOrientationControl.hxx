fb399032256bf013b5fb07ee6cf0a778fb36916b
mseidel <mseidel@apache.org>
2023-07-29 11:23:01 +0200

    Maintenance cleanup
    (cherry picked from commit 344957f76007126c8053525f8d780d21c982c6d4)

--- main/sw/source/ui/sidebar/PageOrientationControl.hxx
+++ main/sw/source/ui/sidebar/PageOrientationControl.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,17 +7,19 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
+
+
 
 #ifndef _SW_SIDEBAR_PAGE_ORIENTATION_CONTROL_HXX_
 #define _SW_SIDEBAR_PAGE_ORIENTATION_CONTROL_HXX_
@@ -25,7 +27,7 @@
 #include <svx/sidebar/PopupControl.hxx>
 
 namespace svx { namespace sidebar {
-    class ValueSetWithTextControl;
+	class ValueSetWithTextControl;
 } }
 
 namespace sw { namespace sidebar {
@@ -34,25 +36,27 @@ class PagePropertyPanel;
 
 
 class PageOrientationControl
-    : public ::svx::sidebar::PopupControl
+	: public ::svx::sidebar::PopupControl
 {
 public:
-    PageOrientationControl(
-        Window* pParent,
-        PagePropertyPanel& rPanel,
-        const sal_Bool bLandscape );
-    ~PageOrientationControl(void);
+	PageOrientationControl(
+		Window* pParent,
+		PagePropertyPanel& rPanel,
+		const sal_Bool bLandscape );
+	~PageOrientationControl(void);
 
 private:
-    ::svx::sidebar::ValueSetWithTextControl* mpOrientationValueSet;
+	::svx::sidebar::ValueSetWithTextControl* mpOrientationValueSet;
 
-    sal_Bool mbLandscape;
+	sal_Bool mbLandscape;
 
-    PagePropertyPanel& mrPagePropPanel;
+	PagePropertyPanel& mrPagePropPanel;
 
-    DECL_LINK(ImplOrientationHdl, void*);
+	DECL_LINK(ImplOrientationHdl, void*);
 };
 
 } } // end of namespace sw::sidebar
 
 #endif
+
+/* vim: set noet sw=4 ts=4: */
