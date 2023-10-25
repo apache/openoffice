721a7f1581f9cb673a9d17f630d942fe38821485
mseidel <mseidel@apache.org>
2023-10-26 00:17:29 +0200

    Cleanup, fixed typo
    (cherry picked from commit 9e91e3f547760e00502c39d5bb429558959bb7b7)

--- main/shell/inc/internal/iso8601_converter.hxx
+++ main/shell/inc/internal/iso8601_converter.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,16 +7,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
 
 
@@ -28,7 +28,7 @@
 
 //-----------------------------------
 /* Converts ISO 8601 conform date/time
-   represenation to the representation
+   representation to the representation
    conforming to the current locale
 */
 std::wstring iso8601_date_to_local_date(const std::wstring& iso8601date);
@@ -41,3 +41,5 @@ std::wstring iso8601_date_to_local_date(const std::wstring& iso8601date);
 std::wstring iso8601_duration_to_local_duration(const std::wstring& iso8601duration);
 
 #endif
+
+/* vim: set noet sw=4 ts=4: */
