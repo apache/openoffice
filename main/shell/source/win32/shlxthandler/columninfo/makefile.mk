fe5d43d9f07528b759454a4b85b1b3ca9335ba40
mseidel <mseidel@apache.org>
2023-10-26 11:43:48 +0200

    Maintenance cleanup

--- main/shell/source/win32/shlxthandler/columninfo/makefile.mk
+++ main/shell/source/win32/shlxthandler/columninfo/makefile.mk
@@ -1,5 +1,5 @@
 #**************************************************************
-#  
+#
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
 #  distributed with this work for additional information
@@ -7,16 +7,16 @@
 #  to you under the Apache License, Version 2.0 (the
 #  "License"); you may not use this file except in compliance
 #  with the License.  You may obtain a copy of the License at
-#  
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing,
 #  software distributed under the License is distributed on an
 #  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 #  KIND, either express or implied.  See the License for the
 #  specific language governing permissions and limitations
 #  under the License.
-#  
+#
 #**************************************************************
 
 
@@ -30,7 +30,7 @@ ENABLE_EXCEPTIONS=TRUE
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE :  settings.mk
+.INCLUDE :	settings.mk
 
 CFLAGS+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0501
 CFLAGS+=-wd4710 -wd4711 -wd4514 -wd4619 -wd4217 -wd4820
@@ -48,8 +48,10 @@ SLOFILES_X64=$(SLO_X64)$/$(TARGET).obj
 .ENDIF # "$(BUILD_X64)"!=""
 
 # --- Targets ------------------------------------------------------
-.INCLUDE :  set_wntx64.mk
+.INCLUDE :	set_wntx64.mk
 .INCLUDE :	target.mk
 INCLUDE!:=$(subst,/stl, $(INCLUDE))
 
-.INCLUDE :  tg_wntx64.mk
+.INCLUDE :	tg_wntx64.mk
+
+# vim: set noet sw=4 ts=4:
