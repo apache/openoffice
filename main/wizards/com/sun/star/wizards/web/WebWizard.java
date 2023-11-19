ef6cbe3cb12c4ff3b91bda6df15cfa64440a5258
mseidel <mseidel@apache.org>
2023-11-19 12:17:51 +0100

    Fixed typos, removed whitespace
    (cherry picked from commit ac186415c6dad2a9daf05ff4be6f27c5bc921331)

--- main/wizards/com/sun/star/wizards/web/WebWizard.java
+++ main/wizards/com/sun/star/wizards/web/WebWizard.java
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,17 +7,18 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
+
 
 
 /*
@@ -31,37 +32,37 @@ import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.wizards.common.Desktop;
 
 /**
- * The last class in the WebWizard Dialog class hirarchy.
- * Has no functionality, is just nice to have it instanciated.
+ * The last class in the WebWizard Dialog class hierarchy.
+ * Has no functionality, is just nice to have it instantiated.
  * @author  rpiterman
  */
 public class WebWizard extends WWD_Events
 {
 
-    /**
-     * @param xmsf
-     */
-    public WebWizard(XMultiServiceFactory xmsf) throws Exception
-    {
-        super(xmsf);
-    }
+	/**
+	 * @param xmsf
+	 */
+	public WebWizard(XMultiServiceFactory xmsf) throws Exception
+	{
+		super(xmsf);
+	}
 
-    public static void main(String args[])
-    {
+	public static void main(String args[])
+	{
 
-        String ConnectStr =
-                "uno:socket,host=localhost,port=8100;urp,negotiate=0,forcesynchronous=1;StarOffice.ServiceManager";
-        try
-        {
-            XMultiServiceFactory xmsf = Desktop.connect(ConnectStr);
-            WebWizard ww = new WebWizard(xmsf);
-            ww.show();
-            ww.cleanup();
+		String ConnectStr =
+				"uno:socket,host=localhost,port=8100;urp,negotiate=0,forcesynchronous=1;StarOffice.ServiceManager";
+		try
+		{
+			XMultiServiceFactory xmsf = Desktop.connect(ConnectStr);
+			WebWizard ww = new WebWizard(xmsf);
+			ww.show();
+			ww.cleanup();
 
-        }
-        catch (Exception exception)
-        {
-            exception.printStackTrace(System.out);
-        }
-    }
+		}
+		catch (Exception exception)
+		{
+			exception.printStackTrace(System.out);
+		}
+	}
 }
