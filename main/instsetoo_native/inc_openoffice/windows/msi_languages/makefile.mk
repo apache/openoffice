0f58d4df4ee34d7e3a257f302abbfb334b456e56
mseidel <mseidel@apache.org>
2024-01-09 14:26:16 +0100

    Clarify Windows Installer texts

--- main/instsetoo_native/inc_openoffice/windows/msi_languages/makefile.mk
+++ main/instsetoo_native/inc_openoffice/windows/msi_languages/makefile.mk
@@ -1,5 +1,5 @@
 #**************************************************************
-#  
+#
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
 #  distributed with this work for additional information
@@ -7,19 +7,17 @@
 #  to you under the Apache License, Version 2.0 (the
 #  "License"); you may not use this file except in compliance
 #  with the License.  You may obtain a copy of the License at
-#  
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing,
 #  software distributed under the License is distributed on an
 #  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 #  KIND, either express or implied.  See the License for the
 #  specific language governing permissions and limitations
 #  under the License.
-#  
+#
 #**************************************************************
-
-
 
 PRJ=..$/..$/..
 
@@ -34,16 +32,16 @@ common_build:=
 # ------------------------------------------------------------------
 
 ULFFILES = \
-    ActionTe.ulf \
-    Control.ulf  \
-    CustomAc.ulf \
-    Error.ulf    \
-    LaunchCo.ulf \
-    Property.ulf \
-    RadioBut.ulf \
-    Nsis.ulf     \
-    SIS.ulf      \
-    UIText.ulf 
+	ActionTe.ulf \
+	Control.ulf  \
+	CustomAc.ulf \
+	Error.ulf    \
+	LaunchCo.ulf \
+	Property.ulf \
+	RadioBut.ulf \
+	Nsis.ulf     \
+	SIS.ulf      \
+	UIText.ulf 
     
 MLFFILES = $(foreach,i,$(ULFFILES) $(MISC)$/$(TARGET)$/$(i:b).mlf)
 UULFFILES = $(MISC)$/$(TARGET)$/Nsis.uulf
@@ -57,3 +55,5 @@ UULFFILES = $(MISC)$/$(TARGET)$/Nsis.uulf
 ALLTAR : $(MLFFILES) $(UULFFILES)
 
 .ENDIF
+
+# vim: set noet sw=4 ts=4:
