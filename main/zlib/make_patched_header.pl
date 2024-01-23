1e89fc536b900e960e0fca7b6c524ab6ec809102
mseidel <mseidel@apache.org>
2024-01-23 11:37:27 +0100

    Update zlib to 1.3.1
    (cherry picked from commit 31cbf0498f0068311e6843a8dec28f7e1948a304)

--- main/zlib/make_patched_header.pl
+++ main/zlib/make_patched_header.pl
@@ -2,7 +2,7 @@
 eval 'exec perl -S $0 ${1+"$@"}'
     if 0;
 #**************************************************************
-#  
+#
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
 #  distributed with this work for additional information
@@ -10,18 +10,17 @@ eval 'exec perl -S $0 ${1+"$@"}'
 #  to you under the Apache License, Version 2.0 (the
 #  "License"); you may not use this file except in compliance
 #  with the License.  You may obtain a copy of the License at
-#  
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing,
 #  software distributed under the License is distributed on an
 #  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 #  KIND, either express or implied.  See the License for the
 #  specific language governing permissions and limitations
 #  under the License.
-#  
+#
 #**************************************************************
-
 
 #
 # make_patched_header - make patched header
@@ -58,5 +57,4 @@ close PATCHED_FILE;
 close ORIG_FILE;
 
 exit(0);
-
 
