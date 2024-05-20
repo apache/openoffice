c622e83ad38c464d4787e61242dd0f6ebdd5f92d
mseidel <mseidel@apache.org>
2024-05-20 17:33:42 +0200

    Minor cleanup

--- main/packimages/Package_packimages.mk
+++ main/packimages/Package_packimages.mk
@@ -1,5 +1,5 @@
 #**************************************************************
-#  
+#
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
 #  distributed with this work for additional information
@@ -7,19 +7,17 @@
 #  to you under the Apache License, Version 2.0 (the
 #  "License"); you may not use this file except in compliance
 #  with the License.  You may obtain a copy of the License at
-#  
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing,
 #  software distributed under the License is distributed on an
 #  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 #  KIND, either express or implied.  See the License for the
 #  specific language governing permissions and limitations
 #  under the License.
-#  
+#
 #**************************************************************
-
-
 
 $(eval $(call gb_Package_Package,packimages_packimages,$(WORKDIR)/CustomTarget/packimages,packimages))
 
@@ -121,3 +119,4 @@ $(WORKDIR)/CustomTarget/packimages/classic.flag :
 	cat $@ || echo "ERROR: unpacking $(CLASSIC_TARBALL) failed"
 endif
 
+# vim: set noet sw=4 ts=4:
