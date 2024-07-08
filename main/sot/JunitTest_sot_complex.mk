ef5ad92fe4d2b16fd995dae9b1c111aa07052af6
Damjan Jovanovic <damjan@apache.org>
2024-07-08 19:26:16 +0200

    Also needs juh.jar.
    Patch by: me

--- main/sot/JunitTest_sot_complex.mk
+++ main/sot/JunitTest_sot_complex.mk
@@ -35,6 +35,7 @@ $(eval $(call gb_JunitTest_add_jars,sot_complex,\
 	$(OUTDIR)/bin/test.jar \
 	$(OUTDIR)/bin/unoil.jar \
 	$(OUTDIR)/bin/jurt.jar \
+	$(OUTDIR)/bin/juh.jar \
 ))
 
 $(eval $(call gb_JunitTest_add_classes,sot_complex,\
