fa4aafc803275c91719ffded15f2f76b5dd8ea56
Damjan Jovanovic <damjan@apache.org>
2024-07-08 20:04:32 +0200

    Needs juh.jar.
    Patch by: me

--- main/sw/JunitTest_sw_complex.mk
+++ main/sw/JunitTest_sw_complex.mk
@@ -48,6 +48,7 @@ $(eval $(call gb_JunitTest_add_jars,sw_complex,\
 	$(OUTDIR)/bin/test.jar \
 	$(OUTDIR)/bin/unoil.jar \
 	$(OUTDIR)/bin/jurt.jar \
+	$(OUTDIR)/bin/juh.jar \
 ))
 
 $(eval $(call gb_JunitTest_add_classes,sw_complex,\
