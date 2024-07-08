145becff582de2b506259cb5a59655024c2607af
Damjan Jovanovic <damjan@apache.org>
2024-07-08 19:30:14 +0200

    Needs juh.jar.
    Patch by: me

--- main/comphelper/JunitTest_comphelper_complex.mk
+++ main/comphelper/JunitTest_comphelper_complex.mk
@@ -33,6 +33,7 @@ $(eval $(call gb_JunitTest_add_jars,comphelper_complex,\
 	$(OUTDIR)/bin/test.jar \
 	$(OUTDIR)/bin/unoil.jar \
 	$(OUTDIR)/bin/jurt.jar \
+	$(OUTDIR)/bin/juh.jar \
 ))
 
 $(eval $(call gb_JunitTest_add_classes,comphelper_complex,\
