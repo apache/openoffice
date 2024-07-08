aeba200682401e8b083afef4c1c6c73baa44b0c7
Damjan Jovanovic <damjan@apache.org>
2024-07-08 19:27:57 +0200

    Needs juh.jar.
    Patch by: me

--- main/package/JunitTest_package_complex_storages.mk
+++ main/package/JunitTest_package_complex_storages.mk
@@ -33,6 +33,7 @@ $(eval $(call gb_JunitTest_add_jars,package_complex_storages,\
 	$(OUTDIR)/bin/test.jar \
 	$(OUTDIR)/bin/unoil.jar \
 	$(OUTDIR)/bin/jurt.jar \
+	$(OUTDIR)/bin/juh.jar \
 ))
 
 $(eval $(call gb_JunitTest_add_sourcefiles,package_complex_storages,\
