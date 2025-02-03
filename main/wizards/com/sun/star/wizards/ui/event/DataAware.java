28f017add4940d0e7a2615f2c694a42bb4ab8413
mseidel <mseidel@apache.org>
2025-02-03 01:53:08 +0100

    Fixed typo (obejct -> object) and some other
    (cherry picked from commit 0ba51277dcadf5446227c21ffd366f1895b67f0a)

--- main/wizards/com/sun/star/wizards/ui/event/DataAware.java
+++ main/wizards/com/sun/star/wizards/ui/event/DataAware.java
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,18 +7,17 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
-
 
 package com.sun.star.wizards.ui.event;
 
@@ -36,8 +35,8 @@ import com.sun.star.wizards.common.PropertyNames;
  * This class, as a base abstract class, sets a frame of functionality,
  * delegating the data Object get/set methods to a Value object,
  * and leaving the UI get/set methods abstract.
- * Note that event listenning is *not* a part of this model.
- * the updateData() or updateUI() methods should be porogramatically called.
+ * Note that event listening is *not* a part of this model.
+ * the updateData() or updateUI() methods should be programatically called.
  * in child classes, the updateData() will be binded to UI event calls.
  * <br><br>
  * This class holds references to a Data Object and a Value object.
@@ -108,7 +107,7 @@ public abstract class DataAware {
     }
 
     /**
-     * gets the current value from the data obejct.
+     * gets the current value from the data object.
      * this method delegates the job to 
      * the value object.
      * @return the current value of the data object.
@@ -193,7 +192,7 @@ public abstract class DataAware {
 
     /**
      * given a collection containing DataAware objects,
-     * calls updateUI() on each memebr of the collection.
+     * calls updateUI() on each member of the collection.
      * @param dataAwares a collection containing DataAware objects.
      */
     public static void updateUI(Collection dataAwares) {
