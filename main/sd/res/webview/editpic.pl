8cadb362c5edb22d4f8e57188f23e5e3e04d8da2
John Bampton <jbampton@users.noreply.github.com>
2025-02-10 09:26:16 +1000

    Use lowercase HTML tags (#270)

--- main/sd/res/webview/editpic.pl
+++ main/sd/res/webview/editpic.pl
@@ -23,38 +23,38 @@
 require "common.pl";
 
 print "Content-type: text/html\n\n";
-print "<HTML>\n";
+print "<html>\n";
 
-print "<HEAD>\n";
+print "<head>\n";
 print "<title>editpic.pl</title>";
-print "</HEAD>\n";
+print "</head>\n";
 
-print "<BODY>\n";
+print "<body>\n";
     $sCurrPic = join( "", common::File_read( "currpic.txt" ) );
     @aPictureArray = common::File_read( "picture.txt" );
     $nPictureArrayLen = @aPictureArray;
-    print "<FORM action=\"savepic.pl\" method=get>\n";
+    print "<form action=\"savepic.pl\" method=get>\n";
         if( abs( $sCurrPic ) > 1 )
         {
-            print "<INPUT type=submit name=\"Auswahl\" value=\"-\"></INPUT>\n";
+            print "<input type=submit name=\"Auswahl\" value=\"-\"></input>\n";
         }
         else
         {
-            print "<INPUT type=button value=\" \"></INPUT>\n";
+            print "<input type=button value=\" \"></input>\n";
         }
-        print "<INPUT type=text name=\"CurrPic\" value=\"";
+        print "<input type=text name=\"CurrPic\" value=\"";
         print $sCurrPic;
-        print "\" SIZE=3></INPUT>\n";
+        print "\" size=3></input>\n";
         if( abs( $sCurrPic ) < ( $nPictureArrayLen - 1 ) )
         {
-            print "<INPUT type=submit name=\"Auswahl\" value=\"+\"></INPUT>\n";
+            print "<input type=submit name=\"Auswahl\" value=\"+\"></input>\n";
         }
         else
         {
-            print "<INPUT type=button value=\" \"></INPUT>\n";
+            print "<input type=button value=\" \"></input>\n";
         }
-        print "<INPUT type=submit name=\"Auswahl\" value=\"$$2\"></INPUT>\n";
-    print "</FORM>\n";
-print "</BODY>\n";
+        print "<input type=submit name=\"Auswahl\" value=\"$$2\"></input>\n";
+    print "</form>\n";
+print "</body>\n";
 
-print "</HTML>\n";
+print "</html>\n";
