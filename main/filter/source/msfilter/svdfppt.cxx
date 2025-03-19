12d08c4b5c79861fd7796aba4fd1070365433675
John Bampton <jbampton@users.noreply.github.com>
2025-03-19 21:36:15 +1000

    Fix spelling (#335)
    * Fix spelling
    
    * Update ignored words list

--- main/filter/source/msfilter/svdfppt.cxx
+++ main/filter/source/msfilter/svdfppt.cxx
@@ -1331,7 +1331,7 @@ SdrObject* SdrEscherImport::ProcessObj( SvStream& rSt, DffObjData& rObjData, voi
 			}
 		}
 	}
-	if ( pRet )	// sj: #i38501#, and and taking care of connections to group objects
+	if ( pRet )	// sj: #i38501#, and taking care of connections to group objects
 	{
 		if ( rObjData.nSpFlags & SP_FBACKGROUND )
 		{
