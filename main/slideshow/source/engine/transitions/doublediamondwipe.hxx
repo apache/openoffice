06db55ca6e7b86cf496b9e4c4bd824b1b77c53c0
mseidel <mseidel@apache.org>
2025-04-05 20:21:28 +0200

    Cleanup

--- main/slideshow/source/engine/transitions/doublediamondwipe.hxx
+++ main/slideshow/source/engine/transitions/doublediamondwipe.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,19 +7,17 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
-
-
 
 #if ! defined INCLUDED_SLIDESHOW_DOUBLEDIAMONDWIPE_HXX
 #define INCLUDED_SLIDESHOW_DOUBLEDIAMONDWIPE_HXX
@@ -29,21 +27,22 @@
 #include "parametricpolypolygon.hxx"
 #include "transitiontools.hxx"
 
-
 namespace slideshow {
 namespace internal {
 
-/// Generates a double diamond wipe:
+// Generates a double diamond wipe:
 class DoubleDiamondWipe : public ParametricPolyPolygon
 {
 public:
-    DoubleDiamondWipe() : m_unitRect( createUnitRect() ) {}
-    virtual ::basegfx::B2DPolyPolygon operator()( double x );
+	DoubleDiamondWipe() : m_unitRect( createUnitRect() ) {}
+	virtual ::basegfx::B2DPolyPolygon operator()( double x );
 private:
-    const ::basegfx::B2DPolyPolygon m_unitRect;
+	const ::basegfx::B2DPolyPolygon m_unitRect;
 };
 
 }
 }
 
 #endif /* INCLUDED_SLIDESHOW_DOUBLEDIAMONDWIPE_HXX */
+
+/* vim: set noet sw=4 ts=4: */
