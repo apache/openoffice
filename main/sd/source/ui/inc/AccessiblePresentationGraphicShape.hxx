1a03dc2c489554774abaf88ae3ad43d8cca28144
mseidel <mseidel@apache.org>
2025-04-08 11:47:45 +0200

    Cleanup
    (cherry picked from commit 6c3f46008062a6f976c36b489e4ef55c817e828c)

--- main/sd/source/ui/inc/AccessiblePresentationGraphicShape.hxx
+++ main/sd/source/ui/inc/AccessiblePresentationGraphicShape.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,20 +7,17 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
-
-
-
 
 #ifndef _SD_ACCESSIBILITY_ACCESSIBLE_PRESENTATION_GRAPHIC_SHAPE_HXX
 #define _SD_ACCESSIBILITY_ACCESSIBLE_PRESENTATION_GRAPHIC_SHAPE_HXX
@@ -31,40 +28,42 @@
 namespace accessibility {
 
 /**	This class makes Impress shapes accessible.
-*/
+ */
 class AccessiblePresentationGraphicShape
-    :	public AccessibleGraphicShape
+	:	public AccessibleGraphicShape
 {
 public:
-	//=====  internal  ========================================================
+	// ===== internal ========================================================
 	AccessiblePresentationGraphicShape (
-        const AccessibleShapeInfo& rShapeInfo,
-        const AccessibleShapeTreeInfo& rShapeTreeInfo);
+		const AccessibleShapeInfo& rShapeInfo,
+		const AccessibleShapeTreeInfo& rShapeTreeInfo);
 	virtual ~AccessiblePresentationGraphicShape (void);
 
-	//=====  XServiceInfo  ====================================================
-    
-    /**	Returns an identifier for the implementation of this object.
-    */
+	// ===== XServiceInfo ====================================================
+
+	/**	Returns an identifier for the implementation of this object.
+	 */
 	virtual ::rtl::OUString SAL_CALL
-    	getImplementationName (void)
-	    throw (::com::sun::star::uno::RuntimeException);
+		getImplementationName (void)
+		throw (::com::sun::star::uno::RuntimeException);
 
-    //=====  internal  ========================================================
+	// ===== internal ========================================================
 
-    ///	Create a name string that contains the accessible name.
+	//	Create a name string that contains the accessible name.
 	virtual ::rtl::OUString
-    	CreateAccessibleBaseName ()
-        throw (::com::sun::star::uno::RuntimeException);
+		CreateAccessibleBaseName ()
+		throw (::com::sun::star::uno::RuntimeException);
 
-    ///	Create a description string that contains the accessible description.
+	//	Create a description string that contains the accessible description.
 	virtual ::rtl::OUString
-    	CreateAccessibleDescription ()
-        throw (::com::sun::star::uno::RuntimeException);
-	///	Return this object's role.
+		CreateAccessibleDescription ()
+		throw (::com::sun::star::uno::RuntimeException);
+	//	Return this object's role.
 	virtual sal_Int16 SAL_CALL getAccessibleRole () throw (::com::sun::star::uno::RuntimeException);
 };
 
 } // end of namespace accessibility
 
 #endif
+
+/* vim: set noet sw=4 ts=4: */
