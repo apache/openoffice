ba0e87d7fb911293fa8d9f5b3416d4d04511ac24
mseidel <mseidel@apache.org>
2025-04-19 00:20:32 +0200

    Cleanup
    (cherry picked from commit b3897e6aa10c44e4a3a28a6e1f45e38816be3d7f)

--- main/slideshow/source/engine/transitions/veewipe.hxx
+++ main/slideshow/source/engine/transitions/veewipe.hxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,19 +7,17 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
-
-
 
 #if ! defined INCLUDED_SLIDESHOW_VEEWIPE_HXX
 #define INCLUDED_SLIDESHOW_VEEWIPE_HXX
@@ -29,19 +27,20 @@
 #include "parametricpolypolygon.hxx"
 #include "transitiontools.hxx"
 
-
 namespace slideshow {
 namespace internal {
 
-/// Generate a vee wipe
+// Generate a vee wipe
 class VeeWipe : public ParametricPolyPolygon
 {
 public:
-    VeeWipe() {}
-    virtual ::basegfx::B2DPolyPolygon operator()( double x );
+	VeeWipe() {}
+	virtual ::basegfx::B2DPolyPolygon operator()( double x );
 };
 
 }
 }
 
 #endif /* INCLUDED_SLIDESHOW_VEEWIPE_HXX */
+
+/* vim: set noet sw=4 ts=4: */
