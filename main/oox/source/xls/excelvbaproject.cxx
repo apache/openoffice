d3e5b9da5ecd3a21afd95e21a5fd36807d0753aa
mseidel <mseidel@apache.org>
2025-05-19 17:21:45 +0200

    Cleanup

--- main/oox/source/xls/excelvbaproject.cxx
+++ main/oox/source/xls/excelvbaproject.cxx
@@ -1,5 +1,5 @@
 /**************************************************************
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,19 +7,17 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  *************************************************************/
-
-
 
 #include "oox/xls/excelvbaproject.hxx"
 
@@ -54,8 +52,8 @@ using ::rtl::OUStringBuffer;
 // ============================================================================
 
 ExcelVbaProject::ExcelVbaProject( const Reference< XComponentContext >& rxContext, const Reference< XSpreadsheetDocument >& rxDocument ) :
-    ::oox::ole::VbaProject( rxContext, Reference< XModel >( rxDocument, UNO_QUERY ), CREATE_OUSTRING( "Calc" ) ),
-    mxDocument( rxDocument )
+	::oox::ole::VbaProject( rxContext, Reference< XModel >( rxDocument, UNO_QUERY ), CREATE_OUSTRING( "Calc" ) ),
+	mxDocument( rxDocument )
 {
 }
 
@@ -65,11 +63,11 @@ namespace {
 
 struct SheetCodeNameInfo
 {
-    PropertySet         maSheetProps;       /// Property set of the sheet without codename.
-    OUString            maPrefix;           /// Prefix for the codename to be generated.
+	PropertySet         maSheetProps;       // Property set of the sheet without codename.
+	OUString            maPrefix;           // Prefix for the codename to be generated.
 
-    inline explicit     SheetCodeNameInfo( PropertySet& rSheetProps, const OUString& rPrefix ) :
-                            maSheetProps( rSheetProps ), maPrefix( rPrefix ) {}
+	inline explicit     SheetCodeNameInfo( PropertySet& rSheetProps, const OUString& rPrefix ) :
+							maSheetProps( rSheetProps ), maPrefix( rPrefix ) {}
 };
 
 typedef ::std::set< OUString >              CodeNameSet;
@@ -79,65 +77,67 @@ typedef ::std::list< SheetCodeNameInfo >    SheetCodeNameInfoList;
 
 void ExcelVbaProject::prepareImport()
 {
-    /*  Check if the sheets have imported codenames. Generate new unused
-        codenames if not. */
-    if( mxDocument.is() ) try
-    {
-        // collect existing codenames (do not use them when creating new codenames)
-        CodeNameSet aUsedCodeNames;
+	/*  Check if the sheets have imported codenames. Generate new unused
+	    codenames if not. */
+	if( mxDocument.is() ) try
+	{
+		// collect existing codenames (do not use them when creating new codenames)
+		CodeNameSet aUsedCodeNames;
 
-        // collect sheets without codenames
-        SheetCodeNameInfoList aCodeNameInfos;
+		// collect sheets without codenames
+		SheetCodeNameInfoList aCodeNameInfos;
 
-        // iterate over all imported sheets
-        Reference< XEnumerationAccess > xSheetsEA( mxDocument->getSheets(), UNO_QUERY_THROW );
-        Reference< XEnumeration > xSheetsEnum( xSheetsEA->createEnumeration(), UNO_SET_THROW );
-        // own try/catch for every sheet
-        while( xSheetsEnum->hasMoreElements() ) try
-        {
-            PropertySet aSheetProp( xSheetsEnum->nextElement() );
-            OUString aCodeName;
-            aSheetProp.getProperty( aCodeName, PROP_CodeName );
-            if( aCodeName.getLength() > 0 )
-            {
-                aUsedCodeNames.insert( aCodeName );
-            }
-            else
-            {
-                // TODO: once we have chart sheets we need a switch/case on sheet type ('SheetNNN' vs. 'ChartNNN')
-                aCodeNameInfos.push_back( SheetCodeNameInfo( aSheetProp, CREATE_OUSTRING( "Sheet" ) ) );
-            }
-        }
-        catch( Exception& )
-        {
-        }
+		// iterate over all imported sheets
+		Reference< XEnumerationAccess > xSheetsEA( mxDocument->getSheets(), UNO_QUERY_THROW );
+		Reference< XEnumeration > xSheetsEnum( xSheetsEA->createEnumeration(), UNO_SET_THROW );
+		// own try/catch for every sheet
+		while( xSheetsEnum->hasMoreElements() ) try
+		{
+			PropertySet aSheetProp( xSheetsEnum->nextElement() );
+			OUString aCodeName;
+			aSheetProp.getProperty( aCodeName, PROP_CodeName );
+			if( aCodeName.getLength() > 0 )
+			{
+				aUsedCodeNames.insert( aCodeName );
+			}
+			else
+			{
+				// TODO: once we have chart sheets we need a switch/case on sheet type ('SheetNNN' vs. 'ChartNNN')
+				aCodeNameInfos.push_back( SheetCodeNameInfo( aSheetProp, CREATE_OUSTRING( "Sheet" ) ) );
+			}
+		}
+		catch( Exception& )
+		{
+		}
 
-        // create new codenames if sheets do not have one
-        for( SheetCodeNameInfoList::iterator aIt = aCodeNameInfos.begin(), aEnd = aCodeNameInfos.end(); aIt != aEnd; ++aIt )
-        {
-            // search for an unused codename
-            sal_Int32 nCounter = 1;
-            OUString aCodeName;
-            do
-            {
-                aCodeName = OUStringBuffer( aIt->maPrefix ).append( nCounter++ ).makeStringAndClear();
-            }
-            while( aUsedCodeNames.count( aCodeName ) > 0 );
-            aUsedCodeNames.insert( aCodeName );
+		// create new codenames if sheets do not have one
+		for( SheetCodeNameInfoList::iterator aIt = aCodeNameInfos.begin(), aEnd = aCodeNameInfos.end(); aIt != aEnd; ++aIt )
+		{
+			// search for an unused codename
+			sal_Int32 nCounter = 1;
+			OUString aCodeName;
+			do
+			{
+				aCodeName = OUStringBuffer( aIt->maPrefix ).append( nCounter++ ).makeStringAndClear();
+			}
+			while( aUsedCodeNames.count( aCodeName ) > 0 );
+			aUsedCodeNames.insert( aCodeName );
 
-            // set codename at sheet
-            aIt->maSheetProps.setProperty( PROP_CodeName, aCodeName );
+			// set codename at sheet
+			aIt->maSheetProps.setProperty( PROP_CodeName, aCodeName );
 
-            // tell base class to create a dummy module
-            addDummyModule( aCodeName, ModuleType::DOCUMENT );
-        }
-    }
-    catch( Exception& )
-    {
-    }
+			// tell base class to create a dummy module
+			addDummyModule( aCodeName, ModuleType::DOCUMENT );
+		}
+	}
+	catch( Exception& )
+	{
+	}
 }
 
 // ============================================================================
 
 } // namespace xls
 } // namespace oox
+
+/* vim: set noet sw=4 ts=4: */
