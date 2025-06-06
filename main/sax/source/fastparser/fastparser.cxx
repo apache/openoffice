85ec52e3ec50dfd34a97af98ddb418c203742409
Damjan Jovanovic <damjan@apache.org>
2025-06-06 05:45:43 +0200

    Port main/sax component_getFactory() to the component context API.
    Patch by: me

--- main/sax/source/fastparser/fastparser.cxx
+++ main/sax/source/fastparser/fastparser.cxx
@@ -539,6 +539,11 @@ void FastSaxParser::setLocale( const Locale & Locale ) throw (RuntimeException)
 	maData.maLocale = Locale;
 }
 
+OUString FastSaxParser::getImplementationName_Static(void)
+{
+    return OUString::createFromAscii( PARSER_IMPLEMENTATION_NAME );
+}
+
 Sequence< OUString > FastSaxParser::getSupportedServiceNames_Static(void)
 {
 	Sequence<OUString> aRet(1);
@@ -549,7 +554,7 @@ Sequence< OUString > FastSaxParser::getSupportedServiceNames_Static(void)
 // XServiceInfo
 OUString FastSaxParser::getImplementationName() throw (RuntimeException)
 {
-    return OUString::createFromAscii( PARSER_IMPLEMENTATION_NAME );
+    return getImplementationName_Static();
 }
 
 // XServiceInfo
