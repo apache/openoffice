04b6b6870a929068b71eb32da22b56d2ed2733d5
mseidel <mseidel@apache.org>
2025-08-27 22:04:43 +0200

    Cut and Paste of Fontwork performs as Copy and Paste.
    This commit adresses the issue. See:
    
    https://bz.apache.org/ooo/show_bug.cgi?id=90322
    
    Many thanks to Buo-ren Lin who analyzed the problem and provided the code under Alv2..
    
    (cherry picked from commit 546f7f5999ca912889b4487dfd727a602cdb1ebc)

--- main/sw/source/ui/wrtsh/delete.cxx
+++ main/sw/source/ui/wrtsh/delete.cxx
@@ -259,6 +259,8 @@ long SwWrtShell::DelRight()
 		nSelection = nsSelectionType::SEL_TBL;
 	if(nSelection & nsSelectionType::SEL_TXT)
 		nSelection = nsSelectionType::SEL_TXT;
+	if(nSelection & nsSelectionType::SEL_FONTWORK)
+		nSelection = nsSelectionType::SEL_DRW;
 
 	const SwTableNode * pWasInTblNd = NULL;
 
