bf1b25cd889b02ff0f0810153811f35fe632c3fe
mseidel <mseidel@apache.org>
2025-11-15 15:18:22 +0100

    Cleanup
    (cherry picked from commit a4bc93b07728844d9952f8a7879d86d0f8968835)

--- main/sw/source/ui/table/convert.cxx
+++ main/sw/source/ui/table/convert.cxx
@@ -19,16 +19,12 @@
  *
  *************************************************************/
 
-
-
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sw.hxx"
-
 
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
-
 
 #include <vcl/msgbox.hxx>
 #include <svl/stritem.hxx>
@@ -53,56 +49,56 @@ namespace swui
 	SwAbstractDialogFactory * GetFactory();
 }
 
-//keep the state of the buttons on runtime
+// keep the state of the buttons on runtime
 static int nSaveButtonState = -1; // 0: tab, 1: semicolon, 2: paragraph, 3: other, -1: not yet used
 static sal_Bool bIsKeepColumn = sal_True;
 static sal_Unicode uOther = ',';
 
-void SwConvertTableDlg::GetValues(  sal_Unicode& rDelim,
-									SwInsertTableOptions& rInsTblOpts,
-									SwTableAutoFmt *& prTAFmt )
+void SwConvertTableDlg::GetValues( sal_Unicode& rDelim,
+								   SwInsertTableOptions& rInsTblOpts,
+								   SwTableAutoFmt *& prTAFmt )
 {
 	if( aTabBtn.IsChecked() )
-    {
-        //0x0b mustn't be set when re-converting table into text
-        bIsKeepColumn = !aKeepColumn.IsVisible() || aKeepColumn.IsChecked();
-        rDelim = bIsKeepColumn ? 0x09 : 0x0b;
-        nSaveButtonState = 0;
-    }
+	{
+		// 0x0b mustn't be set when re-converting table into text
+		bIsKeepColumn = !aKeepColumn.IsVisible() || aKeepColumn.IsChecked();
+		rDelim = bIsKeepColumn ? 0x09 : 0x0b;
+		nSaveButtonState = 0;
+	}
 	else if( aSemiBtn.IsChecked() )
-    {
+	{
 		rDelim = ';';
-        nSaveButtonState = 1;
-    }
-    else if( aOtherBtn.IsChecked() && aOtherEd.GetText().Len() )
-    {
-        uOther = aOtherEd.GetText().GetChar( 0 );
-        rDelim = uOther;
-        nSaveButtonState = 3;
-    }
+		nSaveButtonState = 1;
+	}
+	else if( aOtherBtn.IsChecked() && aOtherEd.GetText().Len() )
+	{
+		uOther = aOtherEd.GetText().GetChar( 0 );
+		rDelim = uOther;
+		nSaveButtonState = 3;
+	}
 	else
-    {
-        nSaveButtonState = 2;
-        rDelim = cParaDelim;
-        if(aOtherBtn.IsChecked())
-        {
-            nSaveButtonState = 3;
-            uOther = 0;
-        }
-    }
+	{
+		nSaveButtonState = 2;
+		rDelim = cParaDelim;
+		if(aOtherBtn.IsChecked())
+		{
+			nSaveButtonState = 3;
+			uOther = 0;
+		}
+	}
 
 
 	sal_uInt16 nInsMode = 0;
 	if (aBorderCB.IsChecked())
-        nInsMode |= tabopts::DEFAULT_BORDER;
+		nInsMode |= tabopts::DEFAULT_BORDER;
 	if (aHeaderCB.IsChecked())
-        nInsMode |= tabopts::HEADLINE;
+		nInsMode |= tabopts::HEADLINE;
 	if (aRepeatHeaderCB.IsEnabled() && aRepeatHeaderCB.IsChecked())
-        rInsTblOpts.mnRowsToRepeat = sal_uInt16( aRepeatHeaderNF.GetValue() );
+		rInsTblOpts.mnRowsToRepeat = sal_uInt16( aRepeatHeaderNF.GetValue() );
 	else
 		rInsTblOpts.mnRowsToRepeat = 0;
 	if (!aDontSplitCB.IsChecked())
-        nInsMode |= tabopts::SPLIT_LAYOUT;
+		nInsMode |= tabopts::SPLIT_LAYOUT;
 
 	if( pTAutoFmt )
 		prTAFmt = new SwTableAutoFmt( *pTAutoFmt );
@@ -117,27 +113,27 @@ SwConvertTableDlg::SwConvertTableDlg( SwView& rView, bool bToTable )
 #ifdef MSC
 #pragma warning (disable : 4355)
 #endif
-    aTabBtn         (this, SW_RES(CB_TAB)),
+	aTabBtn			(this, SW_RES(CB_TAB)),
 	aSemiBtn		(this, SW_RES(CB_SEMI)),
 	aParaBtn		(this, SW_RES(CB_PARA)),
-    aOtherBtn       (this, SW_RES(RB_OTHER)),
-    aOtherEd        (this, SW_RES(ED_OTHER)),
-    aKeepColumn     (this, SW_RES(CB_KEEPCOLUMN)),
-    aDelimFL       (this, SW_RES(FL_DELIM)),
+	aOtherBtn		(this, SW_RES(RB_OTHER)),
+	aOtherEd		(this, SW_RES(ED_OTHER)),
+	aKeepColumn		(this, SW_RES(CB_KEEPCOLUMN)),
+	aDelimFL		(this, SW_RES(FL_DELIM)),
 
-    aHeaderCB       (this, SW_RES(CB_HEADER)),
+	aHeaderCB		(this, SW_RES(CB_HEADER)),
 	aRepeatHeaderCB	(this, SW_RES(CB_REPEAT_HEADER)),
 
-    aRepeatHeaderFT         (this, SW_RES(FT_REPEAT_HEADER)),
-    aRepeatHeaderBeforeFT   (this),
-    aRepeatHeaderNF         (this, SW_RES(NF_REPEAT_HEADER)),
-    aRepeatHeaderAfterFT    (this),
-    aRepeatHeaderCombo      (this, SW_RES(WIN_REPEAT_HEADER), aRepeatHeaderNF, aRepeatHeaderBeforeFT, aRepeatHeaderAfterFT),
+	aRepeatHeaderFT			(this, SW_RES(FT_REPEAT_HEADER)),
+	aRepeatHeaderBeforeFT	(this),
+	aRepeatHeaderNF			(this, SW_RES(NF_REPEAT_HEADER)),
+	aRepeatHeaderAfterFT	(this),
+	aRepeatHeaderCombo		(this, SW_RES(WIN_REPEAT_HEADER), aRepeatHeaderNF, aRepeatHeaderBeforeFT, aRepeatHeaderAfterFT),
 
-    aOptionsFL      (this, SW_RES(FL_OPTIONS)),
+	aOptionsFL		(this, SW_RES(FL_OPTIONS)),
 	aDontSplitCB	(this, SW_RES(CB_DONT_SPLIT)),
 	aBorderCB		(this, SW_RES(CB_BORDER)),
-    aAutoFmtBtn(this,SW_RES(BT_AUTOFORMAT)),
+	aAutoFmtBtn(this,SW_RES(BT_AUTOFORMAT)),
 
 	aOkBtn(this,SW_RES(BT_OK)),
 	aCancelBtn(this,SW_RES(BT_CANCEL)),
@@ -145,54 +141,54 @@ SwConvertTableDlg::SwConvertTableDlg( SwView& rView, bool bToTable )
 #ifdef MSC
 #pragma warning (default : 4355)
 #endif
-    sConvertTextTable(SW_RES(STR_CONVERT_TEXT_TABLE)),
+	sConvertTextTable(SW_RES(STR_CONVERT_TEXT_TABLE)),
 	pTAutoFmt( 0 ),
-    pShell( &rView.GetWrtShell() )
+	pShell( &rView.GetWrtShell() )
 {
 	aOtherEd.SetAccessibleName(String(SW_RES(STR_SYMBOL)));
 	aOtherEd.SetAccessibleRelationLabeledBy(&aOtherBtn);
 	FreeResource();
-    if(nSaveButtonState > -1)
-    {
-        switch (nSaveButtonState)
-        {
-            case 0:
-                aTabBtn.Check();
-                aKeepColumn.Check(bIsKeepColumn);
-            break;
-            case 1: aSemiBtn.Check();break;
-            case 2: aParaBtn.Check();break;
-            case 3:
-                aOtherBtn.Check();
-                if(uOther)
-                    aOtherEd.SetText(uOther);
-            break;
-        }
+	if(nSaveButtonState > -1)
+	{
+		switch (nSaveButtonState)
+		{
+			case 0:
+				aTabBtn.Check();
+				aKeepColumn.Check(bIsKeepColumn);
+			break;
+			case 1: aSemiBtn.Check();break;
+			case 2: aParaBtn.Check();break;
+			case 3:
+				aOtherBtn.Check();
+				if(uOther)
+					aOtherEd.SetText(uOther);
+			break;
+		}
 
-    }
-    if( bToTable )
+	}
+	if( bToTable )
 	{
 		SetText( sConvertTextTable );
 		aAutoFmtBtn.SetClickHdl(LINK(this, SwConvertTableDlg, AutoFmtHdl));
 		aAutoFmtBtn.Show();
 		aKeepColumn.Show();
 		aKeepColumn.Enable( aTabBtn.IsChecked() );
-        aRepeatHeaderCombo.Arrange( aRepeatHeaderFT );
+		aRepeatHeaderCombo.Arrange( aRepeatHeaderFT );
 	}
 	else
 	{
-		//Einfuege-Optionen verstecken
-		aHeaderCB		   .Show(sal_False);
-		aRepeatHeaderCB	   .Show(sal_False);
-		aDontSplitCB	   .Show(sal_False);
-		aBorderCB		   .Show(sal_False);
-        aOptionsFL         .Show(sal_False);
-        aRepeatHeaderCombo.Show(sal_False);
+		// Einfüge-Optionen verstecken
+		aHeaderCB			.Show(sal_False);
+		aRepeatHeaderCB		.Show(sal_False);
+		aDontSplitCB		.Show(sal_False);
+		aBorderCB			.Show(sal_False);
+		aOptionsFL			.Show(sal_False);
+	aRepeatHeaderCombo		.Show(sal_False);
 
-		//Groesse anpassen
-        Size aSize(GetSizePixel());
-        aSize.Height() = 8 + aHelpBtn.GetSizePixel().Height() + aHelpBtn.GetPosPixel().Y();
-        SetOutputSizePixel(aSize);
+		// Größe anpassen
+		Size aSize(GetSizePixel());
+		aSize.Height() = 8 + aHelpBtn.GetSizePixel().Height() + aHelpBtn.GetPosPixel().Y();
+		SetOutputSizePixel(aSize);
 	}
 	aKeepColumn.SaveValue();
 
@@ -210,10 +206,10 @@ SwConvertTableDlg::SwConvertTableDlg( SwView& rView, bool bToTable )
 	SwInsertTableOptions aInsOpts = pModOpt->GetInsTblFlags(bHTMLMode);
 	sal_uInt16 nInsTblFlags = aInsOpts.mnInsMode;
 
-    aHeaderCB.Check( 0 != (nInsTblFlags & tabopts::HEADLINE) );
-    aRepeatHeaderCB.Check(aInsOpts.mnRowsToRepeat > 0);
-    aDontSplitCB.Check( 0 == (nInsTblFlags & tabopts::SPLIT_LAYOUT));
-    aBorderCB.Check( 0!= (nInsTblFlags & tabopts::DEFAULT_BORDER) );
+	aHeaderCB.Check( 0 != (nInsTblFlags & tabopts::HEADLINE) );
+	aRepeatHeaderCB.Check(aInsOpts.mnRowsToRepeat > 0);
+	aDontSplitCB.Check( 0 == (nInsTblFlags & tabopts::SPLIT_LAYOUT));
+	aBorderCB.Check( 0!= (nInsTblFlags & tabopts::DEFAULT_BORDER) );
 
 	aHeaderCB.SetClickHdl(LINK(this, SwConvertTableDlg, CheckBoxHdl));
 	aRepeatHeaderCB.SetClickHdl(LINK(this, SwConvertTableDlg, ReapeatHeaderCheckBoxHdl));
@@ -228,14 +224,14 @@ SwConvertTableDlg::	~SwConvertTableDlg()
 
 IMPL_LINK( SwConvertTableDlg, AutoFmtHdl, PushButton*, pButton )
 {
-    SwAbstractDialogFactory* pFact = swui::GetFactory();
-    DBG_ASSERT(pFact, "SwAbstractDialogFactory fail!");
+	SwAbstractDialogFactory* pFact = swui::GetFactory();
+	DBG_ASSERT(pFact, "SwAbstractDialogFactory fail!");
 
-    AbstractSwAutoFormatDlg* pDlg = pFact->CreateSwAutoFormatDlg(pButton, pShell, DLG_AUTOFMT_TABLE, sal_False, pTAutoFmt);
-    DBG_ASSERT(pDlg, "Dialogdiet fail!");
-    if( RET_OK == pDlg->Execute())
-        pDlg->FillAutoFmtOfIndex( pTAutoFmt );
-    delete pDlg;
+	AbstractSwAutoFormatDlg* pDlg = pFact->CreateSwAutoFormatDlg(pButton, pShell, DLG_AUTOFMT_TABLE, sal_False, pTAutoFmt);
+	DBG_ASSERT(pDlg, "Dialogdiet fail!");
+	if( RET_OK == pDlg->Execute())
+		pDlg->FillAutoFmtOfIndex( pTAutoFmt );
+	delete pDlg;
 	return 0;
 }
 
@@ -268,10 +264,12 @@ IMPL_LINK(SwConvertTableDlg, CheckBoxHdl, CheckBox*, EMPTYARG)
 
 IMPL_LINK(SwConvertTableDlg, ReapeatHeaderCheckBoxHdl, void*, EMPTYARG)
 {
-    sal_Bool bEnable = aHeaderCB.IsChecked() && aRepeatHeaderCB.IsChecked();
-    aRepeatHeaderBeforeFT.Enable(bEnable);
-    aRepeatHeaderAfterFT.Enable(bEnable);
-    aRepeatHeaderNF.Enable(bEnable);
+	sal_Bool bEnable = aHeaderCB.IsChecked() && aRepeatHeaderCB.IsChecked();
+	aRepeatHeaderBeforeFT.Enable(bEnable);
+	aRepeatHeaderAfterFT.Enable(bEnable);
+	aRepeatHeaderNF.Enable(bEnable);
 
 	return 0;
 }
+
+/* vim: set noet sw=4 ts=4: */
