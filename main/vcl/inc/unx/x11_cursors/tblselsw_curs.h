56f6108c19104a09e3db4b4e6a75ddee5e807a44
mseidel <mseidel@apache.org>
2025-12-16 16:07:40 +0100

    Next updated X11 cursor

--- main/vcl/inc/unx/x11_cursors/tblselsw_curs.h
+++ main/vcl/inc/unx/x11_cursors/tblselsw_curs.h
@@ -4,7 +4,7 @@
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License,Version 2.0 (the
+ * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
@@ -12,13 +12,12 @@
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS,WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND,either express or implied.  See the License for the
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  *
  *************************************************************/
-
 
 #define tblselsw_curs_width 16
 #define tblselsw_curs_height 16
@@ -26,19 +25,19 @@
 #define tblselsw_curs_y_hot 14
 
 static const unsigned char tblselsw_curs_bits[] = {
-	0x00,0x00,
-	0x00,0x00,
-	0x00,0x00,
-	0x00,0x00,
-	0x00,0x06,
-	0x00,0x0f,
-	0x80,0x0f,
-	0xc0,0x07,
-	0xe2,0x03,
-	0xf6,0x01,
-	0xfe,0x00,
-	0x7e,0x00,
-	0x3e,0x00,
-	0x7e,0x00,
-	0xfe,0x00,
-	0x00,0x00};
+	0x00,0x00,	// 0000 0000 0000 0000
+	0x00,0x00,	// 0000 0000 0000 0000
+	0x00,0x00,	// 0000 0000 0000 0000
+	0x40,0x00,	// 0010 0000 0000 0000
+	0xC0,0x00,	// 0011 0000 0000 0000
+	0xC1,0x00,	// 0011 1000 0000 0000
+	0xC3,0x00,	// 0011 1100 0000 0000
+	0xC7,0x00,	// 0011 1110 0000 0000
+	0xCF,0x00,	// 0011 1111 0000 0000
+	0xCF,0x10,	// 0011 1111 1000 0000
+	0xCF,0x30,	// 0011 1111 1100 0000
+	0xCF,0x70,	// 0011 1111 1110 0000
+	0xCF,0xF0,	// 0011 1111 1111 0000
+	0xCF,0xF1,	// 0011 1111 1111 1000
+	0x00,0x00,	// 0000 0000 0000 0000
+	0x00,0x00};	// 0000 0000 0000 0000
