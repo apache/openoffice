083b2232d8f8b2c575804666ca87d7a6e8058818
Peter Kovacs <petko@apache.org>
2026-03-29 01:30:37 +0100

    added rasqual without patches fix to sha gen script windows

--- build/tools/sha256-file.ps1
+++ build/tools/sha256-file.ps1
@@ -6,4 +6,3 @@ $hash = (Get-FileHash $FilePath -Algorithm SHA256).Hash
 $bytes = [byte[]] -split ($hash -replace '..', '0x$& ')
 $base64 = [Convert]::ToBase64String($bytes)
 Write-Output "sha256-$base64"
-s
