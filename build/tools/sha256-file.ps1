81aeccc5200dcfb6d630a9cdecc47dc9e1fd92e6
Peter Kovacs <petko@apache.org>
2026-03-29 01:20:56 +0100

    migrate redland without patches

---
+++ build/tools/sha256-file.ps1
@@ -0,0 +1,9 @@
+param(
+    [string]$FilePath
+)
+
+$hash = (Get-FileHash $FilePath -Algorithm SHA256).Hash
+$bytes = [byte[]] -split ($hash -replace '..', '0x$& ')
+$base64 = [Convert]::ToBase64String($bytes)
+Write-Output "sha256-$base64"
+s
