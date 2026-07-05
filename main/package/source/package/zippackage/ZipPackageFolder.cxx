apache/openoffice

commit 1972b29432a5b05e40ec16d939ddb345b981e240

Correct possible issues with ZIP files

main/package/source/package/zippackage/ZipPackageFolder.cxx
@@ -75,7 +75,9 @@ ZipPackageFolder::ZipPackageFolder ( const uno::Reference< XMultiServiceFactory 
 	aEntry.nCrc			= 0;
 	aEntry.nCompressedSize	= 0;
 	aEntry.nSize		= 0;
-	aEntry.nOffset		= -1;
+	aEntry.nFileHeaderOffset		= -1;
+	aEntry.nFileDataOffset		= -1;
+	aEntry.bHasDataDescriptor = sal_False;
 	uno::Sequence < sal_Int8 > &rCachedImplId = lcl_CachedImplId::get();
 	if ( !rCachedImplId.getLength() )
 	    rCachedImplId = getImplementationId();
@@ -176,10 +178,13 @@ void ZipPackageFolder::copyZipEntry( ZipEntry &rDest, const ZipEntry &rSource)
     rDest.nCrc				= rSource.nCrc;
     rDest.nCompressedSize	= rSource.nCompressedSize;
     rDest.nSize				= rSource.nSize;
-    rDest.nOffset			= rSource.nOffset;
+    rDest.nFileHeaderOffset	= rSource.nFileHeaderOffset;
+	rDest.nFileDataOffset	= rSource.nFileDataOffset;
     rDest.sPath				= rSource.sPath;
     rDest.nPathLen			= rSource.nPathLen;
-    rDest.nExtraLen			= rSource.nExtraLen;
+    rDest.nLOCExtraLen		= rSource.nLOCExtraLen;
+	rDest.nCENExtraLen		= rSource.nCENExtraLen;
+	rDest.bHasDataDescriptor= rSource.bHasDataDescriptor;
 }
 
 const ::com::sun::star::uno::Sequence < sal_Int8 >& ZipPackageFolder::static_getImplementationId()
@@ -669,7 +674,7 @@ bool ZipPackageFolder::saveChild( const ::rtl::OUString &rShortName, const Conte
             if ( rInfo.pStream->IsEncrypted() )
                 rInfo.pStream->setSize( nOwnStreamOrigSize );
 
-            rInfo.pStream->aEntry.nOffset *= -1;
+            rInfo.pStream->aEntry.nFileDataOffset = rInfo.pStream->aEntry.nFileHeaderOffset;
         }
     }
 
@@ -692,7 +697,7 @@ void ZipPackageFolder::saveContents( ::rtl::OUString &rPath, std::vector < uno::
 		ZipEntry* pTempEntry = new ZipEntry();
 		ZipPackageFolder::copyZipEntry ( *pTempEntry, aEntry );
 		pTempEntry->nPathLen = (sal_Int16)( ::rtl::OUStringToOString( rPath, RTL_TEXTENCODING_UTF8 ).getLength() );
-		pTempEntry->nExtraLen = -1;
+		pTempEntry->nLOCExtraLen = -1;
 		pTempEntry->sPath = rPath;
 
 		try

main/comphelper/source/misc/storagehelper.cxx
@@ -506,7 +506,7 @@ sal_Bool OStorageHelper::IsValidZipEntryFileName(
             case ':':
                 return sal_False;
             case '/':
-                if ( !bSlashAllowed )
+                if ( ( i == 0 ) || !bSlashAllowed )
                     return sal_False;
                 break;
             default:

main/package/inc/ZipEntry.hxx
@@ -25,18 +25,52 @@
 
 #include <rtl/ustring.hxx>
 
+/// Data from a central directory header.
 struct ZipEntry
 {
+    /// version needed to extract
     sal_Int16 nVersion;
+    /// general purpose bit flag
     sal_Int16 nFlag;
+    /// compression method
     sal_Int16 nMethod;
+    /// last mod file date & time
     sal_Int32 nTime;
+    /// crc-32
     sal_Int32 nCrc;
+    /// compressed size
     sal_Int32 nCompressedSize;
+    /// uncompressed size
     sal_Int32 nSize;
-    sal_Int32 nOffset;
+    /// (Absolute) offset to the local file header
+    sal_Int32 nFileHeaderOffset;
+    /// (Absolute) offset to the file data
+    sal_Int32 nFileDataOffset;
+    /// file name length
     sal_Int16 nPathLen;
-    sal_Int16 nExtraLen;
+    /// extra field length of local file header
+    sal_Int16 nLOCExtraLen;
+    /// extra field length of central directory header
+    sal_Int16 nCENExtraLen;
+    /// file name
     ::rtl::OUString sPath;
+    /// True if the entry has a data descriptor after the file data
+    sal_Bool bHasDataDescriptor;
+    /// Default constructor
+    ZipEntry():
+        nVersion(-1),
+        nFlag(0),
+        nMethod(0),
+        nTime(-1),
+        nCrc(0),
+        nCompressedSize(0),
+        nSize(0),
+        nFileHeaderOffset(-1),
+        nFileDataOffset(-1),
+        nPathLen(0),
+        nLOCExtraLen(0),
+        nCENExtraLen(0),
+        bHasDataDescriptor(sal_False) {
+    }
 };
 #endif

main/package/inc/ZipFile.hxx
@@ -194,10 +194,20 @@ public:
 
     ZipEnumeration * SAL_CALL entries(  );
 protected:
+	/** Decode a local file header of an entry and update a ZipEntry
+	 *
+	 * @param rEntry entry with the nFileHeaderOffset field populated. At return, the ZipEntry::nFileDataOffset and ZipEntry::nLOCExtraLen fields will be set.
+	 *
+	 * Also searches for a data descriptor after the file data and validates it.
+	 *
+	 * @return sal_True if successful, otherwise throws exceptions.
+	 */
 	sal_Bool		readLOC ( ZipEntry &rEntry)
 		throw(::com::sun::star::io::IOException, com::sun::star::packages::zip::ZipException, com::sun::star::uno::RuntimeException);
+	/// Decode the End of central directory record
 	sal_Int32		readCEN()
 		throw(::com::sun::star::io::IOException, com::sun::star::packages::zip::ZipException, com::sun::star::uno::RuntimeException);
+	/// Find the End of central directory record
 	sal_Int32		findEND()
 		throw(::com::sun::star::io::IOException, com::sun::star::packages::zip::ZipException, com::sun::star::uno::RuntimeException);
 	sal_Int32 		recover()

main/package/source/package/zipapi/XUnbufferedStream.cxx
@@ -80,16 +80,16 @@ XUnbufferedStream::XUnbufferedStream(
 , mnMyCurrent ( 0 )
 , mbCheckCRC( !bRecoveryMode )
 {
-	mnZipCurrent = maEntry.nOffset;
+	mnZipCurrent = maEntry.nFileDataOffset;
 	if ( mbRawStream )
 	{
 		mnZipSize = maEntry.nMethod == DEFLATED ? maEntry.nCompressedSize : maEntry.nSize;
-		mnZipEnd = maEntry.nOffset + mnZipSize;
+		mnZipEnd = maEntry.nFileDataOffset + mnZipSize;
 	}
 	else
 	{
 		mnZipSize = maEntry.nSize;
-		mnZipEnd = maEntry.nMethod == DEFLATED ? maEntry.nOffset + maEntry.nCompressedSize : maEntry.nOffset + maEntry.nSize;
+		mnZipEnd = maEntry.nMethod == DEFLATED ? maEntry.nFileDataOffset + maEntry.nCompressedSize : maEntry.nFileDataOffset + maEntry.nSize;
 	}
 	sal_Bool bHaveEncryptData = ( rData.is() && rData->m_aSalt.getLength() && rData->m_aInitVector.getLength() && rData->m_nIterationCount != 0 ) ? sal_True : sal_False;
 	sal_Bool bMustDecrypt = ( nStreamMode == UNBUFF_STREAM_DATA && bHaveEncryptData && bIsEncrypted ) ? sal_True : sal_False;

main/package/source/package/zipapi/ZipOutputStream.cxx
@@ -110,7 +110,7 @@ void SAL_CALL ZipOutputStream::putNextEntry( ZipEntry& rEntry,
 		m_pCurrentStream = pStream;
 	}
 	sal_Int32 nLOCLength = writeLOC(rEntry);
-	rEntry.nOffset = static_cast < sal_Int32 > (aChucker.GetPosition()) - nLOCLength;
+	rEntry.nFileHeaderOffset = static_cast < sal_Int32 > (aChucker.GetPosition()) - nLOCLength;
 	aZipList.push_back( &rEntry );
 	pCurrentEntry = &rEntry;
 }
@@ -239,10 +239,10 @@ void SAL_CALL ZipOutputStream::finish(  )
 	if (aZipList.size() < 1)
 		OSL_ENSURE(false,"Zip file must have at least one entry!\n");
 
-	sal_Int32 nOffset= static_cast < sal_Int32 > (aChucker.GetPosition());
+	sal_Int32 nCENOffset= static_cast < sal_Int32 > (aChucker.GetPosition());
 	for (sal_Int32 i =0, nEnd = aZipList.size(); i < nEnd; i++)
 		writeCEN( *aZipList[i] );
-	writeEND( nOffset, static_cast < sal_Int32 > (aChucker.GetPosition()) - nOffset);
+	writeEND( nCENOffset, static_cast < sal_Int32 > (aChucker.GetPosition()) - nCENOffset);
 	bFinished = sal_True;
 	xStream->flush();
 }
@@ -296,7 +296,7 @@ void ZipOutputStream::doDeflate()
     }
 }
 
-void ZipOutputStream::writeEND(sal_uInt32 nOffset, sal_uInt32 nLength)
+void ZipOutputStream::writeEND(sal_uInt32 nCENOffset, sal_uInt32 nLength)
 	throw(IOException, RuntimeException)
 {
 	aChucker << ENDSIG;
@@ -305,7 +305,7 @@ void ZipOutputStream::writeEND(sal_uInt32 nOffset, sal_uInt32 nLength)
 	aChucker << static_cast < sal_Int16 > ( aZipList.size() );
 	aChucker << static_cast < sal_Int16 > ( aZipList.size() );
 	aChucker << nLength;
-	aChucker << nOffset;
+	aChucker << nCENOffset;
 	aChucker << static_cast < sal_Int16 > ( 0 );
 }
 void ZipOutputStream::writeCEN( const ZipEntry &rEntry )
@@ -338,12 +338,12 @@ void ZipOutputStream::writeCEN( const ZipEntry &rEntry )
 	aChucker << rEntry.nCompressedSize;
 	aChucker << rEntry.nSize;
 	aChucker << nNameLength;
-	aChucker << static_cast < sal_Int16> (0);
-	aChucker << static_cast < sal_Int16> (0);
-	aChucker << static_cast < sal_Int16> (0);
-	aChucker << static_cast < sal_Int16> (0);
-	aChucker << static_cast < sal_Int32> (0);
-	aChucker << rEntry.nOffset;
+	aChucker << static_cast < sal_Int16> (0); // extra field length
+	aChucker << static_cast < sal_Int16> (0); // file comment length
+	aChucker << static_cast < sal_Int16> (0); // disk number start
+	aChucker << static_cast < sal_Int16> (0); // internal file attributes
+	aChucker << static_cast < sal_Int32> (0); // external file attributes
+	aChucker << rEntry.nFileHeaderOffset;
 
 	Sequence < sal_Int8 > aSequence( (sal_Int8*)sUTF8Name.getStr(), sUTF8Name.getLength() );
 	aChucker.WriteBytes( aSequence );
@@ -367,40 +367,40 @@ sal_Int32 ZipOutputStream::writeLOC( const ZipEntry &rEntry )
 	sal_Int16 nNameLength 		= static_cast < sal_Int16 > ( sUTF8Name.getLength() );
 
 	aChucker << LOCSIG;
-	aChucker << rEntry.nVersion;
+	aChucker << rEntry.nVersion; // version needed to extract
 
 	if (rEntry.nFlag & (1 << 4) )
 	{
 		// If it's an encrypted entry, we pretend its stored plain text
 		sal_Int16 nTmpFlag = rEntry.nFlag;
 		nTmpFlag &= ~(1 <<4 );
-		aChucker << nTmpFlag;
-		aChucker << static_cast < sal_Int16 > ( STORED );
+		aChucker << nTmpFlag;	// general purpose bit flag
+		aChucker << static_cast < sal_Int16 > ( STORED ); // compression method
 	}
 	else
 	{
-		aChucker << rEntry.nFlag;
-		aChucker << rEntry.nMethod;
+		aChucker << rEntry.nFlag; // general purpose bit flag
+		aChucker << rEntry.nMethod; // compression method
 	}
 
-	aChucker << static_cast < sal_uInt32 > (rEntry.nTime);
+	aChucker << static_cast < sal_uInt32 > (rEntry.nTime); // last mod file time & date
 	if ((rEntry.nFlag & 8) == 8 )
 	{
-		aChucker << static_cast < sal_Int32 > (0);
-		aChucker << static_cast < sal_Int32 > (0);
-		aChucker << static_cast < sal_Int32 > (0);
+		aChucker << static_cast < sal_Int32 > (0); // crc-32
+		aChucker << static_cast < sal_Int32 > (0); // compressed size
+		aChucker << static_cast < sal_Int32 > (0); // uncompressed size
 	}
 	else
 	{
-		aChucker << static_cast < sal_uInt32 > (rEntry.nCrc);
-		aChucker << rEntry.nCompressedSize;
-		aChucker << rEntry.nSize;
+		aChucker << static_cast < sal_uInt32 > (rEntry.nCrc); // crc-32
+		aChucker << rEntry.nCompressedSize; // compressed size
+		aChucker << rEntry.nSize; // uncompressed size
 	}
-	aChucker << nNameLength;
-	aChucker << static_cast < sal_Int16 > (0);
+	aChucker << nNameLength; // file name length
+	aChucker << static_cast < sal_Int16 > (0); // extra field length
 
 	Sequence < sal_Int8 > aSequence( (sal_Int8*)sUTF8Name.getStr(), sUTF8Name.getLength() );
-	aChucker.WriteBytes( aSequence );
+	aChucker.WriteBytes( aSequence ); // file name
 
 	return LOCHDR + nNameLength;
 }

main/package/source/package/zippackage/ZipPackage.cxx
@@ -536,6 +536,7 @@ void ZipPackage::getZipFileContents()
 	OUString sTemp, sDirName;
 	sal_Int32 nOldIndex, nIndex, nStreamIndex;
 	FolderHash::iterator aIter;
+	sal_Bool mustHaveEncryption = sal_False;
 
 	while ( pEnum->hasMoreElements() )
 	{
@@ -591,12 +592,16 @@ void ZipPackage::getZipFileContents()
 			pPkgStream->setName( sTemp );
 			pPkgStream->doSetParent( pCurrent, sal_True );
 		}
+		if ( ( rEntry.nMethod == STORED ) && rEntry.bHasDataDescriptor )
+			mustHaveEncryption = sal_True;
 	}
 
 	if ( m_nFormat == embed::StorageFormats::PACKAGE )
 		parseManifest();
 	else if ( m_nFormat == embed::StorageFormats::OFOPXML )
 		parseContentType();
+	if ( mustHaveEncryption && !m_bHasEncryptedEntries )
+		throw ZipException( OUString( RTL_CONSTASCII_USTRINGPARAM ( "inconsistent ZIP entries" ) ), uno::Reference < XInterface > () );
 }
 
 //--------------------------------------------------------

commit 7f52a288cb10ed2497d68d96ab2828ed813f91a6

Fix the serious regression from commit 1972b29432a5b05e40ec16d939ddb345b981e240, where HSQLDB database documents were getting corrupted and OpenOffice was even crashing.
The ZipEntry.nFileDataOffset field needed to be set to -1 like before,
not to the nFileHeaderOffset.

Patch by: me

main/package/source/package/zippackage/ZipPackageFolder.cxx
@@ -674,7 +674,7 @@ bool ZipPackageFolder::saveChild( const ::rtl::OUString &rShortName, const Conte
             if ( rInfo.pStream->IsEncrypted() )
                 rInfo.pStream->setSize( nOwnStreamOrigSize );
 
-            rInfo.pStream->aEntry.nFileDataOffset = rInfo.pStream->aEntry.nFileHeaderOffset;
+            rInfo.pStream->aEntry.nFileDataOffset = -1;
         }
     }
 