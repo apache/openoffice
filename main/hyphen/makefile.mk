apache/openoffice

commit 7e8ab750cfd3aa68da5fd305f11cfb43d73e574c

We can directly use the latest versions of hunspell/hyphen and mythes

main/hyphen/makefile.mk
@@ -39,14 +39,14 @@ all:
 
 .ELSE
 
-TARFILE_NAME=hyphen-2.7.1
-TARFILE_MD5=48a9f787f43a09c0a9b7b00cd1fddbbf
+TARFILE_NAME=hyphen-2.8.8
+TARFILE_MD5=5ade6ae2a99bc1e9e57031ca88d36dad
 
 ADDITIONAL_FILES += makefile.mk
 
 PATCH_FILES= \
-	hyphen-2.7.1.patch \
-	hyphen-2.7.1-read-charset.patch
+	hyphen-2.8.8.patch \
+	hyphen-2.8.8-read-charset.patch
 
 .IF "$(GUI)"=="UNX"
 CONFIGURE_DIR=$(BUILD_DIR)

main/mythes/makefile.mk
@@ -33,15 +33,14 @@ TARGET=mythes
 # --- Files --------------------------------------------------------
 
 
-TARFILE_NAME=mythes-1.2.0
-TARFILE_MD5=067201ea8b126597670b5eff72e1f66c
+TARFILE_NAME=mythes-1.2.4
+TARFILE_MD5=a8c2c5b8f09e7ede322d5c602ff6a4b6
 
 ADDITIONAL_FILES += makefile.mk
 
-PATCH_FILES=mythes-1.2.0-vanilla-th-gen-idx.patch \
-	mythes-1.2.0-overflow.patch \
-	mythes-1.2.0-makefile-mk.patch \
-	mythes-1.2.0-disable-example.patch
+PATCH_FILES=mythes-1.2.4-vanilla-th-gen-idx.patch \
+	mythes-1.2.4-makefile-mk.patch \
+	mythes-1.2.4-disable-example.patch
 
 .IF "$(GUI)"=="UNX"
 CONFIGURE_DIR=$(BUILD_DIR)
@@ -52,6 +51,10 @@ CONFIGURE_DIR=$(BUILD_DIR)
 # still needed also in system-mythes case as it creates the makefile
 CONFIGURE_ACTION=configure
 CONFIGURE_FLAGS= --disable-shared --with-pic
+# hunspell is only needed by the example program, which is not built
+# (see mythes-1.2.4-disable-example.patch); preset the variables so
+# configure does not abort when no hunspell pkg-config file is around
+CONFIGURE_FLAGS+= HUNSPELL_CFLAGS=-I. HUNSPELL_LIBS=-L.
 
 .IF "$(COM)"=="C52" && "$(CPU)"=="U"
 LCL_CONFIGURE_CFLAGS+=-m64
@@ -80,6 +83,7 @@ OUT2INC += mythes.hxx
 .IF "$(COM)"=="GCC"
 CONFIGURE_ACTION=configure
 CONFIGURE_FLAGS= --disable-shared --with-pic
+CONFIGURE_FLAGS+= HUNSPELL_CFLAGS=-I. HUNSPELL_LIBS=-L.
 
 BUILD_ACTION=make
 

main/external_deps.lst
@@ -44,9 +44,9 @@ if (ENABLE_UNIT_TESTS == YES)
     URL2 = $(OOO_EXTRAS)$(MD5)-$(name)
 
 if ( true )
-    MD5 = 067201ea8b126597670b5eff72e1f66c
-    name = mythes-1.2.0.tar.gz
-    URL1 = https://sourceforge.net/projects/hunspell/files/MyThes/1.2.0/mythes-1.2.0.tar.gz
+    MD5 = a8c2c5b8f09e7ede322d5c602ff6a4b6
+    name = mythes-1.2.4.tar.gz
+    URL1 = https://sourceforge.net/projects/hunspell/files/MyThes/1.2.4/mythes-1.2.4.tar.gz
     URL2 = $(OOO_EXTRAS)$(MD5)-$(name)
 
 if (SYSTEM_MDDS != YES)
@@ -283,9 +283,9 @@ if (WITH_CATB_FONTS == YES)
     URL2 = $(OOO_EXTRAS)$(MD5)-$(name)
 
 if (ENABLE_CATEGORY_B==YES && SYSTEM_HYPH!=YES)
-    MD5 = 48a9f787f43a09c0a9b7b00cd1fddbbf
-    name = hyphen-2.7.1.tar.gz
-    URL1 = https://sourceforge.net/projects/hunspell/files/Hyphen/2.7/$(name)
+    MD5 = 5ade6ae2a99bc1e9e57031ca88d36dad
+    name = hyphen-2.8.8.tar.gz
+    URL1 = https://sourceforge.net/projects/hunspell/files/Hyphen/2.8/$(name)
     URL2 = $(OOO_EXTRAS)$(MD5)-$(name)
 
 if (ENABLE_CATEGORY_B==YES && SYSTEM_HUNSPELL!=YES)

main/hyphen/hyphen-2.7.1-read-charset.patch
@@ -1,20 +1,0 @@
---- misc/hyphen-2.7.1/hyphen.c	2010-12-01 01:47:22.000000000 +0100
-+++ misc/build/hyphen-2.7.1/hyphen.c	2011-01-18 16:26:50.953125000 +0100
-@@ -291,13 +291,10 @@
-   /* read in character set info */
-   if (k == 0) {
-     for (i=0;i<MAX_NAME;i++) dict[k]->cset[i]= 0;
--    if (fgets(dict[k]->cset,  sizeof(dict[k]->cset),f) != NULL) {
--      for (i=0;i<MAX_NAME;i++)
--        if ((dict[k]->cset[i] == '\r') || (dict[k]->cset[i] == '\n'))
--          dict[k]->cset[i] = 0;
--    } else {
--      dict[k]->cset[0] = 0;
--    }
-+    fgets(dict[k]->cset,  sizeof(dict[k]->cset),f);
-+    for (i=0;i<MAX_NAME;i++)
-+      if ((dict[k]->cset[i] == '\r') || (dict[k]->cset[i] == '\n'))
-+        dict[k]->cset[i] = 0;
-     dict[k]->utf8 = (strcmp(dict[k]->cset, "UTF-8") == 0);
-   } else {
-     strcpy(dict[k]->cset, dict[0]->cset);

main/hyphen/hyphen-2.7.1.patch
@@ -1,72 +1,0 @@
---- misc/hyphen-2.7.1/Makefile.am	2010-07-19 11:23:17.000000000 +0200
-+++ misc/build/hyphen-2.7.1/Makefile.am	2010-12-02 10:15:44.390625000 +0100
-@@ -25,14 +25,13 @@
- 
- hyphen.us3:
- 	cp -f $(srcdir)/hyphen.tex hyphen.us
--	patch < $(srcdir)/hyphen.patch
-+	$(GNUPATCH) < $(srcdir)/hyphen.patch
- 	$(srcdir)/tbhyphext.sh <$(srcdir)/tbhyphext.tex >hyphen.us2
- 	cat hyphen.us hyphen.us2 | $(AWK) -f $(srcdir)/lig.awk >hyphen.us3
- 	cat $(srcdir)/ligpatch.txt >>hyphen.us3
- 
--hyph_en_US.dic: hyphen.us3
--	perl $(srcdir)/substrings.pl hyphen.us3 hyphen.us4 UTF-8 2 3 >/dev/null
--	cat hyphen.us4 | $(SED) -f $(srcdir)/ooopatch.sed >hyph_en_US.dic
-+hyph_en_US.dic:
-+	@echo "hyph_en_US.txt distributed with Hyphen library"
- 
- clean-local:
- 	rm -rf hyphen.us* hyph_en_US.dic
---- misc/hyphen-2.7.1/Makefile.in	2010-12-01 02:31:29.000000000 +0100
-+++ misc/build/hyphen-2.7.1/Makefile.in	2010-12-02 10:17:16.546875000 +0100
-@@ -940,14 +940,13 @@
- 
- hyphen.us3:
- 	cp -f $(srcdir)/hyphen.tex hyphen.us
--	patch < $(srcdir)/hyphen.patch
-+	$(GNUPATCH) < $(srcdir)/hyphen.patch
- 	$(srcdir)/tbhyphext.sh <$(srcdir)/tbhyphext.tex >hyphen.us2
- 	cat hyphen.us hyphen.us2 | $(AWK) -f $(srcdir)/lig.awk >hyphen.us3
- 	cat $(srcdir)/ligpatch.txt >>hyphen.us3
- 
--hyph_en_US.dic: hyphen.us3
--	perl $(srcdir)/substrings.pl hyphen.us3 hyphen.us4 UTF-8 2 3 >/dev/null
--	cat hyphen.us4 | $(SED) -f $(srcdir)/ooopatch.sed >hyph_en_US.dic
-+hyph_en_US.dic:
-+	@echo "hyph_en_US.txt distributed with Hyphen library"
- 
- clean-local:
- 	rm -rf hyphen.us* hyph_en_US.dic
---- misc/hyphen-2.7.1/makefile.mk	2010-12-02 10:35:40.265625000 +0100
-+++ misc/build/hyphen-2.7.1/makefile.mk	2010-12-02 10:25:45.750000000 +0100
-@@ -1 +1,28 @@
--dummy
-+PRJ = ..$/..$/..$/..
-+
-+PRJNAME = hyphen
-+TARGET  = hyphen
-+CFLAGSCALL=gsd
-+
-+USE_DEFFILE=TRUE
-+EXTERNAL_WARNINGS_NOT_ERRORS := TRUE
-+UWINAPILIB=
-+
-+.INCLUDE : settings.mk
-+
-+# --- Files --------------------------------------------------------
-+
-+# !! not to be compiled because those belong to a stand alone programs: !!
-+#        $(SLO)$/createfp.obj\
-+#        $(SLO)$/testtextcat.obj
-+
-+SLOFILES=   \
-+        $(SLO)$/hyphen.obj\
-+        $(SLO)$/hnjalloc.obj
-+
-+# --- Targets ------------------------------------------------------
-+
-+ALL: ALLTAR
-+
-+.INCLUDE : target.mk
-+

main/hyphen/prj/d.lst
@@ -1,5 +1,5 @@
 ..\%__SRC%\slb\hyphen.lib     %_DEST%\lib%_EXT%\hyphen.lib
 
 ..\%__SRC%\inc\hyphen.h %_DEST%\inc%_EXT%\hyphen.h
-..\%__SRC%\misc\build\hyphen-2.7.1\.libs\libhyphen.a %_DEST%\lib%_EXT%\libhyphen.a
-..\%__SRC%\misc\build\hyphen-2.7.1\hyph_en_US.dic %_DEST%\bin%_EXT%\hyph_en_US.dic
+..\%__SRC%\misc\build\hyphen-2.8.8\.libs\libhyphen.a %_DEST%\lib%_EXT%\libhyphen.a
+..\%__SRC%\misc\build\hyphen-2.8.8\hyph_en_US.dic %_DEST%\bin%_EXT%\hyph_en_US.dic

main/mythes/mythes-1.2.0-disable-example.patch
@@ -1,79 +1,0 @@
-diff -uNrp misc/build/mythes-1.2.0/configure misc/mythes-1.2.0/configure
---- misc/build/mythes-1.2.0/configure	2010-02-27 12:57:37.000000000 -0300
-+++ misc/mythes-1.2.0/configure	2011-11-21 10:48:03.724435792 -0300
-@@ -748,6 +748,7 @@ LTLIBOBJS
- LIBOBJS
- HUNSPELL_LIBS
- HUNSPELL_CFLAGS
-+ENABLE_EXAMPLE
- PKG_CONFIG
- CXXCPP
- CPP
-@@ -867,6 +868,7 @@ ac_subst_files=''
- ac_user_opts='
- enable_option_checking
- enable_dependency_tracking
-+enable_example
- with_gnu_ld
- enable_rpath
- enable_shared
-@@ -1519,6 +1521,8 @@ Optional Features:
-                           optimize for fast installation [default=yes]
-   --disable-libtool-lock  avoid locking (might break parallel builds)
- 
-+  --enable-example        compiles the example (requires hunspell library)
-+
- Optional Packages:
-   --with-PACKAGE[=ARG]    use PACKAGE [ARG=yes]
-   --without-PACKAGE       do not use PACKAGE (same as --with-PACKAGE=no)
-@@ -3723,6 +3727,19 @@ fi
- 
- 
- 
-+# Check whether --enable-example was given.
-+if test "${enable_example+set}" != set; then :
-+  ENABLE_EXAMPLE=no;
-+  $enable_example=no;
-+else
-+  ENABLE_EXAMPLE=$enable_example;
-+fi
-+
-+
-+
-+
-+
-+
- depcc="$CC"   am_compiler_list=
- 
- { $as_echo "$as_me:${as_lineno-$LINENO}: checking dependency style of $depcc" >&5
-@@ -14476,6 +14493,8 @@ $as_echo "no" >&6; }
- 
- fi
- 
-+
-+if test "$ENABLE_EXAMPLE" != "no"; then
- pkg_failed=no
- { $as_echo "$as_me:${as_lineno-$LINENO}: checking for HUNSPELL" >&5
- $as_echo_n "checking for HUNSPELL... " >&6; }
-@@ -14561,6 +14580,9 @@ else
- $as_echo "yes" >&6; }
- 	:
- fi
-+fi
-+export ENABLE_EXAMPLE
-+
- 
- ac_config_files="$ac_config_files Makefile mythes.pc"
- 
-diff -uNrp misc/build/mythes-1.2.0/Makefile.in misc/mythes-1.2.0/Makefile.in
---- misc/build/mythes-1.2.0/Makefile.in	2010-02-27 12:57:35.000000000 -0300
-+++ misc/mythes-1.2.0/Makefile.in	2011-11-22 20:39:45.589038536 -0300
-@@ -675,7 +675,7 @@ distcleancheck: distclean
- 	       exit 1; } >&2
- check-am: all-am
- check: check-am
--all-am: Makefile $(LTLIBRARIES) $(PROGRAMS) $(SCRIPTS) $(DATA) \
-+all-am: Makefile $(LTLIBRARIES) $(SCRIPTS) $(DATA) \
- 		$(HEADERS) config.h
- installdirs:
- 	for dir in "$(DESTDIR)$(libdir)" "$(DESTDIR)$(bindir)" "$(DESTDIR)$(pkgconfdir)" "$(DESTDIR)$(libmythes_1_2_includedir)"; do \

main/mythes/mythes-1.2.0-makefile-mk.patch
@@ -1,94 +1,0 @@
---- misc/mythes-1.2.0/makefile.mk	2010-05-14 23:36:09.000000000 +0200
-+++ misc/build/mythes-1.2.0/makefile.mk	2010-05-14 23:35:14.000000000 +0200
-@@ -1 +1,90 @@
--dummy
-+#*************************************************************************
-+#
-+#   $RCSfile: makefile.mk,v $
-+#
-+#   $Revision: 1.1.1.1 $
-+#
-+#   last change: $Author: caolan $ $Date: 2010/02/27 15:52:17 $
-+#
-+#   The Contents of this file are made available subject to the terms of
-+#   either of the following licenses
-+#
-+#          - GNU Lesser General Public License Version 2.1
-+#          - Sun Industry Standards Source License Version 1.1
-+#
-+#   Sun Microsystems Inc., October, 2000
-+#
-+#   GNU Lesser General Public License Version 2.1
-+#   =============================================
-+#   Copyright 2000 by Sun Microsystems, Inc.
-+#   901 San Antonio Road, Palo Alto, CA 94303, USA
-+#
-+#   This library is free software; you can redistribute it and/or
-+#   modify it under the terms of the GNU Lesser General Public
-+#   License version 2.1, as published by the Free Software Foundation.
-+#
-+#   This library is distributed in the hope that it will be useful,
-+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-+#   Lesser General Public License for more details.
-+#
-+#   You should have received a copy of the GNU Lesser General Public
-+#   License along with this library; if not, write to the Free Software
-+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-+#   MA  02111-1307  USA
-+#
-+#
-+#   Sun Industry Standards Source License Version 1.1
-+#   =================================================
-+#   The contents of this file are subject to the Sun Industry Standards
-+#   Source License Version 1.1 (the "License"); You may not use this file
-+#   except in compliance with the License. You may obtain a copy of the
-+#   License at http://www.openoffice.org/license.html.
-+#
-+#   Software provided under this License is provided on an "AS IS" basis,
-+#   WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
-+#   WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
-+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
-+#   See the License for the specific provisions governing your rights and
-+#   obligations concerning the Software.
-+#
-+#   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
-+#
-+#   Copyright: 2000 by Sun Microsystems, Inc.
-+#
-+#   All Rights Reserved.
-+#
-+#   Contributor(s): _______________________________________
-+#
-+#
-+#
-+#*************************************************************************
-+
-+PRJ = ../../../..
-+
-+PRJNAME	= mythes
-+TARGET	= mythes
-+LIBTARGET=YES
-+EXTERNAL_WARNINGS_NOT_ERRORS := TRUE
-+
-+#----- Settings ---------------------------------------------------------
-+
-+.INCLUDE : settings.mk
-+
-+# --- Files --------------------------------------------------------
-+
-+all_target: ALLTAR
-+
-+
-+
-+SLOFILES=	\
-+		$(SLO)$/mythes.obj
-+
-+LIB1TARGET= $(SLB)$/lib$(TARGET).lib
-+LIB1ARCHIV= $(LB)/lib$(TARGET).a
-+LIB1OBJFILES= $(SLOFILES)
-+
-+# --- Targets ------------------------------------------------------
-+
-+.INCLUDE : target.mk
-+

commit 56bedd18ba2ee9641bf8fed2988973374575729b

Fix a regression in 7e8ab750cfd3aa68da5fd305f11cfb43d73e574c where hyphen cannot build in Windows, because the makefile.mk generated by main/hyphen/hyphen-2.7.1.patch that Windows uses to build it, was deleted.
Patch by: me

main/hyphen/hyphen-2.8.8-makefile-mk.patch
@@ -1,0 +1,32 @@
+--- misc/hyphen-2.8.8/makefile.mk	2010-12-02 10:35:40.265625000 +0100
++++ misc/build/hyphen-2.8.8/makefile.mk	2010-12-02 10:25:45.750000000 +0100
+@@ -1 +1,28 @@
+-dummy
++PRJ = ..$/..$/..$/..
++
++PRJNAME = hyphen
++TARGET  = hyphen
++CFLAGSCALL=gsd
++
++USE_DEFFILE=TRUE
++EXTERNAL_WARNINGS_NOT_ERRORS := TRUE
++UWINAPILIB=
++
++.INCLUDE : settings.mk
++
++# --- Files --------------------------------------------------------
++
++# !! not to be compiled because those belong to a stand alone programs: !!
++#        $(SLO)$/createfp.obj\
++#        $(SLO)$/testtextcat.obj
++
++SLOFILES=   \
++        $(SLO)$/hyphen.obj\
++        $(SLO)$/hnjalloc.obj
++
++# --- Targets ------------------------------------------------------
++
++ALL: ALLTAR
++
++.INCLUDE : target.mk
++

main/hyphen/makefile.mk
@@ -44,6 +44,9 @@ TARFILE_MD5=5ade6ae2a99bc1e9e57031ca88d36dad
 
 ADDITIONAL_FILES += makefile.mk
 
+PATCH_FILES= \
+	hyphen-2.8.8-makefile-mk.patch
+
 # hyphen 2.8.8 builds cleanly from the upstream tarball; no in-tree patches
 # are needed (the old 2.7.1 patches that rewired the dictionary build and
 # tweaked the charset read are obsolete for this version).