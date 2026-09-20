c8c07ca898e41fafebb34bcd6a71572aca680e5d
Arrigo Marchiori <ardovm@yahoo.it>
2026-09-20 09:12:57 +0200

    Fix null pointer dereference

--- main/sfx2/source/appl/appuno.cxx
+++ main/sfx2/source/appl/appuno.cxx
@@ -1907,6 +1907,8 @@ ErrCode SfxMacroLoader::loadMacro( const ::rtl::OUString& rURL, com::sun::star::
                     if ( !pCurrent->AdjustMacroMode( String() ) )
                         return ERRCODE_IO_ACCESSDENIED;
                 }
+                else
+                    return ERRCODE_IO_ACCESSDENIED;
             }
 
             // find BASIC method
@@ -1972,7 +1974,7 @@ ErrCode SfxMacroLoader::loadMacro( const ::rtl::OUString& rURL, com::sun::star::
     else
     {
         // direct API call on a specified object
-        if ( !pCurrent->AdjustMacroMode( String() ) )
+        if ( !pCurrent || !pCurrent->AdjustMacroMode( String() ) )
             // check forbids execution
             return ERRCODE_IO_ACCESSDENIED;
         String aCall( '[' );
